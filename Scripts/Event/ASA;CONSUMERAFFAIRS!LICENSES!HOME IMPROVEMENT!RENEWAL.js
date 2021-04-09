02333ea4ab2b21d8d2366b56e2c48c1785f8b0ff
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2021-04-09 15:59:04 -0400

    Renewal scripts

--- Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!HOME IMPROVEMENT!RENEWAL.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!HOME IMPROVEMENT!RENEWAL.js
@@ -11,7 +11,7 @@ var addChild = aa.cap.createRenewalCap(parentCapId, capId, true);
 
 aa.cap.updateAccessByACA(capId, "N");
 copyContacts(parentCapId, capId);
-//copyASIFields(parentCapId, capId);
+copyASIFields(parentCapId, capId);
 copyASITables(parentCapId, capId);
 copyAddresses(parentCapId, capId);
 copyOwner(parentCapId, capId);
