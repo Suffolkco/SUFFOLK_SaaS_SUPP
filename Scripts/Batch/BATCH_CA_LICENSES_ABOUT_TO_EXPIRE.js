1ba53abe5ed793b8ca7337a5f5e9a105d2a7e49d
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-04-15 16:34:03 -0400

    Updated Workflow Steps

--- Scripts/Batch/BATCH_CA_LICENSES_ABOUT_TO_EXPIRE.js
+++ Scripts/Batch/BATCH_CA_LICENSES_ABOUT_TO_EXPIRE.js
@@ -227,11 +227,11 @@ function mainProcess()
                             addACAUrlsVarToEmail(vEParams);
                             for (i in wfObj)
                             {
-                                if (wfObj[i].getTaskDescription() == "License Status")
+                                if (wfObj[i].getTaskDescription() == "Issuance")
                                 {
-                                    if (wfObj[i].getDisposition() != "About to Expire")
+                                    if (wfObj[i].getDisposition() != "Pending Renewal")
                                     {
-                                        aa.workflow.handleDisposition(capId, wfObj[i].getStepNumber(), wfObj[i].getProcessID(), "About to Expire", aa.date.getCurrentDate(), "Updated via BATCH_CA_LICENSES_ABOUT_TO_EXPIRE", "Updated via BATCH_CA_LICENSES_ABOUT_TO_EXPIRE", systemUserObj, "Y");
+                                        aa.workflow.handleDisposition(capId, wfObj[i].getStepNumber(), wfObj[i].getProcessID(), "Pending Renewal", aa.date.getCurrentDate(), "Updated via BATCH_CA_LICENSES_ABOUT_TO_EXPIRE", "Updated via BATCH_CA_LICENSES_ABOUT_TO_EXPIRE", systemUserObj, "Y");
                                     }
                                 }
                             }
