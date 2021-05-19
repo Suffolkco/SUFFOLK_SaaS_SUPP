84e38f59e08dba08909624cf95ccf96b7aa728e3
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2021-05-19 11:59:22 -0400

    Removing duplicate contacts copy

--- Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
@@ -13,6 +13,16 @@ aa.cap.updateAccessByACA(capId, "N");
 if (publicUser)
 {
     //copying the contacts from the parent to the renewal record when beginning the renewal for ACA records only
+    var capContacts = aa.people.getCapContactByCapID(parentCapId);
+    if (capContacts.getSuccess())
+    {
+        capContacts = capContacts.getOutput();
+        logDebug("capContacts: " + capContacts);
+        for (var yy in capContacts)
+        {
+            aa.people.removeCapContact(parentCapId, capContacts[yy].getPeople().getContactSeqNumber());
+        }
+    }
     copyContacts(parentCapId, capId);
     copyASIFields(parentCapId, capId);
     copyASITables(parentCapId, capId);
