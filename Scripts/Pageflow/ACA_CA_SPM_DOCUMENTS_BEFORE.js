94c79b58a9fa4f60cf10ba18cbcec0fe0fc54d33
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-06-11 17:02:12 -0400

    Updates to Document Names

--- Scripts/Pageflow/ACA_CA_SPM_DOCUMENTS_BEFORE.js
+++ Scripts/Pageflow/ACA_CA_SPM_DOCUMENTS_BEFORE.js
@@ -40,23 +40,23 @@ try
     {
         docComments += "Passport Photo" + "<br>";
     }
-    if (!determineACADocumentAttached("New York State Driver's License"))
+    if (!determineACADocumentAttached("Copy of New York State Driver's License or NYSDMV non-driver photo I.D."))
     {
-        docComments += "New York State Driver's License" + "<br>";
+        docComments += "Copy of New York State Driver's License or NYSDMV non-driver photo I.D." + "<br>";
     }
     if (!determineACADocumentAttached("Proof of Banking"))
     {
         docComments += "Proof of Banking" + "<br>";
     }
 
-    if (!determineACADocumentAttached("DBA Certificate of New York State Filing Receipt"))
+    if (!determineACADocumentAttached("Copy of DBA Certificate or State Filing Receipt"))
     {
-        docComments += "DBA Certificate of New York State Filing Receipt" + "<br>";
+        docComments += "Copy of DBA Certificate or State Filing Receipt" + "<br>";
     }
 
-    if (!determineACADocumentAttached("Certificate of Liability Insurance"))
+    if (!determineACADocumentAttached("Certificate of Liability and Property Damage Insurance"))
     {
-        docComments += "Certificate of Liability Insurance" + "<br>";
+        docComments += "Certificate of Liability and Property Damage Insurance" + "<br>";
     }
 
     if (!determineACADocumentAttached("Certification by the Association of Pool and Spa Certificate"))
