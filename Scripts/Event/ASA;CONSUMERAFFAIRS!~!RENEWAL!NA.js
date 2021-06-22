39c328eae23b90f411591ef8b7d413398c408da0
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2021-06-22 14:12:21 -0400

    Updating script

--- Scripts/Event/ASA;CONSUMERAFFAIRS!~!RENEWAL!NA.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!~!RENEWAL!NA.js
@@ -1,20 +1,27 @@
 // ASA;CONSUMERAFFAIRS!~!RENEWAL!NA
 showDebug = true;
 
-var vGoodToRenew;
-var vOrgCapId;
+aa.runScriptInNewTransaction("APPLICATIONSUBMITAFTER4RENEW");
+aa.runScript("APPLICATIONSUBMITAFTER4RENEW");
 
-//Setup/Check renewal
-vGoodToRenew = prepareRenewal();
-if (parentCapId != null && vGoodToRenew)
+
+var addChild = aa.cap.createRenewalCap(parentCapId, capId, true);
+
+aa.cap.updateAccessByACA(capId, "N");
+if (publicUser)
 {
-    //copy ASI Info from license to renewal
-    copyASIInfo(parentCapId, capId);
-
-    //Copy ASIT from license to renewal
+    //copying the contacts from the parent to the renewal record when beginning the renewal for ACA records only
+    var capContacts = aa.people.getCapContactByCapID(parentCapId);
+    if (capContacts.getSuccess())
+    {
+        capContacts = capContacts.getOutput();
+        logDebug("capContacts: " + capContacts);
+        for (var yy in capContacts)
+        {
+            aa.people.removeCapContact(parentCapId, capContacts[yy].getPeople().getContactSeqNumber());
+        }
+    }
+    copyContacts(parentCapId, capId);
+    copyASIFields(parentCapId, capId);
     copyASITables(parentCapId, capId);
-
-    //Copy Contacts from license to renewal
-    copyContacts3_0(parentCapId, capId);
 }
-//End Core Renewal Functionality
