06f3e9733a4d7d2c82014fb080b72e3401cc0fa6
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-06-30 12:35:39 -0400

    Updates to required documents.

--- Scripts/Pageflow/ACA_CA_CP_DOCUMENTS_BEFORE.js
+++ Scripts/Pageflow/ACA_CA_CP_DOCUMENTS_BEFORE.js
@@ -52,6 +52,11 @@ try
     {
         docComments += "Copy of DBA Certificate or State Filing Receipt" + "<br>";
     }
+
+    if (!determineACADocumentAttached("Background Explanation"))
+    {
+        docComments += "Background Explanation" + "<br>";
+    }
   
 
     if (docComments != "")
