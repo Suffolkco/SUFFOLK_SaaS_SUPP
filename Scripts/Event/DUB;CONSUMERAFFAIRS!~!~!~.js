24a024a64e476be226d6ade1f24c67d1e2e91b68
Ada <ada.chan@suffolkcountyny.gov>
2021-07-12 09:18:36 -0400

    DCA: Add code to check for document name vs file name.

---
+++ Scripts/Event/DUB;CONSUMERAFFAIRS!~!~!~.js
@@ -0,0 +1,22 @@
+showDebug = true;
+var itemCapType = aa.cap.getCap(capId).getOutput().getCapType().toString();
+// If record type is WWM and it's a backoffice user, we do not want to update the status
+
+if (!publicUser)
+{
+   
+    var docsList = new Array();
+    docsList = getDocumentList();	//Get all Documents on a Record
+    var assignDocList = aa.util.newArrayList();
+    for(var counter = 0; counter < docsList.length; counter++)
+    {
+        //logDebug("Looping through docList.  Iterator = " + counter+ "  this is the type " +  docsList[counter].getDocCategory());
+        var thisDocument = docsList[counter];
+        logDebug("thisDocument.getDocName():" + thisDocument.getDocName());
+        logDebug("thisDocument.getFileName():" + thisDocument.getFileName());
+        // thisDocument.getDocDescription()
+        // thisDocument.getDocName()
+        // thisDocument.getFileName()
+        // thisDocument.setDocName()
+    }
+}
