c500e02411d6f37959bae01ca2391c4091165616
Ada <ada.chan@suffolkcountyny.gov>
2021-07-12 17:14:39 -0400

    test

--- Scripts/Event/DUA;CONSUMERAFFAIRS!~!~!~.js
+++ Scripts/Event/DUA;CONSUMERAFFAIRS!~!~!~.js
@@ -1,30 +1,42 @@
 showDebug = true;
 showMessage = true;
 
-var capDocResult = aa.document.getDocumentListByEntity(capId, "CAP");
-if (capDocResult.getSuccess())
-{       
-    logDebug("*** count *** " + capDocResult.getOutput().size());
-    debugObject("**********" + capDocResult.getOutput());
+if (!publicUser)
+{
+    aa.ads.ads.DocumentModel
 
-    for (docInx = 0; docInx < capDocResult.getOutput().size(); docInx++)
-    {
-        var documentObject = capDocResult.getOutput().get(docInx);        
-       
-        if (documentObject.getDocName() == "*")
+    var capDocResult = aa.document.getDocumentListByEntity(capId, "CAP");
+    if (capDocResult.getSuccess())
+    {       
+        logDebug("*** count *** " + capDocResult.getOutput().size());
+        debugObject("*******capDocResult*****" + capDocResult);
+        debugObject("*******getOutput*****" + capDocResult.getOutput());
+
+        for (docInx = 0; docInx < capDocResult.getOutput().size(); docInx++)
         {
-            logDebug("*** documentNo *****" + documentObject.getDocumentNo());
-            logDebug("docName:" + documentObject.getDocName());
-            logDebug("fileName:" + documentObject.getFileName());
-            documentObject.setDocName(documentObject.getFileName());
-            //documentObject.setCapId(capId);
-            logDebug("Setting docName to filename:" + documentObject.getFileName());
-            logDebug("Getting docName:" + documentObject.getDocumentNo() + ":" + documentObject.getDocName());
-            //emailText = emailText + documentObject.getDocName();
+            var documentObject = capDocResult.getOutput().get(docInx);        
+      
+            if (documentObject.getDocName() == "*")
+            {
+                debugObject("*******documentObject*****" +documentObject);
+                logDebug("*** documentNo *****" + documentObject.getDocumentNo());
+                logDebug("docName:" + documentObject.getDocName());
+                logDebug("fileName:" + documentObject.getFileName());
+                docContent = documentObject.getDocumentContent();
+                documentObject.setDocName(documentObject.getFileName());
+                
+                documentObject.setDocDescription("Test");
+                documentObject.setDocumentContent(docContent);
+                
+                logDebug("Setting docName to filename:" + documentObject.getFileName());
+                //documentObject.setCapId(capId);
+                logDebug("Setting docName to filename:" + documentObject.getFileName());
+                logDebug("Getting docName:" + documentObject.getDocumentNo() + ":" + documentObject.getDocName());
+                //emailText = emailText + documentObject.getDocName();
+            }
         }
     }
 }
-
 //aa.sendMail("noreplyehimslower@suffolkcountyny.gov", "ada.chan@suffolkcountyny.gov", "", "DUA script", emailText);
 
 
