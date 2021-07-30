bc4b94429f13f9d6ae251521a5541c675171f6b8
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2021-07-30 10:47:49 -0400

    Updating script

--- Scripts/ConsumerAffairs/CA_REN_TO_LIC_SUBMITTAL.js
+++ Scripts/ConsumerAffairs/CA_REN_TO_LIC_SUBMITTAL.js
@@ -72,15 +72,21 @@ if (publicUser)
             }
         }
 
-        if (isTaskActive("Renewal Review"))
+        if (isTaskActive("Issuance"))
         {
             logDebug("Task is closing");
-            closeTask("Renewal Review", "Complete", "Updated by Renewal Script", "Updated by Renewal Script");
+            closeTask("Issuance", "Renewed", "Updated by Renewal Script", "Updated by Renewal Script");
         }
         //copying back from the renewal to the parent for records where the condition has been met
         copyContacts(capId, parentCapId);
-        copyASIFields(capId, parentCapId);
-        copyASITables(capId, parentCapId);
+        AInfo = new Array();
+        loadAppSpecific(AInfo, capId);
+        for (asi in AInfo)
+        {
+            //Check list
+            logDebug("ASI: " + asi + " value is:" + AInfo[asi]);
+            editAppSpecificLOCAL(asi, AInfo[asi], parentCapId);
+        }        copyASITables(capId, parentCapId);
 
         var conArray = getContactByType("Applicant", capId);
         var conEmail = "";
@@ -93,5 +99,56 @@ if (publicUser)
             logDebug("Email addresses: " + conEmail);
             sendNotification("", conEmail, "", "CA_LICENSE_RENEWAL_APPLICANT_NOTICE", vEParams, null);
         }
+    }
+} 
+
+function editAppSpecificLOCAL(itemName, itemValue)  // optional: itemCap
+{
+    var itemCap = capId;
+    var itemGroup = null;
+    if (arguments.length == 3) itemCap = arguments[2]; // use cap ID specified in args
+
+    if (useAppSpecificGroupName)
+    {
+        if (itemName.indexOf(".") < 0)
+        { logDebug("**WARNING: (editAppSpecific) requires group name prefix when useAppSpecificGroupName is true"); return false }
+
+
+        itemGroup = itemName.substr(0, itemName.indexOf("."));
+        itemName = itemName.substr(itemName.indexOf(".") + 1);
+    }
+    // change 2/2/2018 - update using: aa.appSpecificInfo.editAppSpecInfoValue(asiField)
+    // to avoid issue when updating a blank custom form via script. It was wiping out the field alias 
+    // and replacing with the field name
+
+    var asiFieldResult = aa.appSpecificInfo.getByList(itemCap, itemName);
+    if (asiFieldResult.getSuccess())
+    {
+        var asiFieldArray = asiFieldResult.getOutput();
+        if (asiFieldArray.length > 0)
+        {
+            var asiField = asiFieldArray[0];
+            if (asiField)
+            {
+                var origAsiValue = asiField.getChecklistComment();
+                asiField.setChecklistComment(itemValue);
+
+                var updateFieldResult = aa.appSpecificInfo.editAppSpecInfoValue(asiField);
+                if (updateFieldResult.getSuccess())
+                {
+                    logDebug("Successfully updated custom field on record: " + itemCap.getCustomID() + " on " + itemName + " with value: " + itemValue);
+                    if (arguments.length < 3) //If no capId passed update the ASI Array
+                        AInfo[itemName] = itemValue;
+                }
+                else
+                { logDebug("WARNING: (editAppSpecific) " + itemName + " was not updated."); }
+            }
+            else
+            { logDebug("WARNING: (editAppSpecific) " + itemName + " was not updated."); }
+        }
+    }
+    else
+    {
+        logDebug("ERROR: (editAppSpecific)" + asiFieldResult.getErrorMessage());
     }
 } 
