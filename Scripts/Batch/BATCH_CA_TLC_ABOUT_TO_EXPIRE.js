844e70dbc9435a6f3375adf423bc2193c3ac5d32
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-07-30 10:22:20 -0400

    Removal of unnecessary functions

--- Scripts/Batch/BATCH_CA_TLC_ABOUT_TO_EXPIRE.js
+++ Scripts/Batch/BATCH_CA_TLC_ABOUT_TO_EXPIRE.js
@@ -184,7 +184,6 @@ function mainProcess()
                                                         addParameter(vEParams, "$$altID$$", capIDString);
                                                         addParameter(vEParams, "$$capAlias$$", cap.getCapType().getAlias());
                                                         addParameter(vRParams, "RecordID", capIDString);
-                                                        addParameter(vRParams, "RecordID", capIDString);
                                                         addParameter(vRParams, "FromDate", dateToCheck);
                                                         addParameter(vRParams, "ToDate", dateToCheck);
                                                         addParameter(vRParams, "Email", "Yes");
@@ -434,88 +433,7 @@ function getAppStatus()
     }
     return appStatus;
 }
-function emailWithReportAttachASync(pSendToEmailAddresses, pEmailTemplate, pEParams, pReportTemplate, pRParams, pAddAdHocTask, pChangeReportDescription)
-{
-    var x = 0;
-    var vAsyncScript = "SEND_EMAIL_ATTACH_ASYNC";
-    var envParameters = aa.util.newHashMap();
 
-    //Initialize optional parameters	
-    var vEParams = aa.util.newHashtable();
-    var vReportTemplate = "";
-    var vRParams = aa.util.newHashtable();
-    var vAddAdHocTask = true;
-    var vChangeReportDescription = "";
-
-    if (pEParams != undefined && pEParams != null && pEParams != "")
-    {
-        logDebug("pEParams is defined");
-        vEParams = pEParams;
-    }
-
-    if (pReportTemplate != undefined && pReportTemplate != null && pReportTemplate != "")
-    {
-        logDebug("pReportTemplate is defined");
-        vReportTemplate = pReportTemplate;
-    }
-
-    if (pRParams != undefined && pRParams != null && pRParams != "")
-    {
-        logDebug("pRParams is defined");
-        vRParams = pRParams;
-    }
-
-    if (pAddAdHocTask != undefined && pAddAdHocTask != null && pAddAdHocTask != "")
-    {
-        logDebug("pAddAdHocTask is defined");
-        if (pAddAdHocTask == "N")
-        {
-            vAddAdHocTask = false;
-        } else if (pAddAdHocTask == false)
-        {
-            vAddAdHocTask = false;
-        }
-    }
-
-    if (pChangeReportDescription != undefined && pChangeReportDescription != null && pChangeReportDescription != "")
-    {
-        logDebug("pChangeReportDescription is defined");
-        vChangeReportDescription = pChangeReportDescription;
-    }
-
-    //Save variables to the hash table and call sendEmailASync script. This allows for the email to contain an ACA deep link for the document
-    envParameters.put("sendToEmailAddresses", pSendToEmailAddresses);
-    envParameters.put("emailTemplate", pEmailTemplate);
-    envParameters.put("vEParams", vEParams);
-    envParameters.put("reportTemplate", vReportTemplate);
-    envParameters.put("vRParams", vRParams);
-    envParameters.put("vChangeReportDescription", vChangeReportDescription);
-    envParameters.put("CapId", capId);
-
-    //Start modification to support batch script
-    var vEvntTyp = aa.env.getValue("eventType");
-    if (vEvntTyp == "Batch Process")
-    {
-        aa.env.setValue("sendToEmailAddresses", pSendToEmailAddresses);
-        aa.env.setValue("emailTemplate", pEmailTemplate);
-        aa.env.setValue("vEParams", vEParams);
-        aa.env.setValue("reportTemplate", vReportTemplate);
-        aa.env.setValue("vRParams", vRParams);
-        aa.env.setValue("vChangeReportDescription", vChangeReportDescription);
-        aa.env.setValue("CapId", capId);
-        //call sendEmailASync script
-        logDebug("Attempting to run Non-Async: " + vAsyncScript);
-        aa.includeScript(vAsyncScript);
-    } else
-    {
-        //call sendEmailASync script
-        logDebug("Attempting to run Async: " + vAsyncScript);
-        aa.runAsyncScript(vAsyncScript, envParameters);
-    }
-    //End modification to support batch script
-
-    return true;
-}
 function generateReportBatch(itemCap, reportName, module, parameters)
 {
     //returns the report file which can be attached to an email.
