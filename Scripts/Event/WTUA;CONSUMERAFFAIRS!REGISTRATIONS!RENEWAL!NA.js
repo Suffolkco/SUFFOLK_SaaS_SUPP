280a4e368a94b70f595762eeb0ae54f9e29eada6
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2021-07-30 11:49:00 -0400

    Updating scripts

--- Scripts/Event/WTUA;CONSUMERAFFAIRS!REGISTRATIONS!RENEWAL!NA.js
+++ Scripts/Event/WTUA;CONSUMERAFFAIRS!REGISTRATIONS!RENEWAL!NA.js
@@ -10,7 +10,7 @@ if (wfTask == "Issuance" && wfStatus == "Renewed")
     logDebug("New Date Exp Date is: " + expDateASI)
     var newExpDate = (expDateASI.getMonth() + 1) + "/" + 1 + "/" + (expDateASI.getFullYear() + 2);
     logDebug("New Exp Date is: " + newExpDate);
-    editAppSpecific("Expiration Date", newExpDate, parentCapId);
+    editAppSpecificLOCAL("Expiration Date", newExpDate, parentCapId);
     if (expDateASI != null)
     {
         var b1ExpResult = aa.expiration.getLicensesByCapID(parentCapId);
