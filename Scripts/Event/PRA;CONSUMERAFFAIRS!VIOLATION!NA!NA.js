09f1aa7573b91a2d8cd45beac5b00dfef4306bb7
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-08-02 15:45:11 -0400

    Updates

--- Scripts/Event/PRA;CONSUMERAFFAIRS!VIOLATION!NA!NA.js
+++ Scripts/Event/PRA;CONSUMERAFFAIRS!VIOLATION!NA!NA.js
@@ -94,3 +94,28 @@ if (contactResult.getSuccess())
         }
     }
 }    
+function getContactName(vConObj)
+{
+    if (vConObj.people.getContactTypeFlag() == "organization")
+    {
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
+    else
+    {
+        if (vConObj.people.getFullName() != null && vConObj.people.getFullName() != "")
+        {
+            return vConObj.people.getFullName();
+        }
+        if (vConObj.people.getFirstName() != null && vConObj.people.getLastName() != null)
+        {
+            return vConObj.people.getFirstName() + " " + vConObj.people.getLastName();
+        }
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
+}
