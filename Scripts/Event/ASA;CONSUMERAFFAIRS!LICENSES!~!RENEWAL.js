fe01e3e6b3a08f34ad1530dd7aeb4a0ab98cbcf9
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-08-23 13:21:44 -0400

    UPDATES

--- Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
@@ -9,7 +9,7 @@ aa.runScript("APPLICATIONSUBMITAFTER4RENEW");
 
 var addChild = aa.cap.createRenewalCap(parentCapId, capId, true);
 
-aa.cap.updateAccessByACA(capId, "N");
+aa.cap.updateAccessByACA(capId, "Y");
 if (publicUser)
 {
     //copying the contacts from the parent to the renewal record when beginning the renewal for ACA records only
