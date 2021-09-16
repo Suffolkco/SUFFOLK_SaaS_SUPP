e377b2e65f6cec6bcfc1fa49170d541bd4afc129
Ada <ada.chan@suffolkcountyny.gov>
2021-09-16 13:47:24 -0400

    Add Feeexists function.

--- Scripts/Batch/DEQ_OPC_RENEWAL_EXPIRATION_60DAYS.js
+++ Scripts/Batch/DEQ_OPC_RENEWAL_EXPIRATION_60DAYS.js
@@ -282,6 +282,33 @@ function mainProcess()
 /*------------------------------------------------------------------------------------------------------/
 | <===========Internal Functions and Classes (Used by this script)
 /------------------------------------------------------------------------------------------------------*/
+function feeExists(feestr) // optional statuses to check for
+{
+	var checkStatus = false;
+	var statusArray = new Array();
+
+	//get optional arguments
+	if (arguments.length > 1) {
+		checkStatus = true;
+		for (var i = 1; i < arguments.length; i++)
+			statusArray.push(arguments[i]);
+	}
+
+	var feeResult = aa.fee.getFeeItems(capId, feestr, null);
+	if (feeResult.getSuccess()) {
+		var feeObjArr = feeResult.getOutput();
+	} else {
+		logDebug("**ERROR: getting fee items: " + capContResult.getErrorMessage());
+		return false
+	}
+
+	for (ff in feeObjArr)
+		if (feestr.equals(feeObjArr[ff].getFeeCod()) && (!checkStatus || exists(feeObjArr[ff].getFeeitemStatus(), statusArray)))
+			return true;
+
+	return false;
+} 
+
 function isMatchCapCondition(capConditionScriptModel1, capConditionScriptModel2)
 {
   if (capConditionScriptModel1 == null || capConditionScriptModel2 == null)
