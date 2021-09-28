fff00bbd10d59734ce6c256d7eb0f251f88cf1e9
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2021-09-28 15:50:00 -0400

    Updates

--- Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
@@ -45,23 +45,6 @@ if (tableCopy == 0)
     tableCopy = tableCopy + 1;
 }
 
-
-var rowsInTable = 0;
-if (typeof (RESTRICTIONS) == "object")
-{
-    logDebug("RESTRICTIONS is an object")
-    for (var rows = 0; rows < RESTRICTIONS.length; rows++)
-    {
-        rowsInTable += 1; 
-    }
-}
-logDebug("Number of Rows in Table is: " + rowsInTable);
-var feesToAssess = rowsInTable - 1;
-if (feesToAssess != 0 && publicUser)
-{
-    updateFee("CA_SALES", "SLS_22", "FINAL", feesToAssess, "Y");
-}
-
 function editAppSpecificLOCAL(itemName, itemValue)  // optional: itemCap
 {
     var itemCap = capId;
