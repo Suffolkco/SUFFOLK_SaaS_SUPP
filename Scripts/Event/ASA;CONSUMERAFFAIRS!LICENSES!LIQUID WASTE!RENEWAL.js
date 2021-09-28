0c999c55a492a708886a9fe2f44c6e9eadae5507
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2021-09-28 16:20:14 -0400

    Updates

--- Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!LIQUID WASTE!RENEWAL.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!LIQUID WASTE!RENEWAL.js
@@ -10,11 +10,19 @@ if (typeof (RESTRICTIONS) == "object")
     for (var rows = 0; rows < RESTRICTIONS.length; rows++)
     {
         rowsInTable += 1;
+        var thisRow = RESTRICTIONS[rows];
+        var categoryNeeded = thisRow["Category"];
+        logDebug("category needed is: " + categoryNeeded);
     }
-}
-logDebug("Number of Rows in Table is: " + rowsInTable);
-var feesToAssess = rowsInTable - 1;
-if (feesToAssess != 0 && publicUser)
-{
-    updateFee("CA_SALES", "SLS_22", "FINAL", feesToAssess, "Y");
+
+    logDebug("Number of Rows in Table is: " + rowsInTable);
+    var feesToAssess = rowsInTable - 1;
+    if (feesToAssess != 0 && publicUser)
+    {
+        if (matches(categoryNeeded, "LW - Wastewater Demo Project", "LW12 - All Endorsements"))
+        {
+            logDebug("we have a match for one row");
+            updateFee("SLS_22", "CA_SALES", "FINAL", feesToAssess, "Y");
+        }
+    }
 }
