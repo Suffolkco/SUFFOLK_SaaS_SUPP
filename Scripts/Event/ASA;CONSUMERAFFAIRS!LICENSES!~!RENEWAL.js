dae27030cc507cbc457b6b4edc36ffae31507ad3
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2021-09-28 15:34:17 -0400

    Updates

--- Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
@@ -57,7 +57,7 @@ if (typeof (RESTRICTIONS) == "object")
 }
 logDebug("Number of Rows in Table is: " + rowsInTable);
 var feesToAssess = rowsInTable - 1;
-if (feesToAssess != 0 && capId)
+if (feesToAssess != 0 && !publicUser)
 {
     updateFee("CA_SALES", "SLS_22", "FINAL", feesToAssess, "Y");
 }
