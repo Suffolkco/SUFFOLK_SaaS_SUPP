3ca7ffb9c1cdf4331398869d8473403d658f3bea
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2021-09-28 16:28:23 -0400

    Updates

--- Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!LIQUID WASTE!RENEWAL.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!LIQUID WASTE!RENEWAL.js
@@ -3,26 +3,25 @@
 //showDebug = 1;
 //logDebug("Entering Renew ASA");
 
-var rowsInTable = 0;
+var rowsToPay = 0;
 if (typeof (RESTRICTIONS) == "object")
 {
     logDebug("RESTRICTIONS is an object")
     for (var rows = 0; rows < RESTRICTIONS.length; rows++)
     {
-        rowsInTable += 1;
         var thisRow = RESTRICTIONS[rows];
         var categoryNeeded = thisRow["Category"];
         logDebug("category needed is: " + categoryNeeded);
+        if (!matches(categoryNeeded, "LW - Wastewater Demo Project", "LW12 - All Endorsements"))
+        {
+            rowsToPay = rowsToPay + 1;
+        }
     }
-
-    logDebug("Number of Rows in Table is: " + rowsInTable);
-    var feesToAssess = rowsInTable - 1;
+    logDebug("Number of Billable Rows in Table is: " + rowsToPay);
+    var feesToAssess = rowsToPay - 1;
     if (feesToAssess != 0 && publicUser)
     {
-        if (matches(categoryNeeded, "LW - Wastewater Demo Project", "LW12 - All Endorsements"))
-        {
-            logDebug("we have a match for one row");
-            updateFee("SLS_22", "CA_SALES", "FINAL", feesToAssess, "Y");
-        }
+        logDebug("we have a match for one row");
+        updateFee("SLS_22", "CA_SALES", "FINAL", feesToAssess, "Y");
     }
 }
