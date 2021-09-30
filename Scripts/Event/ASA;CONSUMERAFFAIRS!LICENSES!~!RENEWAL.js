3491bed1ce2dc1c84a652f041c9c910b95a262ce
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-09-30 17:49:09 -0400

    Updates

--- Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
@@ -157,3 +157,20 @@ function editAppSpecificLOCAL(itemName, itemValue)  // optional: itemCap
         logDebug("ERROR: (editAppSpecific)" + asiFieldResult.getErrorMessage());
     }
 }
+function getIncompleteCapId()  
+{
+    var s_id1 = aa.env.getValue("PermitId1");
+    var s_id2 = aa.env.getValue("PermitId2");
+    var s_id3 = aa.env.getValue("PermitId3");
+
+    var result = aa.cap.getCapIDModel(s_id1, s_id2, s_id3);
+    if(result.getSuccess())
+	{
+      return result.getOutput();
+	}  
+    else 
+    {
+      aa.print("ERROR: Failed to get capId: " + result.getErrorMessage());
+      return null;
+    }
+}
