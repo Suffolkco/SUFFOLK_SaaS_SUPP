2268f6b1d288379656e3c7be6ee27ba210d5f466
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-09-30 18:43:47 -0400

    Updates

--- Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
@@ -6,18 +6,15 @@
 //aa.runScriptInNewTransaction("APPLICATIONSUBMITAFTER4RENEW");
 //aa.runScript("APPLICATIONSUBMITAFTER4RENEW");
 
+
+
+
 var conArray = getContactArray(capId);
-var partialCapId = getIncompleteCapId();
-var parentCapId = aa.env.getValue("ParentCapID");
 
 if (conArray.length < 1)
 {
     var addChild = aa.cap.createRenewalCap(parentCapId, capId, true);
     aa.cap.updateAccessByACA(capId, "Y");
-
-}
-if (isRenewProcess(parentCapId, partialCapId))
-{
     if (publicUser)
     {
         //copying the contacts from the parent to the renewal record when beginning the renewal for ACA records only
@@ -31,82 +28,28 @@ if (isRenewProcess(parentCapId, partialCapId))
                 aa.people.removeCapContact(parentCapId, capContacts[yy].getPeople().getContactSeqNumber());
             }
         }
-        aa.print("CAPID(" + parentCapId + ") is ready for renew. PartialCap (" + partialCapId + ")");
-        //2. Associate partial cap with parent CAP.
-        var result = aa.cap.createRenewalCap(parentCapId, partialCapId, true);
-        if (result.getSuccess())
 
-            copyContacts(parentCapId, capId);
 
-        AInfo = new Array();
-        loadAppSpecific(AInfo, parentCapId);
-        for (asi in AInfo)
-        {
-            //Check list
-            logDebug("ASI: " + asi + " value is:" + AInfo[asi]);
-            editAppSpecificLOCAL(asi, AInfo[asi], capId);
-        }
-
-        var tableCopy = 0;
-        if (tableCopy == 0)
-        {
-            copyASITables(parentCapId, capId);
-            tableCopy = tableCopy + 1;
-        }
-
-        //4. Set B1PERMIT.B1_ACCESS_BY_ACA to "N" for partial CAP to not allow that it is searched by ACA user.
-        aa.cap.updateAccessByACA(partialCapId, "N");
     }
-    else
+
+    copyContacts(parentCapId, capId);
+
+    AInfo = new Array();
+    loadAppSpecific(AInfo, parentCapId);
+    for (asi in AInfo)
     {
-        aa.print("ERROR: Associate partial cap with parent CAP. " + result.getErrorMessage());
+        //Check list
+        logDebug("ASI: " + asi + " value is:" + AInfo[asi]);
+        editAppSpecificLOCAL(asi, AInfo[asi], capId);
     }
-    aa.env.setValue("ScriptReturnCode", "0");
-    aa.env.setValue("ScriptReturnMessage", "ApplicationSubmitAfter4Renew perform renewal process.");
+
+    var tableCopy = 0;
+    if (tableCopy == 0)
+    {
+        copyASITables(parentCapId, capId);
+        tableCopy = tableCopy + 1;
+    }
 }
-
-function isRenewProcess(parentCapID, partialCapID)
-{
-    //1. Check to see parent CAP ID is null.
-    if (parentCapID == null || partialCapID == null
-        || aa.util.instanceOfString(parentCapID))
-    {
-        return false;
-    }
-    //2. Get CAPModel by PK for partialCAP.
-    var result = aa.cap.getCap(partialCapID);
-    if (result.getSuccess())
-    {
-        capScriptModel = result.getOutput();
-        //2.1. Check to see if it is partial CAP.	
-        if (capScriptModel.isCompleteCap())
-        {
-            aa.print("ERROR: It is not partial CAP(" + capScriptModel.getCapID() + ")");
-            return false;
-        }
-    }
-    else
-    {
-        aa.print("ERROR: Fail to get CAPModel (" + partialCapID + "): " + result.getErrorMessage());
-        return false;
-    }
-    //3.  Check to see if the renewal was initiated before. 
-    result = aa.cap.getProjectByMasterID(parentCapID, "Renewal", "Incomplete");
-    if (result.getSuccess())
-    {
-        partialProjects = result.getOutput();
-        if (partialProjects != null && partialProjects.length > 0)
-        {
-            //Avoid to initiate renewal process multiple times.
-            aa.print("Warning: Renewal process was initiated before. ( " + parentCapID + ")");
-            return false;
-        }
-
-    }
-    //4 . Check to see if parent CAP is ready for renew.
-    return isReadyRenew(parentCapID);
-}
-
 function editAppSpecificLOCAL(itemName, itemValue)  // optional: itemCap
 {
     var itemCap = capId;
@@ -155,22 +98,5 @@ function editAppSpecificLOCAL(itemName, itemValue)  // optional: itemCap
     else
     {
         logDebug("ERROR: (editAppSpecific)" + asiFieldResult.getErrorMessage());
-    }
-}
-function getIncompleteCapId()  
-{
-    var s_id1 = aa.env.getValue("PermitId1");
-    var s_id2 = aa.env.getValue("PermitId2");
-    var s_id3 = aa.env.getValue("PermitId3");
-
-    var result = aa.cap.getCapIDModel(s_id1, s_id2, s_id3);
-    if(result.getSuccess())
-	{
-      return result.getOutput();
-	}  
-    else 
-    {
-      aa.print("ERROR: Failed to get capId: " + result.getErrorMessage());
-      return null;
     }
 }
