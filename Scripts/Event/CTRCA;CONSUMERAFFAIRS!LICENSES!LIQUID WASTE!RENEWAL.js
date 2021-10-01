57782075de789b5f664e97ecbba7e7c6856d00e1
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-10-01 15:03:39 -0400

    Updates

--- Scripts/Event/CTRCA;CONSUMERAFFAIRS!LICENSES!LIQUID WASTE!RENEWAL.js
+++ Scripts/Event/CTRCA;CONSUMERAFFAIRS!LICENSES!LIQUID WASTE!RENEWAL.js
@@ -2,27 +2,32 @@
 
 //showDebug = 1;
 //logDebug("Entering Renew ASA");
-if(!publicuser)
+
+if (!publicUser)
 {
-removeAllFees(capId); //If user updates table after assessment 
-if (typeof (RESTRICTIONS) == "object") {
-    var rowsToPay = 0;
-    logDebug("RESTRICTIONS is an object");
-    for (var rows = 1; rows < RESTRICTIONS.length; rows++) {
-        var thisRow = RESTRICTIONS[rows];
-        var categoryNeeded = thisRow["Category"];
-        logDebug("Category found is: " + categoryNeeded);
-        if (!matches(categoryNeeded, "LW - Wastewater Demo Project", "LW12 - All Endorsements")) {
-            rowsToPay++;
-            logDebug("Found a row that is: " + categoryNeeded + ". We will bill for this row.")
+    if (typeof (RESTRICTIONS) == "object")
+    {
+        var rowsToPay = 0;
+        logDebug("RESTRICTIONS is an object");
+        for (var rows = 1; rows < RESTRICTIONS.length; rows++)
+        {
+            var thisRow = RESTRICTIONS[rows];
+            var categoryNeeded = thisRow["Category"];
+            logDebug("Category found is: " + categoryNeeded);
+            if (!matches(categoryNeeded, "LW - Wastewater Demo Project", "LW12 - All Endorsements"))
+            {
+                rowsToPay++;
+                logDebug("Found a row that is: " + categoryNeeded + ". We will bill for this row.")
+            }
+            else
+            {
+                logDebug("Found a row that is: " + categoryNeeded + ". Will not bill for this row.")
+            }
         }
-        else {
-            logDebug("Found a row that is: " + categoryNeeded + ". Will not bill for this row.")
+        logDebug("Number of Billable Rows in Table is: " + rowsToPay);
+        if (rowsToPay)
+        {
+            addFee("SLS_22", "CA_SALES", "FINAL", rowsToPay, "Y");
         }
-    } 
-    logDebug("Number of Billable Rows in Table is: " + rowsToPay);
-    if (rowsToPay) {
-        addFee("SLS_22", "CA_SALES", "FINAL", rowsToPay, "Y");
     }
-}
 }
