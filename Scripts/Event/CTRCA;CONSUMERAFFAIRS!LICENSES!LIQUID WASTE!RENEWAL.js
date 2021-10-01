1ee4529f91a9f08ed3b04b9d21a057e3aabbb820
Jacob Greene <jacob.greene@scubeenterprise.com>
2021-10-01 14:30:19 -0400

    Updates

--- Scripts/Event/CTRCA;CONSUMERAFFAIRS!LICENSES!LIQUID WASTE!RENEWAL.js
+++ Scripts/Event/CTRCA;CONSUMERAFFAIRS!LICENSES!LIQUID WASTE!RENEWAL.js
@@ -3,30 +3,24 @@
 //showDebug = 1;
 //logDebug("Entering Renew ASA");
 
-var rowsToPay = 0;
-if (typeof (RESTRICTIONS) == "object")
-{
-    logDebug("RESTRICTIONS is an object")
-    for (var rows = 0; rows < RESTRICTIONS.length; rows++)
-    {
+removeAllFees(capId); //If user updates table after assessment 
+if (typeof (RESTRICTIONS) == "object") {
+    var rowsToPay = 0;
+    logDebug("RESTRICTIONS is an object");
+    for (var rows = 1; rows < RESTRICTIONS.length; rows++) {
         var thisRow = RESTRICTIONS[rows];
         var categoryNeeded = thisRow["Category"];
         logDebug("Category found is: " + categoryNeeded);
-        if (!matches(categoryNeeded, "LW - Wastewater Demo Project", "LW12 - All Endorsements"))
-        {
-            rowsToPay = rowsToPay + 1;
+        if (!matches(categoryNeeded, "LW - Wastewater Demo Project", "LW12 - All Endorsements")) {
+            rowsToPay++;
             logDebug("Found a row that is: " + categoryNeeded + ". We will bill for this row.")
         }
-        else
-        { 
-            logDebug("Found a row that is: " + categoryNeeded + ". Will not bill for this row.") 
+        else {
+            logDebug("Found a row that is: " + categoryNeeded + ". Will not bill for this row.")
         }
-    }
+    } 
     logDebug("Number of Billable Rows in Table is: " + rowsToPay);
-    var feesToAssess = rowsToPay - 1;
-    if (feesToAssess > 0 && !publicUser)
-    {
-        removeAllFees(capId);
-        addFee("SLS_22", "CA_SALES", "FINAL", feesToAssess, "Y");
+    if (rowsToPay) {
+        addFee("SLS_22", "CA_SALES", "FINAL", rowsToPay, "Y");
     }
 }
