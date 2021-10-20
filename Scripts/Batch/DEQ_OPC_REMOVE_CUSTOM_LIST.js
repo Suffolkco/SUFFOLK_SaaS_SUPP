7dc1629bf630ada6fde85d9657b38e6484043043
Ada <ada.chan@suffolkcountyny.gov>
2021-10-20 11:35:21 -0400

    Test

--- Scripts/Batch/DEQ_OPC_REMOVE_CUSTOM_LIST.js
+++ Scripts/Batch/DEQ_OPC_REMOVE_CUSTOM_LIST.js
@@ -236,6 +236,57 @@ function mainProcess()
 /*------------------------------------------------------------------------------------------------------/
 | <===========Internal Functions and Classes (Used by this script)
 /------------------------------------------------------------------------------------------------------*/
+function loadASITable(tname)
+{
+	itemCap = arguments[1]; // use cap ID specified in args
+
+	var gm = aa.appSpecificTableScript.getAppSpecificTableGroupModel(itemCap).getOutput();
+	var ta = gm.getTablesArray()
+	var tai = ta.iterator();
+
+	while (tai.hasNext())
+	{
+	  var tsm = tai.next();
+	  var tn = tsm.getTableName();
+
+      if (!tn.equals(tname)) continue;
+
+	  if (tsm.rowIndex.isEmpty())
+	  	{
+			aa.print("Couldn't load ASI Table " + tname + " it is empty");
+			return false;
+		}
+
+   	  var tempObject = new Array();
+	  var tempArray = new Array();
+
+  	  var tsmfldi = tsm.getTableField().iterator();
+	  var tsmcoli = tsm.getColumns().iterator();
+      var readOnlyi = tsm.getAppSpecificTableModel().getReadonlyField().iterator(); // get Readonly filed
+	  var numrows = 1;
+
+	  while (tsmfldi.hasNext())  // cycle through fields
+		{
+		if (!tsmcoli.hasNext())  // cycle through columns
+			{
+			var tsmcoli = tsm.getColumns().iterator();
+			tempArray.push(tempObject);  // end of record
+			var tempObject = new Array();  // clear the temp obj
+			numrows++;
+			}
+		var tcol = tsmcoli.next();
+		var tval = tsmfldi.next();
+		var readOnly = 'N';
+		if (readOnlyi.hasNext()) {
+			readOnly = readOnlyi.next();
+		}
+		var fieldInfo = new asiTableValObj(tcol.getColumnName(), tval, readOnly);
+		tempObject[tcol.getColumnName()] = fieldInfo;
+		}		
+	  tempArray.push(tempObject);  // end of record
+	}	
+	return tempArray;
+}
 function removeASITable(tableName) // optional capId
 {
 	//  tableName is the name of the ASI table
