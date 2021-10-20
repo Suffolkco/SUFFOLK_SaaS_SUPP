185773b8806089cc48bb2074f6da6b025f97f2d6
Ada <ada.chan@suffolkcountyny.gov>
2021-10-20 10:26:14 -0400

    Test

--- Scripts/Batch/DEQ_OPC_REMOVE_CUSTOM_LIST.js
+++ Scripts/Batch/DEQ_OPC_REMOVE_CUSTOM_LIST.js
@@ -172,7 +172,7 @@ function mainProcess()
     {
 		var count = 0;
         logDebug("Batch script will run");        
-		var vSQL = "SELECT DISTINCT COUNT(B.B1_ALT_ID) as recordNumber FROM B1PERMIT B JOIN BCHCKBOX C ON B.B1_PER_ID1 = C.B1_PER_ID1 AND B.B1_PER_ID2 = C.B1_PER_ID2 AND B.B1_PER_ID3 = C.B1_PER_ID3 WHERE B.SERV_PROV_CODE = 'SUFFOLKCO' AND B.B1_PER_GROUP = 'DEQ' AND B.B1_PER_TYPE = 'General' AND B.B1_PER_SUB_TYPE = 'Site' AND B.B1_PER_CATEGORY = 'NA' AND C.B1_CHECKBOX_DESC = 'OPC' AND C.B1_CHECKLIST_COMMENT = 'CHECKED'";       		
+		var vSQL = "SELECT DISTINCT B.B1_ALT_ID as recordNumber FROM B1PERMIT B JOIN BCHCKBOX C ON B.B1_PER_ID1 = C.B1_PER_ID1 AND B.B1_PER_ID2 = C.B1_PER_ID2 AND B.B1_PER_ID3 = C.B1_PER_ID3 WHERE B.SERV_PROV_CODE = 'SUFFOLKCO' AND B.B1_PER_GROUP = 'DEQ' AND B.B1_PER_TYPE = 'General' AND B.B1_PER_SUB_TYPE = 'Site' AND B.B1_PER_CATEGORY = 'NA' AND C.B1_CHECKBOX_DESC = 'OPC' AND C.B1_CHECKLIST_COMMENT = 'CHECKED'";       		
         var output = "Record ID\n";  		
         
 		// SQL to pull active OPC site records that has NO child Tank records		
