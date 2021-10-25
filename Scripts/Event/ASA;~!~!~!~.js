22f0abdde24b8955f9fe64face5e6e707961da9d
Ada <ada.chan@suffolkcountyny.gov>
2021-10-25 10:42:52 -0400

    Test

--- Scripts/Event/ASA;~!~!~!~.js
+++ Scripts/Event/ASA;~!~!~!~.js
@@ -1,9 +1,8 @@
 showDebug = true; 
 
 if (!publicUser) {
-        logDebug("**START** " + capId);
-        createupdateRefLPFromRecordLP(capId);
-        logDebug("**SECOND** " + capId);
+        /*
+        createupdateRefLPFromRecordLP(capId);       
 		createRefContactsFromCapContactsAndLink(capId, null, null, null, true, comparePeopleMatchCriteria);
-        logDebug("**THIRD** " + capId);
+        */
 } 
