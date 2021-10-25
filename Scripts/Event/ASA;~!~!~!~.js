4f86cf915f8557585c1837d012a18eb0b0ff792a
Ada <ada.chan@suffolkcountyny.gov>
2021-10-25 10:34:53 -0400

    EHIMS-4663: Testing

--- Scripts/Event/ASA;~!~!~!~.js
+++ Scripts/Event/ASA;~!~!~!~.js
@@ -1,4 +1,9 @@
+showDebug = true; 
+
 if (!publicUser) {
+        logDebug("**START** " + capId);
         createupdateRefLPFromRecordLP(capId);
+        logDebug("**SECOND** " + capId);
 		createRefContactsFromCapContactsAndLink(capId, null, null, null, true, comparePeopleMatchCriteria);
+        logDebug("**THIRD** " + capId);
 } 
