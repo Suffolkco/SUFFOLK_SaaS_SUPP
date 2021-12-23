3f4452c4fd496a521437ba0a7861c52b516c9445
Ada <ada.chan@suffolkcountyny.gov>
2021-12-23 10:49:35 -0500

    EHIMS-4609: Request modify workflow to include renewal process for OPC application types.

---
+++ Scripts/Event/PRA;DEQ!OPC!GLOBAL CONTAINMENT!APPLICATION.js
@@ -0,0 +1,224 @@
+//PRA:DEQ/OPC/Global Containment/Application
+var showDebug = true;
+
+if (publicUser)
+{     
+    var appStatus = getAppStatus(capId);
+
+    if (balanceDue <= 0)
+    {
+        if (isTaskActive("Inspection") || isTaskActive("Final Review"))
+        {
+            if (isTaskActive("Inspection"))
+            {
+                updateTask("Inspection", "Permit Renewed", "", "");        
+                updateAppStatus("Plan Approved");
+            }
+            else if (isTaskActive("Final Review"))
+            {
+                updateTask("Final Review", "Resubmitted", "", "");       
+                updateAppStatus("Final Review");
+            }
+
+            var b1ExpResult = aa.expiration.getLicensesByCapID(capId);
+        
+            if (b1ExpResult.getSuccess())
+            {
+                b1Exp = b1ExpResult.getOutput();
+                var newDate = new Date();
+                var newExpDate = (newDate.getMonth() + 1) + "/" + newDate.getDate() + "/" + (newDate.getFullYear() + 1);       
+                logDebug("This is the current month: " + newDate.getMonth());
+    
+                newIndExpDateOne = aa.date.parseDate(newExpDate);
+                b1Exp.setExpDate(newIndExpDateOne);
+                b1Exp.setExpStatus("Active");
+                aa.expiration.editB1Expiration(b1Exp.getB1Expiration());        
+            }        
+        }
+    }
+}
+
+function logDebug(dstr) {
+    vLevel = 1
+    if (arguments.length > 1)
+        vLevel = arguments[1];
+    if ((showDebug & vLevel) == vLevel || vLevel == 1)
+        debug += dstr + br;
+    if ((showDebug & vLevel) == vLevel)
+        aa.debug(aa.getServiceProviderCode() + " : " + aa.env.getValue("CurrentUserID"), dstr);
+}
+
+function getAppStatus() {
+	var itemCap = capId;
+	if (arguments.length == 1) itemCap = arguments[0]; // use cap ID specified in args
+
+	var appStatus = null;
+   var capResult = aa.cap.getCap(itemCap);
+   if (capResult.getSuccess()) {
+      licCap = capResult.getOutput();
+      if (licCap != null) {
+         appStatus = "" + licCap.getCapStatus();
+      }
+   } else {
+		logDebug("ERROR: Failed to get app status: " + capResult.getErrorMessage());
+	}
+	return appStatus;
+}
+
+
+
+
+//WTUA:DEQ/OPC/SWIMMING POOL/APPLICATION
+/* As per Xiaoyu on 01/29/2020, we don't use this for swimming pool applicaiton 
+var showDebug = false;
+
+var workflowResult = aa.workflow.getTasks(capId);
+var swimPools = loadASITable("SWIMMING POOL INFORMATION");
+var totalPools = swimPools.length;
+var indoorArray = new Array();
+var outdoorArray = new Array();
+var adHocArray = new Array();
+var b1ExpResult = aa.expiration.getLicensesByCapID(capId);
+
+if (balanceDue <= 0)
+{
+    if (b1ExpResult.getSuccess())
+    {
+        b1Exp = b1ExpResult.getOutput();
+        var newDate = new Date();
+        var todDateCon = (newDate.getMonth() + 1) + "/" + newDate.getDate() + "/" + (newDate.getFullYear());
+        //logDebug("This is the current month: " + newDate.getMonth());
+        //logDebug("This is the current day: " + newDate.getDate());
+        //logDebug("This is the current year: " + newDate.getFullYear());
+        var dateAdd = addDays(todDateCon, 365);
+        var dateMMDDYYY = jsDateToMMDDYYYY(dateAdd);
+
+        var year = newDate.getFullYear();
+        var yearOne = newDate.getFullYear() + 1;
+        var yearTwo = newDate.getFullYear() + 2;
+
+        var newIndExpDate = new Date("12/31" + "/" + year);
+
+        var newIndExpDateOne = (newIndExpDate.getMonth() + 1) + "/" + newIndExpDate.getDate() + "/" + (newIndExpDate.getFullYear() + 1);
+
+        var newIndExpDateTwo = (newIndExpDate.getMonth() + 1) + "/" + newIndExpDate.getDate() + "/" + (newIndExpDate.getFullYear() + 2);
+
+        var newOutExpDate = new Date("9/30" + "/" + year);
+
+        var newOutExpDateOne = (newOutExpDate.getMonth() + 1) + "/" + newOutExpDate.getDate() + "/" + (newOutExpDate.getFullYear() + 1);
+
+        var newOutExpDateTwo = (newOutExpDate.getMonth() + 1) + "/" + newOutExpDate.getDate() + "/" + (newOutExpDate.getFullYear() + 2);
+    }
+    //logDebug("This is how many rows my table has: " + totalPools);
+    if(totalPools != null && totalPools >= 1)
+    {
+        for(p in swimPools)
+        {
+            var poolLoc = swimPools[p]["Location"];
+            if (poolLoc == "Indoor")
+            {
+                //logDebug("Indoor pool!");
+                indoorArray.push(poolLoc);
+            }
+            else
+            {
+                //logDebug("Outdoor pool!");
+                outdoorArray.push(poolLoc);
+            }
+        }
+    }
+    if (workflowResult.getSuccess())
+	{
+		var wfObj = workflowResult.getOutput();
+	}
+	else
+	{ 
+		logDebug("**ERROR: Failed to get workflow object"); 
+	}
+	for (i in wfObj)
+	{
+		fTask = wfObj[i];
+        if (fTask.getTaskDescription().equals("Operation Review"))
+        {
+            adHocArray.push(fTask);
+        }
+    }
+    for (h in adHocArray)
+    {
+        var latestAdhoc = adHocArray.slice(-1)[0];
+        //logDebug("My latest adhoc task is: " + latestAdhoc);
+        if (latestAdhoc.getDisposition() != null && latestAdhoc.getDisposition().equals("Complete") && latestAdhoc.getCompleteFlag() == "Y")
+        {
+            //logDebug("This is how many pools are in my indoor array: " + indoorArray.length);
+            //logDebug("This is how many pools are in my outdoor array: " + outdoorArray.length);
+            if (indoorArray.length >=1 && outdoorArray.length == 0)
+            {
+                if (isEven(year))
+                {
+                    logDebug("We have only indoor pools, updating epiration date to 12/31" + "/" + yearTwo);
+                    newIndExpDateTwo = aa.date.parseDate(newIndExpDateTwo);
+                    b1Exp.setExpDate(newIndExpDateTwo);
+                    b1Exp.setExpStatus("Active");
+                    aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
+                }
+                else
+                {
+                    logDebug("We have only indoor pools, updating epiration date to 12/31" + "/" + yearOne);
+                    newIndExpDateOne = aa.date.parseDate(newIndExpDateOne);
+                    b1Exp.setExpDate(newIndExpDateOne);
+                    b1Exp.setExpStatus("Active");
+                    aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
+                }
+            }
+            if (outdoorArray.length >=1 && indoorArray.length == 0)
+            {
+                if (isOdd(year))
+                {
+                    logDebug("We have only outdoor pools, updating epiration date to 9/30" + "/" + yearTwo);
+                    newOutExpDateTwo = aa.date.parseDate(newOutExpDateTwo);
+                    b1Exp.setExpDate(newOutExpDateTwo);
+                    b1Exp.setExpStatus("Active");
+                    aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
+                }
+                else
+                {
+                    logDebug("We have only outdoor pools, updating epiration date to 9/30" + "/" + yearOne);
+                    newOutExpDateOne = aa.date.parseDate(newOutExpDateOne);
+                    b1Exp.setExpDate(newOutExpDateOne);
+                    b1Exp.setExpStatus("Active");
+                    aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
+                }
+            }    
+        }
+    break;	
+	}
+}
+function isOdd(n) 
+{
+   return Math.abs(n % 2) == 1;
+}
+function isEven(n) 
+{
+    return n % 2 == 0;
+}
+function addDays(date, days) 
+{
+	var result = new Date(date);
+	result.setDate(result.getDate() + days);
+	return result;
+}
+function jsDateToMMDDYYYY(pJavaScriptDate) {
+	//converts javascript date to string in MM/DD/YYYY format
+	if (pJavaScriptDate != null) {
+		if (Date.prototype.isPrototypeOf(pJavaScriptDate)) {
+			return (pJavaScriptDate.getMonth() + 1).toString() + "/" + pJavaScriptDate.getDate() + "/" + pJavaScriptDate.getFullYear();
+		} else {
+			logDebug("Parameter is not a javascript date");
+			return ("INVALID JAVASCRIPT DATE");
+		}
+	} else {
+		logDebug("Parameter is null");
+		return ("NULL PARAMETER VALUE");
+	}
+}
+*/
