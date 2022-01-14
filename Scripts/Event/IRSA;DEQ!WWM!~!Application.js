b6f2f2d613d0181c3b007c7e15796d049f11a463
Ada <ada.chan@suffolkcounty.gov>
2022-01-13 19:11:09 -0500

    Fixed request date

--- Scripts/Event/IRSA;DEQ!WWM!~!Application.js
+++ Scripts/Event/IRSA;DEQ!WWM!~!Application.js
@@ -90,8 +90,16 @@ if (itemCapType == "DEQ/WWM/Residence/Application" ||
                         var sysDate = aa.date.getCurrentDate();
                         var sysDateMMDDYYYY = dateFormatted(sysDate.getMonth(), sysDate.getDayOfMonth(), sysDate.getYear(), "MM/DD/YYYY");
                         logDebug("Current Date: " + sysDateMMDDYYYY);                       
-                        inspObj.setRequestDate(sysDate);
-                        logDebug("Retrieve request date: "+ inspObj.getRequestDate());     
+                        inspObj.setRequestDate(aa.date.parseDate(sysDateMMDDYYYY));
+                     
+                        logDebug ("Request Date Year: " + inspObj.getRequestDate().getYear());
+                        logDebug ("Request Date Month: " + inspObj.getRequestDate().getMonth());    
+                        logDebug ("Request Date Day: " + inspObj.getRequestDate().getDayofMonth());             
+                        logDebug ("Request Date Hour: " + inspObj.getRequestDate().getHourOfDay());
+                        logDebug ("Request Date getMinute: " + inspObj.getRequestDate().getMinute());
+                        logDebug ("Request Date getSecond: " + inspObj.getRequestDate().getSecond());
+
+
                         var fromInspEntityId = capId + "-" + inspId;
                         var newInspEntityId = capId + "-" + newInspId;
                         copyInspectionDocuments(fromInspEntityId, newInspEntityId);
