8a9fa6eb12f0a8b10773906f07ed504c991afe19
Ada <ada.chan@suffolkcountyny.gov>
2022-01-18 09:41:11 -0500

    Removed code.

--- Scripts/Event/ISA;DEQ!WWM!~!Application.js
+++ Scripts/Event/ISA;DEQ!WWM!~!Application.js
@@ -1,1 +1,16 @@
 //ISA;DEQ!WWM!~!Application
+showDebug = true;
+// Not used anymore
+/*
+inspId 566190   // is the new inspection
+inspInspector =
+InspectorFirstName = null
+InspectorMiddleName = null
+InspectorLastName = null
+inspGroup = WWM_INSP
+inspType = WWM_RES_System 1
+inspSchedDate = 1/18/2022 */
+
+
+
+
