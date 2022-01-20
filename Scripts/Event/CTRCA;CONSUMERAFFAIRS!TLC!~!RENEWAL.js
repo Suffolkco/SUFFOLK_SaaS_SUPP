bdb1e995502c426140a930cdba76526c9056fc85
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-01-20 11:16:44 -0500

    Updates

--- Scripts/Event/CTRCA;CONSUMERAFFAIRS!TLC!~!RENEWAL.js
+++ Scripts/Event/CTRCA;CONSUMERAFFAIRS!TLC!~!RENEWAL.js
@@ -3,7 +3,7 @@ aa.cap.updateAccessByACA(capId, "Y");
 
 include("CA_REN_TO_LIC_SUBMITTAL");
 
-if (!publicUser)
+/*if (!publicUser)
 {
     addFee("LIC_REN_01", "CA_LIC_REN", "FINAL", 1, "Y")
-}
+}*/
