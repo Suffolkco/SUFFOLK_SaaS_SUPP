67295053a502b81f5936d424284650af720e0579
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-01-26 14:31:44 -0500

    Updates

--- Scripts/Event/CTRCA;CONSUMERAFFAIRS!~!~!~.js
+++ Scripts/Event/CTRCA;CONSUMERAFFAIRS!~!~!~.js
@@ -5,36 +5,40 @@
 // copy Vendor address to record address
 
 try{
-	var contactType = "Vendor";
-	var vendorInfo = getVendorInfo(contactType, capId);
-	if(vendorInfo == false){
-		logDebug("No vendor contact exists on this record");
-	}else{
-		// copy Vendor address to record address
-		var vAddrLine1 = vendorInfo[0];
-		var vCity = vendorInfo[1];
-		var vState = vendorInfo[2];
-		var vZip = vendorInfo[3];
-		var vAddress = new Array();
-		vAddress.push(vAddrLine1);
-		vAddress.push(vCity);
-		vAddress.push(vState);
-		vAddress.push(vZip);
-		createNewAddress(vAddress);
-		
-		// copy Vendor name, org name & phone to short notes
-		var fName = vendorInfo[4];
-		var lName = vendorInfo[5];
-		var vbusiness = vendorInfo[6];
-		var vPhone = vendorInfo[7];
-		if(matches(vPhone, null, " ")){
-			vPhone = " ";
-		}
-		var shortNotesString = fName + " " + lName + ", " + vbusiness + ", " + vPhone;
-		updateShortNotes(shortNotesString); 
-	}
+    var newCap = checkTypeAndRename(capId);
+    if (newCap){
+    logDebug("new renewal CAP ID: " + newCap.getCustomID());
+    }
+   var contactType = "Vendor";
+   var vendorInfo = getVendorInfo(contactType, capId);
+   if(vendorInfo == false){
+       logDebug("No vendor contact exists on this record");
+   }else{
+       // copy Vendor address to record address
+       var vAddrLine1 = vendorInfo[0];
+       var vCity = vendorInfo[1];
+       var vState = vendorInfo[2];
+       var vZip = vendorInfo[3];
+       var vAddress = new Array();
+       vAddress.push(vAddrLine1);
+       vAddress.push(vCity);
+       vAddress.push(vState);
+       vAddress.push(vZip);
+       createNewAddress(vAddress);
+
+       // copy Vendor name, org name & phone to short notes
+       var fName = vendorInfo[4];
+       var lName = vendorInfo[5];
+       var vbusiness = vendorInfo[6];
+       var vPhone = vendorInfo[7];
+       if(matches(vPhone, null, " ")){
+           vPhone = " ";
+       }
+       var shortNotesString = fName + " " + lName + ", " + vbusiness + ", " + vPhone;
+       updateShortNotes(shortNotesString);
+   }
 }catch(err){
-	logDebug("**WARN: Error in CTRCA updating short notes and address -  " + err.message);
+   logDebug("**WARN: Error in CTRCA updating short notes and address -  " + err.message);
 }
 
 //var newCap = checkTypeAndRename(capId);
@@ -46,16 +50,16 @@ try{
 //function getVendorInfo(cType, capId) {
 //	var returnArray = new Array();
 //	var haveCType = false;
-//	
-//	var contModel = null; 
-//	var consResult = aa.people.getCapContactByCapID(capId);	
+//
+//	var contModel = null;
+//	var consResult = aa.people.getCapContactByCapID(capId);
 //	if (consResult.getSuccess()) {
 //		var cons = consResult.getOutput();
 //		for (thisCon in cons) {
 //			var capContactType = cons[thisCon].getCapContactModel().getPeople().getContactType();
-//			if (capContactType == cType) {				
-//				var contModel = cons[thisCon].getCapContactModel(); 
-//				
+//			if (capContactType == cType) {
+//				var contModel = cons[thisCon].getCapContactModel();
+//
 //				var firstName = contModel.getFirstName();
 //				var lastName = contModel.getLastName();
 //				var business = contModel.getBusinessName();
@@ -64,7 +68,7 @@ try{
 //				var city = contModel.getCity();
 //				var state = contModel.getState();
 //				var zip = contModel.getZip();
-//				
+//
 //				// build returnArray
 //				returnArray.push(addr1);
 //				returnArray.push(city);
@@ -89,12 +93,12 @@ try{
 //	var newCity = address[1];
 //	var newState = address[2];
 //	var newZip = address[3];
-//	
+//
 //	var newAddressModel = aa.proxyInvoker.newInstance("com.accela.aa.aamain.address.AddressModel").getOutput();
 //	newAddressModel.setCapID(capId);
 //	newAddressModel.setServiceProviderCode(aa.getServiceProviderCode());
 //	newAddressModel.setAuditID("ADMIN");
-//	newAddressModel.setPrimaryFlag("Y"); 
+//	newAddressModel.setPrimaryFlag("Y");
 //
 //// per customer - add address to BOTH AddressLine1 and StreetName
 //	newAddressModel.setAddressLine1(newAddr1);
@@ -107,12 +111,13 @@ try{
 //	//logDebug("Added record address " + newAddr1 + ", " + newCity + ", " + newState + ", " + newZip + " successfully!");
 //}
 
-/*function checkTypeAndRename(capIdObj) {
-    var title = "checkTypeAndRenew(): ";
+function checkTypeAndRename(capIdObj)
+{
+    var title = "checkTypeAndRename(): ";
     var newRecObj = null;
 
     var typesObj = {
-        "Building/Residential/Addition/NA": true,
+
         "ConsumerAffairs/ID Cards/Backflow Tester/Renewal": true,
         "ConsumerAffairs/Licenses/Appliance Repair/Renewal": true,
         "ConsumerAffairs/Licenses/Commercial Paint/Renewal": true,
@@ -125,37 +130,51 @@ try{
         "ConsumerAffairs/Licenses/Master Plumber/Renewal": true,
         "ConsumerAffairs/Licenses/Pet Cemetary/Renewal": true,
         "ConsumerAffairs/Licenses/Polygraph Examiner/Renewal": true,
-        "ConsumerAffairs/Licenses/Precious Metal/Renewal": true,
+       "ConsumerAffairs/Licenses/Precious Metal/Renewal": true,
         "ConsumerAffairs/Licenses/Restricted Electrical/Renewal": true,
         "ConsumerAffairs/Licenses/Restricted Plumbing/Renewal": true,
         "ConsumerAffairs/Licenses/Second Hand Dealer/Renewal": true,
         "ConsumerAffairs/Licenses/Sign Hanger/Renewal": true,
         "ConsumerAffairs/Licenses/Tax Grievance/Renewal": true,
         "ConsumerAffairs/Registrations/Swimming Pool Maintenance/Renewal": true,
+        "ConsumerAffairs/Registrations/Renewal/NA": true,
         "ConsumerAffairs/TLC/Drivers/Renewal": true,
         "ConsumerAffairs/TLC/Renewal/NA": true,
-        "ConsumerAffairs/TLC/Vehicles/Renewal": true
+        "ConsumerAffairs/TLC/Vehicles/Renewal": true,
     };
 
-    var getCapResult = aa.cap.getCap(capIdObj);
+    var getCapResult = {};
+    try
+    {
+        getCapResult = aa.cap.getCap(capIdObj);
+    } catch (err)
+    {
+        logDebug(title + "Error in EMSE API: " + err.message);
+        return newRecObj;
+    }
+
     var capObj = {};
-    if (!getCapResult.getSuccess()) {
+   if (!getCapResult.getSuccess())
+    {
         logDebug(title + "Unable to retrieve CAP object. Message = " + getCapResult.getErrorMessage());
         return newRecObj;
     }
     capObj = getCapResult.getOutput();
     var capType = capObj.getCapType();
 
-    if (typesObj[capType]) {
+    if (typesObj[capType])
+    {
         newRecObj = renamePermit(capIdObj);
-    } else {
+    } else
+    {
         logDebug(title + "Wrong record type to renew.");
     }
 
     return newRecObj;
 }
 
-function renamePermit(capIdObj) {
+function renamePermit(capIdObj)
+{
     var title = "renamePermit(): ";
 
     var capIdString = capIdObj.getCustomID();
@@ -164,7 +183,8 @@ function renamePermit(capIdObj) {
     //get the cap object
     var getCapResult = aa.cap.getCap(capIdObj);
     var capObj = {};
-    if (!getCapResult.getSuccess()) {
+    if (!getCapResult.getSuccess())
+    {
         logDebug(title + "Unable to retrieve CAP object. Message = " + getCapResult.getErrorMessage());
         return retval;
     }
@@ -174,7 +194,8 @@ function renamePermit(capIdObj) {
 
     //retrieve parent cap for the same record type
     var parentCapObj = getParentLicense(capIdObj);
-    if (parentCapObj == null) {
+    if (parentCapObj == null)
+    {
         logDebug(title + "No parent license available for record (" + capIdObj + ")");
         return retval;
     }
@@ -182,57 +203,80 @@ function renamePermit(capIdObj) {
 
     var seqArr = [];
     var childrenArr = getChildren(appTypeString, parentCapObj);
-    if (childrenArr) {
-        for (var cIdx in childrenArr) {
-            if (childrenArr[cIdx].getCustomID().indexOf(parentIdString + '-REN') > -1) {
+    exploreObject(childrenArr);
+    if (childrenArr)
+    {
+        for (var cIdx in childrenArr)
+        {
+            if (childrenArr[cIdx].getCustomID().indexOf(parentIdString + '-REN') > -1)
+            {
                 var seqVal = parseInt(childrenArr[cIdx].getCustomID().replace(parentIdString + '-REN', ''));
-                if (!isNaN(seqVal)) {
+                if (!isNaN(seqVal))
+                {
                     seqArr.push(seqVal);
                 }
             }
         }
     }
-
-    var childNewAltId = capIdObj.getCustomID() + '-REN01';
-    if (seqArr.length > 0) {
-        var lastSeq = seqArr.sort(function (a, b) {
+    var childNewAltId = parentCapObj.getCustomID() + '-REN01';
+    if (seqArr.length > 0)
+    {
+        var lastSeq = seqArr.sort(function (a, b)
+        {
             return b - a;
         })[0] + 1;
-        childNewAltId = capIdObj.getCustomID() + '-REN' + (String(lastSeq).length > 1 ? lastSeq : '0' + String(lastSeq));
+        childNewAltId = parentCapObj.getCustomID() + '-REN' + (String(lastSeq).length > 1 ? lastSeq : '0' + String(lastSeq));
     }
     var updateCapAltIdResult = aa.cap.updateCapAltID(capIdObj, childNewAltId);
-    if (!updateCapAltIdResult.getSuccess()) {
+    if (!updateCapAltIdResult.getSuccess())
+    {
         logDebug(title + "Error updating record to renewal CAP Id. Message = " + updateCapAltIdResult.getErrorMessage());
         return retval;
     }
 
     var childCapIdResult = aa.cap.getCapID(childNewAltId);
-    if (!childCapIdResult.getSuccess()) {
+    if (!childCapIdResult.getSuccess())
+    {
         logDebug(title + "Unable to retrieve new child CAP Id. Message = " + childCapIdResult.getErrorMessage());
         return retval;
     }
     var childCapId = aa.cap.getCapID(childNewAltId).getOutput();
 
-    return childCapId;    
+    return childCapId;
 }
 
-function getParentLicense(capIdObj) {
+function getParentLicense(capIdObj)
+{
     var title = "getParentLicense(): ";
 
     var parentLicObj = null;
     var parentLicArr = [];
 
     var getProjectResult = aa.cap.getProjectByChildCapID(capIdObj, "Renewal", null);
-    if (!getProjectResult.getSuccess()) {
+    if (!getProjectResult.getSuccess())
+    {
         getProjectResult = aa.cap.getProjectByChildCapID(capIdObj, "EST", null);
     }
 
     parentLicArr = getProjectResult.getOutput();
-    if (parentLicArr != null && parentLicArr.length > 0) {
+    if (parentLicArr != null && parentLicArr.length > 0)
+    {
         parentLicObj = parentLicArr[0];
-    } else {
+    } else
+    {
         logDebug(title + "No parent license for child license (" + capIdObj + ")");
     }
 
-    return parentLicObj;
-}*/
+    var projIdStr = String(parentLicObj.getProjectID());
+    var projIdArr = projIdStr.split('-');
+
+    var parentCapIdObjResult = aa.cap.getCapID(projIdArr[0], projIdArr[1], projIdArr[2]);
+    if (!parentCapIdObjResult.getSuccess())
+    {
+        logDebug(title + "Unable to retrieve the parent CapID object.");
+        return null;
+    }
+    var parentCapIdObj = parentCapIdObjResult.getOutput();
+
+    return parentCapIdObj;
+}
