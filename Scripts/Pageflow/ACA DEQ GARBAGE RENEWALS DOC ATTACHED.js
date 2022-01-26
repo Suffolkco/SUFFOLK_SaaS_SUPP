f0e7835e2323fadd4b1fbce8b1250fbeaf2b5896
Ada <ada.chan@suffolkcountyny.gov>
2022-01-26 12:59:15 -0500

    EHIMS-4785: User Uploaded Documents but they do not Show up in Back Office

---
+++ Scripts/Pageflow/ACA DEQ GARBAGE RENEWALS DOC ATTACHED.js
@@ -0,0 +1,113 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program : ACA DEQ GARBAGE RENEWALS DOC ATTACHED.js
+| Event   : ACA_BEFORE Event
+|
+|
+| 
+| Author:jgreene
+/------------------------------------------------------------------------------------------------------*/
+
+var documentOnly = false;
+var disableTokens = false;
+var useAppSpecificGroupName = false;
+var useTaskSpecificGroupName = false;
+var enableVariableBranching = false;
+var maxEntries = 99;
+var cancel = false;
+var startDate = new Date();
+var startTime = startDate.getTime();
+var message = "";
+var debug = "";
+var br = "<br>";
+var showDebug = false;
+var showMessage = false;
+//Needed to load the includes files
+//This way we are not re-defining everything over and over.
+eval(getScriptText("INCLUDES_CUSTOM", null, true));
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, true));
+var cap = aa.env.getValue("CapModel");
+var capId = cap.getCapID();
+var AInfo = new Array();
+loadAppSpecific4ACA(AInfo);
+try
+{
+    var docComments = "";
+     if (!determineACADocumentAttached("Vehicle Registration"))
+    {
+        docComments += "Vehicle Registration" + "<br>";
+    }
+    if (!determineACADocumentAttached("Workers Comp Insurance"))
+    {
+        docComments += "Workers Comp Insurance." + "<br>";
+    }
+    if (!determineACADocumentAttached("Disposal Site Authorization"))
+    {
+        docComments += "Disposal Site Authorization" + "<br>";
+    }
+
+    if (!determineACADocumentAttached("Workers Comp Insurance"))
+    {
+        docComments += "Workers Comp Insurance" + "<br>";
+    }
+
+    if (!determineACADocumentAttached("Vehicle Insurance"))
+    {
+        docComments += "Vehicle Insurance" + "<br>";
+    }
+
+
+    if (docComments != "") 
+    {
+        cancel = true;
+        showMessage = true;
+        comment("This submission requires you to submit the following documents: <br>" + docComments);
+    }
+}
+catch (error)
+{
+    logDebug("an error was encoutered: " + error.message);
+    showDebug = true;
+    showMessage = true;
+}
+
+if (debug.indexOf("**ERROR") > 0 || debug.substr(0, 7) == "**ERROR")
+{
+    aa.env.setValue("ErrorCode", "1");
+    aa.env.setValue("ErrorMessage", debug);
+} else
+{
+    if (cancel)
+    {
+        aa.env.setValue("ErrorCode", "-2");
+        if (showMessage) aa.env.setValue("ErrorMessage", message);
+        if (showDebug) aa.env.setValue("ErrorMessage", debug);
+    } else
+    {
+        aa.env.setValue("ErrorCode", "0");
+        if (showMessage) aa.env.setValue("ErrorMessage", message);
+        if (showDebug) aa.env.setValue("ErrorMessage", debug);
+    }
+}
+
+
+//ACA Functions 
+function getScriptText(vScriptName, servProvCode, useProductScripts)
+{
+    if (!servProvCode) servProvCode = aa.getServiceProviderCode();
+    vScriptName = vScriptName.toUpperCase();
+    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+    try
+    {
+        if (useProductScripts)
+        {
+            var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+        } else
+        {
+            var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+        }
+        return emseScript.getScriptText() + "";
+    } catch (err)
+    {
+        return "";
+    }
+}
