85146b93b73da62626f940778ac79ccc3df00f64
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-02-09 14:04:51 -0500

    Update

--- Scripts/Event/ISA;DEQ!WWM!~!Application.js
+++ Scripts/Event/ISA;DEQ!WWM!~!Application.js
@@ -11,67 +11,71 @@ inspGroup = WWM_INSP
 inspType = WWM_RES_System 1
 inspSchedDate = 1/18/2022 */
 
-
-logDebug("inspection type =" + inspType);
-
-if (inspType == "WWM_RES_System 1")
-{
-    logDebug("current insp ID is: " + inspId);
-    var insArray = inspectionResult.getOutput();
-    var inspModel = insArray[insArray.length - 1].getInspection();
-    logDebug("inspModel is: " + inspModel);
-    //getting guidesheets from inspection being scheduled
-    var gs = inspModel.getGuideSheets();
-    if (!gs.isEmpty())
-    {
-        //looping through those guidesheets and removing any that exist before inspection is done being scheduled
-        for (var gLoop = 0; gLoop < gs.size(); gLoop++)
-        {
-            var guideSheetObj = gs.get(gLoop);
-            var guideSeq = guideSheetObj.getGuidesheetSeqNbr();
-            logDebug("Hitting guidesheet number:" + guideSeq);
-            var guideBiz = aa.proxyInvoker.newInstance("com.accela.aa.inspection.guidesheet.GGuideSheetBusiness").getOutput();
-            guideBiz.removeGGuideSheet("SUFFOLKCO", guideSeq, "ADMIN");
-            continue;
-        }
+var inspectionResult = aa.inspection.getInspections(capId);
+if (inspectionResult.getSuccess()) {
+    if (inspectionResult.getOutput().length >= 2) {
+        lastInsp = inspectionResult.getOutput()[inspectionResult.getOutput().length - 2];
+        lastInsp = lastInsp.getIdNumber();
     }
-    else
-    {
-        logDebug("no guidesheets here, somehow...");
+}
+
+
+
+logDebug("this is the script that is running");
+copyAllGuidesheets(lastInsp, inspId, capId, true);
+logDebug("inspResult =" + inspResult);
+
+function copyAllGuidesheets(inspId1, inspId2) { 
+    /*
+    Will copy all guidesheets (1:1) from inspId1 to inspId2 on same record
+    Not using aa.inspection.getInspection() because the .getGuideSheets() always returns null
+	Author CGray
+	@param inspId1 {Number} Inspection sequence number of source
+	@param inspId2 {Number} Inspection sequence number of target
+    Optional {object} Source record
+    Optional {boolean} to remove all existing guidesheets from target
+    */
+    var itemCap = capId;
+    var rExisting = false;
+    if (arguments.length >= 3) {
+        itemCap = arguments[2];
     }
-
-    //defining our empty item list
-    var itemList = aa.util.newArrayList();
-    //getting inspection on the current record
-    var inspectionResult = aa.inspection.getInspections(capId);
-    if (inspectionResult.getSuccess())
-    {
-        //putting those inspections into an array
-        var insArray = inspectionResult.getOutput();
-        //targeting the most recent previous inspection (not the one currently being scheduled)
-        var prevInsp = insArray[insArray.length - 1].getInspection();
-        //pulling previous inspection's guidesheets
-        var prevGs = prevInsp.getGuideSheets();
-
-        if (prevGs != null)
-        {
-            //passing those guidesheets to an array
-            var prevGsArray = prevGs.toArray();
-            //adding each item from the array into itemList
-            for (gs in prevGsArray)
-            {
-                itemList.add(prevGsArray[gs]);
+    if (arguments.length >= 4) {
+        rExisting = arguments[3];
+    }
+    var inspResult = aa.inspection.getInspections(itemCap);
+    if (inspResult.getSuccess()) {
+        var itemList = aa.util.newArrayList();
+        var inspArray = inspResult.getOutput();
+        for (ins in inspArray) {
+            if (inspArray[ins].getIdNumber() == inspId1) {
+                var guides = inspArray[ins].getInspection().getGuideSheets();
+                if (guides != null) {
+                    var gArray = guides.toArray();
+                    for (gs in gArray) {
+                        itemList.add(gArray[gs]);
+                    }
+                }
             }
-            //copy in the stuff from itemList into the new inspection
-            var copyItems = aa.guidesheet.copyGGuideSheetItems(itemList, capId, inspId, currentUserID);
-            if (copyItems.getSuccess())
-            {
-                logDebug("Copied guidesheet successfully.");
+            else if (inspArray[ins].getIdNumber() == inspId2) {
+                if (rExisting) {
+                    var guides = inspArray[ins].getInspection().getGuideSheets();
+                    if (guides != null) {
+                        var gArray = guides.toArray();
+                        for (gs in gArray) {
+                            aa.print("Removing guidesheet: " + gArray[gs].getGuideType());
+                            var guideBiz = aa.proxyInvoker.newInstance("com.accela.aa.inspection.guidesheet.GGuideSheetBusiness").getOutput();
+                            guideBiz.removeGGuideSheet("FFX", gArray[gs].getGuidesheetSeqNbr(), "ADMIN");
+                        }
+                    }
+                }
             }
         }
-    }
-    else
-    {
-        logDebug("**ERROR: Failed to copy: " + res.getErrorMessage());
+        if (!itemList.isEmpty()) {
+            var copyItems = aa.guidesheet.copyGGuideSheetItems(itemList, itemCap, inspId2, currentUserID);
+            if (copyItems.getSuccess()) {
+                aa.print("Copied guidesheet successfully.");
+            }
+        }
     }
 }
