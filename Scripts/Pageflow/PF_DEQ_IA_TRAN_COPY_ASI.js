6563f09689e4eec65f173723056a213c8f32cdc3
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-02-24 09:15:56 -0500

    Updates

--- Scripts/Pageflow/PF_DEQ_IA_TRAN_COPY_ASI.js
+++ Scripts/Pageflow/PF_DEQ_IA_TRAN_COPY_ASI.js
@@ -66,9 +66,7 @@ function copy()
     {
         var AInfo = new Array();
         var iaNumber = AInfo["IA Record Number"];
-        iaCapResult = aa.cap.getCapID(iaNumber);
-        logDebugLocal("iaCapResult: " + iaCapResult);
-        var iaCapID = iaCapResult.getOutput();
+        var iaCapID = aa.cap.getCapID(iaNumber);
 
         //copyAppSpecificTableForDEQ(parentCapId, targetCapId);
 
@@ -81,6 +79,8 @@ function copy()
         copyParcel(iaCapID, targetCapId);
 
         copyOwner(iaCapID, targetCapId);
+
+        copyContactsByType(iaCapID, targetCapId, ["Property Owner"])
 
         var amendCapModel = aa.cap.getCapViewBySingle4ACA(targetCapId);
 
@@ -1734,4 +1734,53 @@ function copyLicenseProfessionalCustomPageflow(srcCapId, targetCapId)
         aa.licenseProfessional.createLicensedProfessional(sourcelicProfModel);
         aa.print("Copied successfully!");
     }
+}
+function copyContactsByType(srcCapId, targetCapId, ContactTypeArr) {
+	//1. Get people with source CAPID.
+	var capPeoples = getPeople3_0(srcCapId);
+	if (capPeoples == null || capPeoples.length == 0) {
+		return;
+	}
+	//2. Get people with target CAPID.
+	var targetPeople = getPeople3_0(targetCapId);
+	//3. Check to see which people is matched in both source and target.
+	for (loopk in capPeoples) {
+		sourcePeopleModel = capPeoples[loopk];
+		//Check if contact type should be ignored
+		doCopy = false;
+		for (kk in ContactTypeArr) {
+			if (ContactTypeArr[kk] == sourcePeopleModel.getCapContactModel().getContactType())
+				doCopy = true;
+		}
+		if (doCopy) {
+			//3.1 Set target CAPID to source people.
+			sourcePeopleModel.getCapContactModel().setCapID(targetCapId);
+			targetPeopleModel = null;
+			//3.2 Check to see if sourcePeople exist.
+			if (targetPeople != null && targetPeople.length > 0) {
+				for (loop2 in targetPeople) {
+					if (isMatchPeople3_0(sourcePeopleModel, targetPeople[loop2])) {
+						targetPeopleModel = targetPeople[loop2];
+						break;
+					}
+				}
+			}
+			//3.3 It is a matched people model.
+			if (targetPeopleModel != null) {
+				//3.3.1 Copy information from source to target.
+				aa.people.copyCapContactModel(sourcePeopleModel.getCapContactModel(), targetPeopleModel.getCapContactModel());
+				//3.3.2 Copy contact address from source to target.
+				if (targetPeopleModel.getCapContactModel().getPeople() != null && sourcePeopleModel.getCapContactModel().getPeople()) {
+					targetPeopleModel.getCapContactModel().getPeople().setContactAddressList(sourcePeopleModel.getCapContactModel().getPeople().getContactAddressList());
+				}
+				//3.3.3 Edit People with source People information.
+				aa.people.editCapContactWithAttribute(targetPeopleModel.getCapContactModel());
+			}
+			//3.4 It is new People model.
+			else {
+				//3.4.1 Create new people.
+				aa.people.createCapContactWithAttribute(sourcePeopleModel.getCapContactModel());
+			}
+		}
+	}
 }
