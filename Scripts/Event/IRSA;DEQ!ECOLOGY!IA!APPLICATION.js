f91e1c451b763a43bcdbffd5716fdfd3d55b4c00
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-03-02 09:26:37 -0500

    Update

--- Scripts/Event/IRSA;DEQ!ECOLOGY!IA!APPLICATION.js
+++ Scripts/Event/IRSA;DEQ!ECOLOGY!IA!APPLICATION.js
@@ -53,3 +53,81 @@ var guideBiz = aa.proxyInvoker.newInstance("com.accela.aa.inspection.guidesheet.
             }
         }
     }
+    function getGuidesheetASIField(pInspId, gName, gItem, asiGroup, asiSubGroup, asiLabel)
+{
+    var vInspId = parseFloat(pInspId);
+    var vInspectionActivity;
+    var asiValue = "";
+    var guideBiz;
+    var vGuideSheetArray = [];
+    var vGuideSheet;
+    var vGuideSheetItemsArray = [];
+    var vGuideSheetItem;
+    var vInspection;
+
+    // Get the specific inspection model
+    vInspection = aa.inspection.getInspection(capId, vInspId);
+    if (vInspection.getSuccess())
+    {
+        vInspection = vInspection.getOutput();
+        vInspectionActivity = vInspection.getInspection().getActivity();
+
+        // Get the guidesheets and their items from the activity model
+        guideBiz = aa.proxyInvoker.newInstance("com.accela.aa.inspection.guidesheet.GGuideSheetBusiness").getOutput();
+        vGuideSheetArray = guideBiz.getGGuideSheetWithItemsByInspections("", [vInspectionActivity]).toArray();
+        if (vGuideSheetArray.length != 0)
+        {
+            var x = 0;
+            for (x in vGuideSheetArray)
+            {
+                vGuideSheet = vGuideSheetArray[x];
+                if (gName.toUpperCase() == vGuideSheet.getGuideType().toUpperCase() && vGuideSheet.getItems() != null)
+                {
+                    vGuideSheetItemsArray = vGuideSheet.getItems().toArray();
+                    var z = 0;
+                    for (z in vGuideSheetItemsArray)
+                    {
+                        vGuideSheetItem = vGuideSheetItemsArray[z];
+                        if (vGuideSheetItem && gItem == vGuideSheetItem.getGuideItemText() && asiGroup == vGuideSheetItem.getGuideItemASIGroupName())
+                        {
+                            var ASISubGroups = vGuideSheetItem.getItemASISubgroupList();
+                            if (ASISubGroups)
+                            {
+                                for (var k = 0; k < ASISubGroups.size(); k++)
+                                {
+                                    var ASISubGroup = ASISubGroups.get(k);
+                                    if (ASISubGroup && ASISubGroup.getSubgroupCode() == asiSubGroup)
+                                    {
+                                        var ASIModels = ASISubGroup.getAsiList();
+                                        if (ASIModels)
+                                        {
+                                            for (var m = 0; m < ASIModels.size(); m++)
+                                            {
+                                                var ASIModel = ASIModels.get(m);
+                                                if (ASIModel && ASIModel.getAsiName() == asiLabel)
+                                                {
+                                                    logDebug("ASI value: " + ASIModel.getAttributeValue());
+                                                    asiValue = ASIModel.getAttributeValue();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else
+                {
+                    logDebug("Failed to get guide sheet item");
+                }
+            }
+        } else
+        {
+            logDebug("Failed to get guidesheets");
+        }
+    } else
+    {
+        logDebug("Failed to get inpection");
+    }
+    return asiValue;
+}
