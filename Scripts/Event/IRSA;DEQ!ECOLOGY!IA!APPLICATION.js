134047fba8b96d508872a89325aadeaa2cd6ffe5
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-03-02 09:12:41 -0500

    Updates

---
+++ Scripts/Event/IRSA;DEQ!ECOLOGY!IA!APPLICATION.js
@@ -0,0 +1,52 @@
+var doValue = getGuidesheetASIField(pInspId, "Lab Results", "IA Lab Results", "DEQ_IA_LAB", "FIELD RESULTS", "DO");
+var phValue = getGuidesheetASIField(pInspId, "Lab Results", "IA Lab Results", "DEQ_IA_LAB", "FIELD RESULTS", "PH");
+var wwTemp = getGuidesheetASIField(pInspId, "Lab Results", "IA Lab Results", "DEQ_IA_LAB", "FIELD RESULTS", "WW TEMP");
+var airTemp = getGuidesheetASIField(pInspId, "Lab Results", "IA Lab Results", "DEQ_IA_LAB", "FIELD RESULTS", "Air Temp");
+
+var insp = aa.inspection.getInspection(capId, inspId).getOutput();
+var vInspectionActivity = insp.getInspection().getActivity();
+
+var guideBiz = aa.proxyInvoker.newInstance("com.accela.aa.inspection.guidesheet.GGuideSheetBusiness").getOutput();
+        var vGuideSheetArray = guideBiz.getGGuideSheetWithItemsByInspections("", [vInspectionActivity]).toArray();
+        if (vGuideSheetArray.length != 0)
+        {
+            var x = 0;
+            for (x in vGuideSheetArray)
+            {
+                var vGuideSheet = vGuideSheetArray[x];
+                if ("IA Lab Results".toUpperCase() == vGuideSheet.getGuideType().toUpperCase() && vGuideSheet.getItems() != null)
+                {
+                    vGuideSheetItemsArray = vGuideSheet.getItems().toArray();
+                    var z = 0;
+                    for (z in vGuideSheetItemsArray)
+                    {
+                        var vGuideSheetItem = vGuideSheetItemsArray[z];
+                        if (vGuideSheetItem && "IA Lab Results" == vGuideSheetItem.getGuideItemText())
+                        {
+                            var labResultsTable = getGuidesheetItemASIT(vGuideSheet, vGuideSheetItem, "LAB RESULTS");
+                            var newLabResultsTable = new Array();
+                            for (var l in labResultsTable)
+                            {
+                                var newRow = new Array();
+                                newRow["Lab ID"] = labResultsTable[l]["Lab ID"];
+                                newRow["TN"] = labResultsTable[l]["TN"];
+                                newRow["NO3 Nitrate"] = labResultsTable[l]["NO3 Nitrate"];
+                                newRow["NO2 Nitrate"] = labResultsTable[l]["NO2 Nitrate"];
+                                newRow["TKN"] = labResultsTable[l]["TKN"];
+                                newRow["NH4 Ammonia"] = labResultsTable[l]["NH4 Ammonia"];
+                                newRow["BOD"] = labResultsTable[l]["BOD"];
+                                newRow["TSS"] = labResultsTable[l]["TSS"];
+                                newRow["ALK"] = labResultsTable[l]["ALK"];
+                                newRow["DO"] = doValue;
+                                newRow["PH"] = phValue;
+                                newRow["WW Temp"] = wwTemp;
+                                newRow["Air Temp"] = airTemp;
+
+                                newLabResultsTable.push(newRow);
+                            }
+                            addASITable("LAB RESULTS", newLabResultsTable, capId);
+                        }
+                    }
+                }
+            }
+        }
