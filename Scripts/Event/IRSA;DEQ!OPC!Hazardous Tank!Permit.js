99b251d231eb654796cff3cf43457a8e320f2b63
Ada <ada.chan@suffolkcountyny.gov>
2022-03-04 15:08:31 -0500

    EHIMS-4805

--- Scripts/Event/IRSA;DEQ!OPC!Hazardous Tank!Permit.js
+++ Scripts/Event/IRSA;DEQ!OPC!Hazardous Tank!Permit.js
@@ -60,11 +60,11 @@ if (inspResult == "Completed" || inspResult == "Fail")
                 
         var inspectionDateCon = month + "/" + day + "/" + year;
         logDebug("Inspection Date: " + month + "/" + day + "/" + year);
-        
+
         reportParams.put("TankRecordID", alternateID.toString());
         reportParams.put("InspectionDate",  inspSchedDate);
             
-		rFile = generateReport("Inspection result Tank Operator", reportParams, 'DEQ')
+		rFile = generateReport1("Inspection result Tank Operator", reportParams, 'DEQ')
         logDebug("This is the rFile: " + rFile);           
         
         if (rFile) {
@@ -83,6 +83,38 @@ if (inspResult == "Completed" || inspResult == "Fail")
     }
 
 } 
+
+function generateReport1(aaReportName,parameters,rModule) {
+	var reportName = aaReportName;
+      
+    report = aa.reportManager.getReportInfoModelByName(reportName);
+    report = report.getOutput();
+  
+    report.setModule(rModule);
+    report.setCapId(capId);
+
+    report.setReportParameters(parameters);
+
+    var permit = aa.reportManager.hasPermission(reportName,currentUserID);
+
+    if(permit.getOutput().booleanValue()) {
+       var reportResult = aa.reportManager.getReportResult(report);
+     
+       if(reportResult) {
+	       reportResult = reportResult.getOutput();
+	       var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+			logMessage("Report Result: "+ reportResult);
+	       reportFile = reportFile.getOutput();
+	       return reportFile
+       } else {
+       		logMessage("Unable to run report: "+ reportName + " for Admin" + systemUserObj);
+       		return false;
+       }
+    } else {
+         logMessage("No permission to report: "+ reportName + " for Admin" + systemUserObj);
+         return false;
+    }
+}
 
 	
 
