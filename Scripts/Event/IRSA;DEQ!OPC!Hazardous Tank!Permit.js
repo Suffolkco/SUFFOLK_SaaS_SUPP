f85c2d7bdb0780f496140d561d20d04667269916
Ada <ada.chan@suffolkcountyny.gov>
2022-03-04 15:20:15 -0500

    Test

--- Scripts/Event/IRSA;DEQ!OPC!Hazardous Tank!Permit.js
+++ Scripts/Event/IRSA;DEQ!OPC!Hazardous Tank!Permit.js
@@ -61,8 +61,12 @@ if (inspResult == "Completed" || inspResult == "Fail")
         var inspectionDateCon = month + "/" + day + "/" + year;
         logDebug("Inspection Date: " + month + "/" + day + "/" + year);
 
+      
+        var retVal = new Date(String(inspectionDateCon));
+           
+
         reportParams.put("TankRecordID", alternateID.toString());
-        reportParams.put("InspectionDate",  inspSchedDate);
+        reportParams.put("InspectionDate",  retVal);
             
 		rFile = generateReport1("Inspection result Tank Operator", reportParams, 'DEQ')
         logDebug("This is the rFile: " + rFile);           
@@ -89,19 +93,21 @@ function generateReport1(aaReportName,parameters,rModule) {
       
     report = aa.reportManager.getReportInfoModelByName(reportName);
     report = report.getOutput();
-  
+    logDebug("This is the report: " + report);           
     report.setModule(rModule);
     report.setCapId(capId);
 
     report.setReportParameters(parameters);
 
     var permit = aa.reportManager.hasPermission(reportName,currentUserID);
-
+    logDebug("This is the permit: " + permit);   
     if(permit.getOutput().booleanValue()) {
        var reportResult = aa.reportManager.getReportResult(report);
      
        if(reportResult) {
 	       reportResult = reportResult.getOutput();
+           logDebug("This is the reportResult: " + reportResult);   
+
 	       var reportFile = aa.reportManager.storeReportToDisk(reportResult);
 			logMessage("Report Result: "+ reportResult);
 	       reportFile = reportFile.getOutput();
