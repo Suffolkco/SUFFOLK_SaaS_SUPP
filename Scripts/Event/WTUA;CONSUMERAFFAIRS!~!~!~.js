fbfe23c097aed68ea4f1443ccc2276c56340d5f8
Ada <ada.chan@suffolkcountyny.gov>
2022-03-08 17:06:33 -0500

    DAP-362: When backoffice user updates the workflow, it will automatically update the status and expiration dates.

--- Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
+++ Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
@@ -15,38 +15,23 @@ if (matches(appTypeArray[1], "Registrations", "Licenses")) {
 	{
 		var expDateASI = getAppSpecific("Expiration Date", capId);
 
-		//Updating Expiration Date of License
-		if (expDateASI != null)
-		{
-			logDebug("ASI Expdate is: " + expDateASI);
-			expDateASI = new Date(expDateASI);
-			logDebug("New Date Exp Date is: " + expDateASI)
-			var newExpDate = (expDateASI.getMonth() + 1) + "/" + 1 + "/" + (expDateASI.getFullYear() + 2);
-			logDebug("New Exp Date is: " + newExpDate);
-			editAppSpecific("Expiration Date", newExpDate, capId);
-			var b1ExpResult = aa.expiration.getLicensesByCapID(capId);
-			if (b1ExpResult.getSuccess())
-			{
-				var b1Exp = b1ExpResult.getOutput();
-				b1Exp.setExpStatus("Active");
-				b1Exp.setExpDate(aa.date.parseDate(newExpDate));
-				aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
-						
-			}
-
+		//Updating Expiration Date of License	
+		logDebug("Current ASI Expdate is: " + expDateASI);
+		
 		var today = new Date();
-        logDebug("today's date is " + today);
-        var nullExpDate = (today.getMonth() + 1) + "/" + 1 + "/" + (today.getFullYear() + 2);
-        logDebug("null date is " + nullExpDate);
-        editAppSpecific("Expiration Date", nullExpDate, capId);
-        var b1ExpResult = aa.expiration.getLicensesByCapID(capId);
-        if (b1ExpResult.getSuccess())
-        {
-            var b1Exp = b1ExpResult.getOutput();
-            b1Exp.setExpStatus("Active");
-            b1Exp.setExpDate(aa.date.parseDate(nullExpDate));
-            aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
-        } 
+		logDebug("today's date is " + today);
+		var nullExpDate = (today.getMonth() + 1) + "/" + 1 + "/" + (today.getFullYear() + 2);
+		logDebug("New expiration date is " + nullExpDate);
+		editAppSpecific("Expiration Date", nullExpDate, capId);
+		var b1ExpResult = aa.expiration.getLicensesByCapID(capId);
+		if (b1ExpResult.getSuccess())
+		{
+			var b1Exp = b1ExpResult.getOutput();
+			b1Exp.setExpStatus("Active");
+			b1Exp.setExpDate(aa.date.parseDate(nullExpDate));
+			aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
+		} 
+		
 	}
 
 } 
