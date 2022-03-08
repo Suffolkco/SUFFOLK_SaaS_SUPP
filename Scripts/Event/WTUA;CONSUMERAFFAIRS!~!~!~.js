1d11d3dbfc5b9bae9162b99cdb1295ce2b1c193b
Ada <ada.chan@suffolkcountyny.gov>
2022-03-08 17:20:36 -0500

    DAP-362

--- Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
+++ Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
@@ -21,17 +21,33 @@ if (matches(appTypeArray[1], "Registrations", "Licenses")) {
 		var today = new Date();
 		logDebug("today's date is " + today);
 		var nullExpDate = (today.getMonth() + 1) + "/" + 1 + "/" + (today.getFullYear() + 2);
-		logDebug("New expiration date is " + nullExpDate);
-		editAppSpecific("Expiration Date", nullExpDate, capId);
+
+		var newExpDate = formatDate(nullExpDate);
+		logDebug("New expiration date is " + newExpDate);
+
+		editAppSpecific("Expiration Date", newExpDate, capId);
 		var b1ExpResult = aa.expiration.getLicensesByCapID(capId);
 		if (b1ExpResult.getSuccess())
 		{
 			var b1Exp = b1ExpResult.getOutput();
 			b1Exp.setExpStatus("Active");
-			b1Exp.setExpDate(aa.date.parseDate(nullExpDate));
+			b1Exp.setExpDate(aa.date.parseDate(newExpDate));
 			aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
 		} 
 		
 	}
 
 } 
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [month, day, year].join('/');
+}
