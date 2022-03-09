3a7ed3a54916e3936c0cef252c24e9fa63f98ace
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-03-09 09:15:43 -0500

    Updates

--- Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
+++ Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
@@ -2,4 +2,6 @@ if (wfTask == "Application Review" && wfStatus == "Full Permit Required")
 {
     //deactivateActiveTasks("DEQ_SHIP")
     closeTask("Application Review", "Full Permit Required", "", "");
+    deactivateTask("Site Consult");
+    deactivateTask("Residential Provisional Phase");
 }
