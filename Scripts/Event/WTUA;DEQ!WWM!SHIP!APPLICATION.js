fe5e47006b229e67a1a389cf8eed3e345fc65c72
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-03-11 11:11:08 -0500

    Updates

--- Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
+++ Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
@@ -4,6 +4,7 @@ if (wfTask == "Application Review" && wfStatus == "Full Permit Required")
     closeTask("Application Review", "Full Permit Required", "", "");
     deactivateTask("Site Consult");
     deactivateTask("Residential Provisional Phase");
+    deactivateTask("Grant Review");
 }
 
 if (wfTask == "Preliminary Sketch Review" && wfStatus == "Full Permit Required")
@@ -12,4 +13,17 @@ if (wfTask == "Preliminary Sketch Review" && wfStatus == "Full Permit Required")
     closeTask("Preliminary Sketch Review", "Full Permit Required", "", "");
     deactivateTask("Inspections");
     deactivateTask("Final Review");
+}
+
+if (wfTask == "Grant Review" && (wfStatus == "No Application Received" || wfStatus == "Not Elligable" || wfStatus == "OK To Proceed"))
+{
+   if(isTaskActive("Site Consult") || isTaskActive("Preliminary Sketch Review"))
+   {
+       deactivateTask("Inspections");
+   }
+   else
+   {
+       activateTask("Inspections");
+   }
+
 }
