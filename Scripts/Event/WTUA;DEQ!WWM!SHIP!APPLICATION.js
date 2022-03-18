33b1cd672c96e6c7753ef1cfedfb33efdc3bce77
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-03-18 13:46:33 -0400

    Updates

--- Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
+++ Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
@@ -7,34 +7,33 @@ if (wfTask == "Application Review" && wfStatus == "Full Permit Required")
     deactivateTask("Grant Review");
 }
 
-if (wfTask == "Preliminary Sketch Review" && wfStatus == "Full Permit Required")
+if (wfTask == "Preliminary Sketch Review")
 {
+    if (wfStatus == "Full Permit Required")
+    {
     //deactivateActiveTasks("DEQ_SHIP")
     closeTask("Preliminary Sketch Review", "Full Permit Required", "", "");
     deactivateTask("Inspections");
-    deactivateTask("Final Review");
-}
-
-if (wfTask == "Grant Review" && (wfStatus == "No Application Received" || wfStatus == "Not Eligible" || wfStatus == "OK to Proceed"))
-{
-   if(isTaskActive("Site Consult") || isTaskActive("Preliminary Sketch Review"))
-   {
-       deactivateTask("Inspections");
-   }
-   else
-   {
-       activateTask("Inspections");
-   }
-
-}
-
-if (wfTask == "Preliminary Sketch Review" && wfStatus == "Ok to Proceed")
-{
-    if (isTaskActive("Grant Review"))
-    {
-        deactivateTask("Final Review")
-    }
-    else{
-        activateTask("Final Review")
+    deactivateTask("Final Review"); 
     }
 }
+
+if (isTaskActive("Grant Review") || isTaskActive("Preliminary Sketch Review"))
+{
+    deactivateTask("Final Review")
+    deactivateTask("Inspections")
+} 
+
+if (wfTask == "Grant Review" && (wfStatus == "No Application Received" || wfStatus == "Not Eligible" || wfStatus == "OK to Proceed")) 
+{
+    if (isTaskStatus("Preliminary Sketch Review", "OK to Proceed"))
+        
+        {
+            deactivateTask("Inspections")
+            activateTask("Final Review");
+
+        }
+
+    
+}
+
