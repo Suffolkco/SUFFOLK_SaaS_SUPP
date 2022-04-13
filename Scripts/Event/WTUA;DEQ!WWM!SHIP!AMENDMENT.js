0c2572116151f522f2de3b430d82d1a6a556df69
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-04-13 14:31:21 -0400

    Updates

--- Scripts/Event/WTUA;DEQ!WWM!SHIP!AMENDMENT.js
+++ Scripts/Event/WTUA;DEQ!WWM!SHIP!AMENDMENT.js
@@ -1,15 +1,16 @@
 var parent = parentCapId.getCustomID();
 var useAppSpecificGroupName = true;
-var startDate = getAppSpecific("OPERATIONS CONTRACT.Contract Start Date", capId);
-var term = getAppSpecific("OPERATIONS CONTRACT.Contract Term");
-var annualCost = getAppSpecific("OPERATIONS CONTRACT.Contract Annual Cost");
-var installDate = getAppSpecific("INSTALLATION DATE.Date of Installation");
-var make = getAppSpecific("I/A OWTS.Make");
-var model = getAppSpecific("I/A OWTS.Model");
-var leachType = getAppSpecific("LEACHING POOLS/GALLEYS.Type");
-var leachProduct = getAppSpecific("OTHER LEACHING STRUCTURES.Leaching Product");
-var effluentPump = getAppSpecific("LEACHING POOLS/GALLEYS.Effluent Pump Manufacturer");
-var propertyUse = getAppSpecific("OPERATIONS CONTRACT.Property Usage")
+var startDate = getAppSpecificLOCAL("OPERATIONS CONTRACT.Contract Start Date");
+logDebug("startDate = " + startDate);
+var term = getAppSpecificLOCAL("OPERATIONS CONTRACT.Contract Term");
+var annualCost = getAppSpecificLOCAL("OPERATIONS CONTRACT.Contract Annual Cost");
+var installDate = getAppSpecificLOCAL("INSTALLATION DATE.Date of Installation");
+var make = getAppSpecificLOCAL("I/A OWTS.Make");
+var model = getAppSpecificLOCAL("I/A OWTS.Model");
+var leachType = getAppSpecificLOCAL("LEACHING POOLS/GALLEYS.Type");
+var leachProduct = getAppSpecificLOCAL("OTHER LEACHING STRUCTURES.Leaching Product");
+var effluentPump = getAppSpecificLOCAL("LEACHING POOLS/GALLEYS.Effluent Pump Manufacturer");
+var propertyUse = getAppSpecificLOCAL("OPERATIONS CONTRACT.Property Usage")
 
 logDebug("Parent = " + parent);
 
@@ -19,20 +20,45 @@ if (wfTask == "Submission Review" && wfStatus == "SHIP Record Complete")
     var wwmIA = createChild('DEQ', 'Ecology', 'IA', 'Application', desc, parentCapId);
     editAppSpecificLOCAL("WWM APPLICATION NUMBER.WWM Application Number", parent, wwmIA);
     editAppSpecificLOCAL("CONTRACT INFORMATION.Contract Start Date", startDate, wwmIA);
+    editAppSpecificLOCAL("CONTRACT INFORMATION.Contract Term", term, wwmIA);
+    editAppSpecificLOCAL("CONTRACT INFORMATION.Contract Annual Cost", annualCost, wwmIA);
     editAppSpecificLOCAL("IA DEVICE INFORMATION.Installation Date", installDate, wwmIA);
     editAppSpecificLOCAL("IA DEVICE INFORMATION.Leaching", leachType, wwmIA);
     editAppSpecificLOCAL("IA DEVICE INFORMATION.Leaching Manufacturer", leachProduct, wwmIA);
     editAppSpecificLOCAL("IA DEVICE INFORMATION.Effluent Pump", effluentPump, wwmIA);
     editAppSpecificLOCAL("PROPERTY INFORMATION.Use", propertyUse, wwmIA);
+    editAppSpecificLOCAL("IA DEVICE INFORMATION.Manufacturer", make, wwmIA)
+    editAppSpecificLOCAL("IA DEVICE INFORMATION.Model", model, wwmIA)
     copyAddress(capId, wwmIA);
-    copyContactsByType(capId, wwmIA, ["Property Owner"]);
-    copyContactsByType(capId, wwmIA, ["Agent"]);
+    copyContactsByType(capId, wwmIA, "Property Owner");
+    //copyContactsByType(capId, wwmIA, "Agent");
     copyParcel(capId, wwmIA);
-    copyLicensedProfByType(capId, wwmIA, "IA Installer");
+    copyLicensedProfByType(capId, wwmIA, ["IA Installer"]);
     //copyLicensedProfByType(capId, wwmIA, ["IA Service Provider"]);
     //copyLicensedProfByType(capId, wwmIA, ["IA Designer"]);
     copyDocuments(capId, wwmIA, "Final Site Sketch");
-    updateTask("Final Review", "Registration Complete", "", "", parentCapId);
+    //updateTask("Final Review", "Registration Complete", "", "", "", parentCapId);
+    closeTaskByCap("Final Review", parentCapId, "Registration Complete", "","");
+
+    if (startDate != null)
+    {
+    
+    contractStart = new Date(startDate);
+    var newExpDate = (contractStart.getMonth() + 1) + "/" + (contractStart.getDate()) + "/" + (contractStart.getFullYear() + Number(term));
+    editAppSpecificLOCAL("CONTRACT INFORMATION.Contract Expiration Date", newExpDate, wwmIA); 
+    }
+
+    if (installDate != null)
+    {
+        installDate = new Date(installDate);
+        var newServiceDate = (installDate.getMonth() + 1 + "/" + (installDate.getDate()) + "/" + (installDate.getFullYear() + 1)); 
+        editAppSpecificLOCAL("CONTRACT INFORMATION.Next Service Date", newServiceDate, wwmIA);
+        var newSampleDate = (installDate.getMonth() + 1 + "/" + (installDate.getDate()) + "/" + (installDate.getFullYear() + 3)); 
+        editAppSpecificLOCAL("CONTRACT INFORMATION.Next Sample Date", newSampleDate, wwmIA);
+    }
+
+
+
 } 
 
 
@@ -87,163 +113,101 @@ function editAppSpecificLOCAL(itemName, itemValue)  // optional: itemCap
     }
 }
 
-function copyDocuments(pFromCapId, pToCapId) {
-	//Copies all attachments (documents) from pFromCapId to pToCapId
-	var categoryArray = new Array();
+// function copyDocuments(pFromCapId, pToCapId) {
+// 	//Copies all attachments (documents) from pFromCapId to pToCapId
+// 	var categoryArray = new Array();
 	
-	// third optional parameter is comma delimited list of categories to copy.
-	if (arguments.length > 2) {
-		categoryList = arguments[2];
-		categoryArray = categoryList.split(",");
-	}
+// 	// third optional parameter is comma delimited list of categories to copy.
+// 	if (arguments.length > 2) {
+// 		categoryList = arguments[2];
+// 		categoryArray = categoryList.split(",");
+// 	}
 
-	var capDocResult = aa.document.getDocumentListByEntity(pFromCapId,"CAP");
-	if(capDocResult.getSuccess()) {
-		if(capDocResult.getOutput().size() > 0) {
-	    	for(docInx = 0; docInx < capDocResult.getOutput().size(); docInx++) {
-	    		var documentObject = capDocResult.getOutput().get(docInx);
-	    		currDocCat = "" + documentObject.getDocCategory();
-	    		if (categoryArray.length == 0 || exists(currDocCat, categoryArray)) {
-	    			// download the document content
-					var useDefaultUserPassword = true;
-					//If useDefaultUserPassword = true, there is no need to set user name & password, but if useDefaultUserPassword = false, we need define EDMS user name & password.
-					var EMDSUsername = null;
-					var EMDSPassword = null;
-					var path = null;
-					var downloadResult = aa.document.downloadFile2Disk(documentObject, documentObject.getModuleName(), EMDSUsername, EMDSPassword, useDefaultUserPassword);
-					if(downloadResult.getSuccess()) {
-						path = downloadResult.getOutput();
-					}
-					var tmpEntId = pToCapId.getID1() + "-" + pToCapId.getID2() + "-" + pToCapId.getID3();
-					documentObject.setDocumentNo(null);
-					documentObject.setCapID(pToCapId)
-					documentObject.setEntityID(tmpEntId);
+// 	var capDocResult = aa.document.getDocumentListByEntity(pFromCapId,"CAP");
+// 	if(capDocResult.getSuccess()) {
+// 		if(capDocResult.getOutput().size() > 0) {
+// 	    	for(docInx = 0; docInx < capDocResult.getOutput().size(); docInx++) {
+// 	    		var documentObject = capDocResult.getOutput().get(docInx);
+// 	    		currDocCat = "" + documentObject.getDocCategory();
+// 	    		if (categoryArray.length == 0 || exists(currDocCat, categoryArray)) {
+// 	    			// download the document content
+// 					var useDefaultUserPassword = true;
+// 					//If useDefaultUserPassword = true, there is no need to set user name & password, but if useDefaultUserPassword = false, we need define EDMS user name & password.
+// 					var EMDSUsername = null;
+// 					var EMDSPassword = null;
+// 					var path = null;
+// 					var downloadResult = aa.document.downloadFile2Disk(documentObject, documentObject.getModuleName(), EMDSUsername, EMDSPassword, useDefaultUserPassword);
+// 					if(downloadResult.getSuccess()) {
+// 						path = downloadResult.getOutput();
+// 					}
+// 					var tmpEntId = pToCapId.getID1() + "-" + pToCapId.getID2() + "-" + pToCapId.getID3();
+// 					documentObject.setDocumentNo(null);
+// 					documentObject.setCapID(pToCapId)
+// 					documentObject.setEntityID(tmpEntId);
 	
-					// Open and process file
-					try {
-						if (path != null && path != "") {
-						// put together the document content - use java.io.FileInputStream
-							var newContentModel = aa.document.newDocumentContentModel().getOutput();
-							inputstream = new java.io.FileInputStream(path);
-							newContentModel.setDocInputStream(inputstream);
-							documentObject.setDocumentContent(newContentModel);
-							var newDocResult = aa.document.createDocument(documentObject);
-							if (newDocResult.getSuccess()) {
-								newDocResult.getOutput();
-								logDebug("Successfully copied document: " + documentObject.getFileName() + " From: " + pFromCapId.getCustomID() + " To: " + pToCapId.getCustomID());
-							}
-							else {
-								logDebug("Failed to copy document: " + documentObject.getFileName());
-								logDebug(newDocResult.getErrorMessage());
-							}
-						}
-					}
-					catch (err) {
-						logDebug("Error copying document: " + err.message);
-						return false;
-					}
-				}
-	    	} // end for loop
+// 					// Open and process file
+// 					try {
+// 						if (path != null && path != "") {
+// 						// put together the document content - use java.io.FileInputStream
+// 							var newContentModel = aa.document.newDocumentContentModel().getOutput();
+// 							inputstream = new java.io.FileInputStream(path);
+// 							newContentModel.setDocInputStream(inputstream);
+// 							documentObject.setDocumentContent(newContentModel);
+// 							var newDocResult = aa.document.createDocument(documentObject);
+// 							if (newDocResult.getSuccess()) {
+// 								newDocResult.getOutput();
+// 								logDebug("Successfully copied document: " + documentObject.getFileName() + " From: " + pFromCapId.getCustomID() + " To: " + pToCapId.getCustomID());
+// 							}
+// 							else {
+// 								logDebug("Failed to copy document: " + documentObject.getFileName());
+// 								logDebug(newDocResult.getErrorMessage());
+// 							}
+// 						}
+// 					}
+// 					catch (err) {
+// 						logDebug("Error copying document: " + err.message);
+// 						return false;
+// 					}
+// 				}
+// 	    	} // end for loop
+// 		}
+//     }
+// }
+
+function copyContactsByType(pFromCapId, pToCapId, pContactType)
+	{
+	//Copies all contacts from pFromCapId to pToCapId
+	//where type == pContactType
+	if (pToCapId==null)
+		var vToCapId = capId;
+	else
+		var vToCapId = pToCapId;
+	
+	var capContactResult = aa.people.getCapContactByCapID(pFromCapId);
+	var copied = 0;
+	if (capContactResult.getSuccess())
+		{
+		var Contacts = capContactResult.getOutput();
+		for (yy in Contacts)
+			{
+			if(Contacts[yy].getCapContactModel().getContactType() == pContactType)
+			    {
+			    var newContact = Contacts[yy].getCapContactModel();
+			    newContact.setCapID(vToCapId);
+			    aa.people.createCapContact(newContact);
+			    copied++;
+			    logDebug("Copied contact from "+pFromCapId.getCustomID()+" to "+vToCapId.getCustomID());
+			    }
+		
+			}
 		}
-    }
-}
-
-function copyContactsByType(srcCapId, targetCapId, ContactTypeArr)
-{
-    //1. Get people with source CAPID.
-    var capPeoples = getPeople3_0(srcCapId);
-    if (capPeoples == null || capPeoples.length == 0)
-    {
-        return;
-    }
-    //2. Get people with target CAPID.
-    var targetPeople = getPeople3_0(targetCapId);
-    //3. Check to see which people is matched in both source and target.
-    for (loopk in capPeoples)
-    {
-        sourcePeopleModel = capPeoples[loopk];
-        //Check if contact type should be ignored
-        doCopy = false;
-        for (kk in ContactTypeArr)
-        {
-            if (ContactTypeArr[kk] == sourcePeopleModel.getCapContactModel().getContactType())
-                doCopy = true;
-        }
-        if (doCopy)
-        {
-            //3.1 Set target CAPID to source people.
-            sourcePeopleModel.getCapContactModel().setCapID(targetCapId);
-            targetPeopleModel = null;
-            //3.2 Check to see if sourcePeople exist.
-            if (targetPeople != null && targetPeople.length > 0)
-            {
-                for (loop2 in targetPeople)
-                {
-                    if (isMatchPeople3_0(sourcePeopleModel, targetPeople[loop2]))
-                    {
-                        targetPeopleModel = targetPeople[loop2];
-                        break;
-                    }
-                }
-            }
-            //3.3 It is a matched people model.
-            if (targetPeopleModel != null)
-            {
-                //3.3.1 Copy information from source to target.
-                aa.people.copyCapContactModel(sourcePeopleModel.getCapContactModel(), targetPeopleModel.getCapContactModel());
-                //3.3.2 Copy contact address from source to target.
-                if (targetPeopleModel.getCapContactModel().getPeople() != null && sourcePeopleModel.getCapContactModel().getPeople())
-                {
-                    targetPeopleModel.getCapContactModel().getPeople().setContactAddressList(sourcePeopleModel.getCapContactModel().getPeople().getContactAddressList());
-                }
-                //3.3.3 Edit People with source People information.
-                aa.people.editCapContactWithAttribute(targetPeopleModel.getCapContactModel());
-            }
-            //3.4 It is new People model.
-            else
-            {
-                //3.4.1 Create new people.
-                aa.people.createCapContactWithAttribute(sourcePeopleModel.getCapContactModel());
-            }
-        }
-    }
-}
-function getPeople3_0(capId)
-{
-    capPeopleArr = null;
-    var s_result = aa.people.getCapContactByCapID(capId);
-    if (s_result.getSuccess())
-    {
-        capPeopleArr = s_result.getOutput();
-        if (capPeopleArr != null || capPeopleArr.length > 0)
-        {
-            for (loopk in capPeopleArr)
-            {
-                var capContactScriptModel = capPeopleArr[loopk];
-                var capContactModel = capContactScriptModel.getCapContactModel();
-                var peopleModel = capContactScriptModel.getPeople();
-                var contactAddressrs = aa.address.getContactAddressListByCapContact(capContactModel);
-                if (contactAddressrs.getSuccess())
-                {
-                    var contactAddressModelArr = convertContactAddressModelArr(contactAddressrs.getOutput());
-                    peopleModel.setContactAddressList(contactAddressModelArr);
-                }
-            }
-        }
-        else
-        {
-            logDebug("WARNING: no People on this CAP:" + capId);
-            capPeopleArr = null;
-        }
-    }
-    else
-    {
-        logDebug("ERROR: Failed to People: " + s_result.getErrorMessage());
-        capPeopleArr = null;
-    }
-    return capPeopleArr;
-}
-
+	else
+		{
+		logMessage("**ERROR: Failed to get contacts: " + capContactResult.getErrorMessage()); 
+		return false; 
+		}
+	return copied;
+	} 
 
 function copyLicensedProfByType(capIdFrom, capIdTo, typesArray)
 {
@@ -281,4 +245,123 @@ function arrayContainsValue(ary, value)
         }//for all types
     }
     return false;
+}
+
+function updateTask(wfstr, wfstat, wfcomment, wfnote) // optional process name, cap id
+{
+	var useProcess = false;
+	var processName = "";
+	if (arguments.length > 4) 
+	{
+		if (arguments[4] != "") 
+		{
+			processName = arguments[4]; // subprocess
+			useProcess = true;
+		}
+	}
+	var itemCap = capId;
+	if (arguments.length == 6)
+	{
+		itemCap = arguments[5]; // use cap ID specified in args
+	}
+	var workflowResult = aa.workflow.getTaskItems(itemCap, wfstr, processName, null, null, null);
+	if (workflowResult.getSuccess())
+	{
+		var wfObj = workflowResult.getOutput();
+	}
+	else 
+	{
+		logDebug("**ERROR: Failed to get workflow object: " + s_capResult.getErrorMessage());
+		return false;
+	}
+	if (!wfstat)
+	{
+		wfstat = "NA";
+	}
+	for (i in wfObj)
+	{
+		var fTask = wfObj[i];
+		if (fTask.getTaskDescription().toUpperCase().equals(wfstr.toUpperCase()) && (!useProcess || fTask.getProcessCode().equals(processName))) 
+		{
+			var dispositionDate = aa.date.getCurrentDate();
+			var stepnumber = fTask.getStepNumber();
+			var processID = fTask.getProcessID();
+			if (useProcess)
+			{
+				aa.workflow.handleDisposition(itemCap, stepnumber, processID, wfstat, dispositionDate, wfnote, wfcomment, systemUserObj, "U");
+				logDebug("Updating Workflow Task " + wfstr + " with status " + wfstat);
+			}
+			else
+			{
+				aa.workflow.handleDisposition(itemCap, stepnumber, wfstat, dispositionDate, wfnote, wfcomment, systemUserObj, "U");
+				logDebug("Updating Workflow Task " + wfstr + " with status " + wfstat);
+			}
+		}
+	}
+}
+function closeTaskByCap(wfstr, capId, wfstat, wfcomment, wfnote) 
+{
+    var currentUserID = aa.env.getValue("CurrentUserID");
+    var systemUserObj = aa.person.getUser(currentUserID).getOutput();
+    var workflowResult = aa.workflow.getTasks(capId);
+    if (workflowResult.getSuccess()) 
+    {
+        var wfObj = workflowResult.getOutput();
+    } 
+    else
+    {
+        logDebug("**ERROR: Failed to get workflow object: " + workflowResult.getErrorMessage());
+        return false;
+    }
+    if (!wfstat)
+        wfstat = "NA";
+    for (var i in wfObj) 
+    {
+        var fTask = wfObj[i];
+        if (fTask.getTaskDescription().toUpperCase().equals(wfstr.toUpperCase())) 
+        {
+            var dispositionDate = aa.date.getCurrentDate();
+            var stepnumber = fTask.getStepNumber();
+            var processID = fTask.getProcessID();
+            //PARAMETERS ARE: Cap ID, StepNumber, Task Status, Status Date, Status Note, Comment, System User, Flag
+            aa.workflow.handleDisposition(capId, stepnumber, wfstat, dispositionDate, wfnote, wfcomment, systemUserObj, "Y");
+            logDebug("Closing Workflow Task: " + wfstr + " with status " + wfstat);
+        }
+    }
+}
+
+function getAppSpecificLOCAL(itemName)  // optional: itemCap
+{
+	var updated = false;
+	var i=0;
+	var itemCap = capId;
+	if (arguments.length == 2) itemCap = arguments[1]; // use cap ID specified in args
+   	
+	if (useAppSpecificGroupName)
+	{
+		if (itemName.indexOf(".") < 0)
+			{ logDebug("**WARNING: editAppSpecific requires group name prefix when useAppSpecificGroupName is true") ; return false }
+		
+		
+		var itemGroup = itemName.substr(0,itemName.indexOf("."));
+		var itemName = itemName.substr(itemName.indexOf(".")+1);
+	}
+	
+    var appSpecInfoResult = aa.appSpecificInfo.getByCapID(itemCap);
+	if (appSpecInfoResult.getSuccess())
+ 	{
+		var appspecObj = appSpecInfoResult.getOutput();
+		
+		if (itemName != "")
+		{
+			for (i in appspecObj)
+				if( appspecObj[i].getCheckboxDesc() == itemName && (!useAppSpecificGroupName || appspecObj[i].getCheckboxType() == itemGroup) )
+				{
+					return appspecObj[i].getChecklistComment();
+					break;
+				}
+		} // item name blank
+	} 
+	else
+		{ logDebug( "**ERROR: getting app specific info for Cap : " + appSpecInfoResult.getErrorMessage()) }
 }
