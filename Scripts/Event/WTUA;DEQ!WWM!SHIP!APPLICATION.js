2be1809cdd83cebcb7ec2d29dd7fd66a980311a8
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-04-14 09:18:55 -0400

    Updates

--- Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
+++ Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
@@ -1,3 +1,18 @@
+var contactResult = aa.people.getCapContactByCapID(capId);
+var capContacts = contactResult.getOutput();
+var conEmail = "";
+for (c in capContacts)
+{
+    if (matches(capContacts[c].getCapContactModel().getContactType(), "Property Owner"))
+
+    {
+        if (!matches(capContacts[c].email, null, undefined, ""))
+        {
+            conEmail += capContacts[c].email + ";"
+        }
+    }
+}
+
 if (wfTask == "Application Review" && wfStatus == "Full Permit Required")
 {
     //deactivateActiveTasks("DEQ_SHIP")
@@ -32,8 +47,31 @@ if (wfTask == "Grant Review" && (wfStatus == "No Application Received" || wfStat
             deactivateTask("Inspections")
             activateTask("Final Review");
 
-        }
+        }   
+}
 
-    
+if (wfTask == "Final Review" && wfStatus == "Registration Complete")
+
+{
+    var capParcelResult = aa.parcel.getParcelandAttribute(capId, null);  
+        if (capParcelResult.getSuccess())
+        {
+            var Parcels = capParcelResult.getOutput().toArray();
+                for (zz in Parcels)
+                    {
+                        var parcelNumber = Parcels[zz].getParcelNumber(); 
+                        logDebug("parcelNumber = " + parcelNumber); 
+                    }
+}
+
+var addrResult = getAddressInALine(capId);
+var vEParams = aa.util.newHashtable();
+var addrResult = getAddressInALine(capId);
+addParameter(vEParams, "$$altID$$", capId.getCustomID());
+addParameter(vEParams, "$$address$$", addrResult);
+addParameter(vEParams, "$$Parcel$$", parcelNumber);
+addParameter(vEParams, "$$FullNameBusName$$", capContacts[c].getCapContactModel().getContactName());
+sendNotification("", conEmail, "", "DEQ_SHIP_HOMEOWNER", vEParams, null);
+
 }
 
