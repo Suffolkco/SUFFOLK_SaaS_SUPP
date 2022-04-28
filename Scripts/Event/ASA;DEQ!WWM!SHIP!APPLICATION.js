2c00b2f3dda2a4bfa105e06d27e5816a3436556a
jakeg70888 <92736306+jakeg70888@users.noreply.github.com>
2022-04-28 11:44:21 -0400

    Updates

--- Scripts/Event/ASA;DEQ!WWM!SHIP!APPLICATION.js
+++ Scripts/Event/ASA;DEQ!WWM!SHIP!APPLICATION.js
@@ -1,111 +1,111 @@
-var contactResult = aa.people.getCapContactByCapID(capId);
-var capContacts = contactResult.getOutput();
-var conEmail = "";
-for (c in capContacts)
-{
-    if (matches(capContacts[c].getCapContactModel().getContactType(), "Property Owner"))
+// var contactResult = aa.people.getCapContactByCapID(capId);
+// var capContacts = contactResult.getOutput();
+// var conEmail = "";
+// for (c in capContacts)
+// {
+//     if (matches(capContacts[c].getCapContactModel().getContactType(), "Property Owner"))
 
-    {
-        if (!matches(capContacts[c].email, null, undefined, ""))
-        {
-            conEmail += capContacts[c].email + ";"
-        }
-    }
-}
+//     {
+//         if (!matches(capContacts[c].email, null, undefined, ""))
+//         {
+//             conEmail += capContacts[c].email + ";"
+//         }
+//     } 
+// }
 
-var capParcelResult = aa.parcel.getParcelandAttribute(capId, null);  
-if (capParcelResult.getSuccess())
-{
-    var Parcels = capParcelResult.getOutput().toArray();
-    for (zz in Parcels)
-    {
-        var parcelNumber = Parcels[zz].getParcelNumber(); 
-        logDebug("parcelNumber = " + parcelNumber);
-    }
-}
+// var capParcelResult = aa.parcel.getParcelandAttribute(capId, null);  
+// if (capParcelResult.getSuccess())
+// {
+//     var Parcels = capParcelResult.getOutput().toArray();
+//     for (zz in Parcels)
+//     {
+//         var parcelNumber = Parcels[zz].getParcelNumber(); 
+//         logDebug("parcelNumber = " + parcelNumber);
+//     }
+// }
 
-var addrResult = getAddressInALine(capId);
+// var addrResult = getAddressInALine(capId);
 
-var vEParams = aa.util.newHashtable();
-var addrResult = getAddressInALine(capId);
-addParameter(vEParams, "$$altID$$", capId.getCustomID());
-addParameter(vEParams, "$$address$$", addrResult);
-addParameter(vEParams, "$$Parcel$$", parcelNumber);
-addParameter(vEParams, "$$FullNameBusName$$", capContacts[c].getCapContactModel().getContactName());
+// var vEParams = aa.util.newHashtable();
+// var addrResult = getAddressInALine(capId);
+// addParameter(vEParams, "$$altID$$", capId.getCustomID());
+// addParameter(vEParams, "$$address$$", addrResult);
+// addParameter(vEParams, "$$Parcel$$", parcelNumber);
+// addParameter(vEParams, "$$FullNameBusName$$", capContacts[c].getCapContactModel().getContactName());
 
-sendNotification("", conEmail, "", "DEQ_SHIP_HOMEOWNER", vEParams, null);
+// sendNotification("", conEmail, "", "DEQ_SHIP_HOMEOWNER", vEParams, null);
 
 
-function getAddressInALine(capId)
-{
+// function getAddressInALine(capId)
+// {
 
-    var capAddrResult = aa.address.getAddressByCapId(capId);
-    var addressToUse = null;
-    var strAddress = "";
+//     var capAddrResult = aa.address.getAddressByCapId(capId);
+//     var addressToUse = null;
+//     var strAddress = "";
 
-    if (capAddrResult.getSuccess())
-    {
-        var addresses = capAddrResult.getOutput();
-        if (addresses)
-        {
-            for (zz in addresses)
-            {
-                capAddress = addresses[zz];
-                if (capAddress.getPrimaryFlag() && capAddress.getPrimaryFlag().equals("Y"))
-                    addressToUse = capAddress;
-            }
-            if (addressToUse == null)
-                addressToUse = addresses[0];
+//     if (capAddrResult.getSuccess())
+//     {
+//         var addresses = capAddrResult.getOutput();
+//         if (addresses)
+//         {
+//             for (zz in addresses)
+//             {
+//                 capAddress = addresses[zz];
+//                 if (capAddress.getPrimaryFlag() && capAddress.getPrimaryFlag().equals("Y"))
+//                     addressToUse = capAddress;
+//             }
+//             if (addressToUse == null)
+//                 addressToUse = addresses[0];
 
-            if (addressToUse)
-            {
-                strAddress = addressToUse.getHouseNumberStart();
-                var addPart = addressToUse.getStreetDirection();
-                if (addPart && addPart != "")
-                    strAddress += " " + addPart;
-                var addPart = addressToUse.getStreetName();
-                if (addPart && addPart != "")
-                    strAddress += " " + addPart;
-                var addPart = addressToUse.getStreetSuffix();
-                if (addPart && addPart != "")
-                    strAddress += " " + addPart;
-                var addPart = addressToUse.getCity();
-                if (addPart && addPart != "")
-                    strAddress += " " + addPart + ",";
-                var addPart = addressToUse.getState();
-                if (addPart && addPart != "")
-                    strAddress += " " + addPart;
-                var addPart = addressToUse.getZip();
-                if (addPart && addPart != "")
-                    strAddress += " " + addPart;
-                return strAddress
-            }
-        }
-    }
-    return null;
-}
-function getContactName(vConObj)
-{
-    if (vConObj.people.getContactTypeFlag() == "organization")
-    {
-        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
-            return vConObj.people.getBusinessName();
+//             if (addressToUse)
+//             {
+//                 strAddress = addressToUse.getHouseNumberStart();
+//                 var addPart = addressToUse.getStreetDirection();
+//                 if (addPart && addPart != "")
+//                     strAddress += " " + addPart;
+//                 var addPart = addressToUse.getStreetName();
+//                 if (addPart && addPart != "")
+//                     strAddress += " " + addPart;
+//                 var addPart = addressToUse.getStreetSuffix();
+//                 if (addPart && addPart != "")
+//                     strAddress += " " + addPart;
+//                 var addPart = addressToUse.getCity();
+//                 if (addPart && addPart != "")
+//                     strAddress += " " + addPart + ",";
+//                 var addPart = addressToUse.getState();
+//                 if (addPart && addPart != "")
+//                     strAddress += " " + addPart;
+//                 var addPart = addressToUse.getZip();
+//                 if (addPart && addPart != "")
+//                     strAddress += " " + addPart;
+//                 return strAddress
+//             }
+//         }
+//     }
+//     return null;
+// }
+// function getContactName(vConObj)
+// {
+//     if (vConObj.people.getContactTypeFlag() == "organization")
+//     {
+//         if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+//             return vConObj.people.getBusinessName();
 
-        return vConObj.people.getBusinessName2();
-    }
-    else
-    {
-        if (vConObj.people.getFullName() != null && vConObj.people.getFullName() != "")
-        {
-            return vConObj.people.getFullName();
-        }
-        if (vConObj.people.getFirstName() != null && vConObj.people.getLastName() != null)
-        {
-            return vConObj.people.getFirstName() + " " + vConObj.people.getLastName();
-        }
-        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
-            return vConObj.people.getBusinessName();
+//         return vConObj.people.getBusinessName2();
+//     }
+//     else
+//     {
+//         if (vConObj.people.getFullName() != null && vConObj.people.getFullName() != "")
+//         {
+//             return vConObj.people.getFullName();
+//         }
+//         if (vConObj.people.getFirstName() != null && vConObj.people.getLastName() != null)
+//         {
+//             return vConObj.people.getFirstName() + " " + vConObj.people.getLastName();
+//         }
+//         if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+//             return vConObj.people.getBusinessName();
 
-        return vConObj.people.getBusinessName2();
-    }
-}
+//         return vConObj.people.getBusinessName2();
+//     }
+// }
