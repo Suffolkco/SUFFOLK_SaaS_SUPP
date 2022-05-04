d096045050c10bb83216fc6a540663b32856fc8a
Ada <ada.chan@suffolkcountyny.gov>
2022-05-04 13:24:35 -0400

    DAP-434

---
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!COMPLAINT!NA!NA.js
@@ -0,0 +1,93 @@
+//ASA;CONSUMERAFFAIRS!COMPLAINTS!NA!NA.js
+
+// Send email notification to Complainant 
+try{
+
+	var emailParams = aa.util.newHashtable();
+	var reportFile = new Array();
+	
+	if (!publicUser){ 
+		var contactType = "Complainant";
+		var contactInfo = getContactInfo(contactType, capId);
+		if(contactInfo == false){
+			logDebug("No complainant contact exists on this record");
+		}else{
+			
+			var vAddrLine1 = contactInfo[0];
+			var vCity = contactInfo[1];
+			var vState = contactInfo[2];
+			var vZip = contactInfo[3];
+			var vAddress = new Array();
+			vAddress.push(vAddrLine1);
+			vAddress.push(vCity);
+			vAddress.push(vState);
+			vAddress.push(vZip);
+					
+			// copy Vendor name, org name & phone to short notes
+			var fName = contactInfo[4];
+			var lName = contactInfo[5];					
+			var email = contactInfo[8];	
+
+			var startDate = new Date();
+			var startTime = startDate.getTime(); // Start timer
+			var todayDate = (startDate.getMonth() + 1) + "/" + startDate.getDate() + "/" + startDate.getFullYear();
+
+			getRecordParams4Notification(emailParams);
+			addParameter(emailParams, "$$altID$$", capId.getCustomID());
+			addParameter(emailParams, "$$name$$", fName + " " + lName);
+			addParameter(emailParams, "$$address", vAddrLine1);
+			addParameter(emailParams, "$$city$$", vCity);
+			addParameter(emailParams, "$$state$$", capId.getCustomID());
+			addParameter(emailParams, "$$zip$$", vZip);
+			addParameter(emailParams, "$$date$$", todayDate);
+
+			sendNotification("", email, "", "DCA_COMPLAINANTNOTIFICATION", emailParams, reportFile);
+
+		}
+	}
+}catch(err){
+	logDebug("**WARN: Error in ASA updating short notes and address -  " + err.message);
+}
+function getContactInfo(cType, capId) {
+	var returnArray = new Array();
+	var haveCType = false;
+	
+	var contModel = null; 
+	var consResult = aa.people.getCapContactByCapID(capId);	
+	if (consResult.getSuccess()) {
+		var cons = consResult.getOutput();
+		for (thisCon in cons) {
+			var capContactType = cons[thisCon].getCapContactModel().getPeople().getContactType();
+			if (capContactType == cType) {				
+				var contModel = cons[thisCon].getCapContactModel(); 
+				
+				var firstName = contModel.getFirstName();
+				var lastName = contModel.getLastName();
+				var business = contModel.getBusinessName();
+				var phone = contModel.getPhone1();
+				var addr1 = contModel.getAddressLine1();
+				var city = contModel.getCity();
+				var state = contModel.getState();
+				var zip = contModel.getZip();
+				var email = contModel.getPeople().getEmail();
+
+			
+				// build returnArray
+				returnArray.push(addr1);
+				returnArray.push(city);
+				returnArray.push(state);
+				returnArray.push(zip);
+				returnArray.push(firstName);
+				returnArray.push(lastName);
+				returnArray.push(business);
+				returnArray.push(phone);
+				returnArray.push(email);
+				return returnArray;
+				haveCType = true;
+			}
+		}
+	}
+	if (haveCType == false){
+		return false;
+	}
+}
