95e52665d17b1deaad71b8997263e9ad9afd1902
Ada <ada.chan@suffolkcountyny.gov>
2022-05-09 16:06:03 -0400

    Test

--- Scripts/Event/DUA;DEQ!~!~!~.js
+++ Scripts/Event/DUA;DEQ!~!~!~.js
@@ -64,11 +64,7 @@ if (publicUser)
             var assignedUserid = cd.getAsgnStaff();
             if (assignedUserid !=  null)
             {
-            iNameResult = aa.person.getUser(assignedUserid)
-
-            if (!iNameResult.getSuccess())
-                { logDebug("**ERROR retrieving  user model " + assignstaffId + " : " + iNameResult.getErrorMessage()) ;}
-
+                iNameResult = aa.person.getUser(assignedUserid)
 
                 if(iNameResult.getSuccess())
                 {
@@ -81,11 +77,9 @@ if (publicUser)
                     if (assignedUser.getEmail() != null)
                     {
                         sendNotification("", assignedUser.getEmail() , "", "DEQ_WWM_REVIEW_REQUIRED", emailParams, reportFile);
-                    }
-                    
+                    }                    
                 }
-            } 
-            
+            }             
         }
     }
 
@@ -127,47 +121,3 @@ if (publicUser)
     
 }
 
-function logDebug(dstr)
-{
-	//if (showDebug.substring(0,1).toUpperCase().equals("Y"))
-	if(showDebug)
-	{
-		aa.print(dstr)
-		emailText+= dstr + "<br>";
-		aa.debug(aa.getServiceProviderCode() + " : " + aa.env.getValue("CurrentUserID"),dstr)
-	}
-}
-
-function assignCap(assignId) // option CapId
-	{
-	var itemCap = capId
-	if (arguments.length > 1) itemCap = arguments[1]; // use cap ID specified in args
-
-	var cdScriptObjResult = aa.cap.getCapDetail(itemCap);
-	if (!cdScriptObjResult.getSuccess())
-		{ logDebug("**ERROR: No cap detail script object : " + cdScriptObjResult.getErrorMessage()) ; return false; }
-
-	var cdScriptObj = cdScriptObjResult.getOutput();
-
-	if (!cdScriptObj)
-		{ logDebug("**ERROR: No cap detail script object") ; return false; }
-
-	cd = cdScriptObj.getCapDetailModel();
-
-	iNameResult  = aa.person.getUser(assignId);
-
-	if (!iNameResult.getSuccess())
-		{ logDebug("**ERROR retrieving  user model " + assignId + " : " + iNameResult.getErrorMessage()) ; return false ; }
-
-	iName = iNameResult.getOutput();
-
-	cd.setAsgnDept(iName.getDeptOfUser());
-	cd.setAsgnStaff(assignId);
-
-	cdWrite = aa.cap.editCapDetail(cd)
-
-	if (cdWrite.getSuccess())
-		{ logDebug("Assigned CAP to " + assignId) }
-	else
-		{ logDebug("**ERROR writing capdetail : " + cdWrite.getErrorMessage()) ; return false ; }
-	}
