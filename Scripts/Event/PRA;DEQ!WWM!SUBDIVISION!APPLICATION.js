538e7a129db165f4034f500cb7b8e5d138bde72e
Ada <ada.chan@suffolkcountyny.gov>
2022-05-10 10:58:15 -0400

    Test

--- Scripts/Event/PRA;DEQ!WWM!SUBDIVISION!APPLICATION.js
+++ Scripts/Event/PRA;DEQ!WWM!SUBDIVISION!APPLICATION.js
@@ -3,51 +3,6 @@
 
 if (publicUser)
 {    
-    // EHIMS-4832: Resubmission after user already submitted.    
-    if (isTaskActive("Plans Coordination") || 
-    getAppStatus() == "Resubmitted" || getAppStatus() == "Review in Process" )
-    {
-     
-        // 1. Set a flag
-        editAppSpecific("New Documents Uploaded", 'CHECKED', capId);
-
-            
-        // 2. Send email to Record Assignee                       
-        var cdScriptObjResult = aa.cap.getCapDetail(capId);
-        if (!cdScriptObjResult.getSuccess())
-            { logDebug("**ERROR: No cap detail script object : " + cdScriptObjResult.getErrorMessage()) ; }
-
-        var cdScriptObj = cdScriptObjResult.getOutput();
-
-        if (!cdScriptObj)
-            { logDebug("**ERROR: No cap detail script object") ; }
-
-        cd = cdScriptObj.getCapDetailModel();
-
-        // Record Assigned to
-        var assignedUserid = cd.getAsgnStaff();
-        if (assignedUserid !=  null)
-        {
-            iNameResult = aa.person.getUser(assignedUserid)
-
-            if(iNameResult.getSuccess())
-            {
-                assignedUser = iNameResult.getOutput();                   
-                var emailParams = aa.util.newHashtable();
-                var reportFile = new Array();
-                getRecordParams4Notification(emailParams);   
-                addParameter(emailParams, "$$assignedUser$$",assignedUser.getFirstName() + " " + assignedUser.getLastName());                 
-                addParameter(emailParams, "$$altID$$", capId.getCustomID());
-                if (assignedUser.getEmail() != null)
-                {
-                    sendNotification("", assignedUser.getEmail() , "", "DEQ_WWM_REVIEW_REQUIRED", emailParams, reportFile);
-                }                    
-            }
-        }            
-            
-     
-    }
-
     if (isTaskActive("Application Review") && isTaskStatus("Application Review","Awaiting Client Reply"))
     {
         updateTask("Application Review", "Resubmitted", "Additional payment submitted by Applicant", "Additional payment submitted by Applicant");       
@@ -85,26 +40,3 @@ function getAppStatus() {
 	return appStatus;
 }
 
-
-function editAppSpecific(itemName, itemValue) // optional: itemCap
-{
-	var itemCap = capId;
-	var itemGroup = null;
-	if (arguments.length == 3)
-		itemCap = arguments[2]; // use cap ID specified in args
-	if (useAppSpecificGroupName) {
-		if (itemName.indexOf(".") < 0) {
-			//logDebug("**WARNING: editAppSpecific requires group name prefix when useAppSpecificGroupName is true");
-			return false
-		}
-		itemGroup = itemName.substr(0, itemName.indexOf("."));
-		itemName = itemName.substr(itemName.indexOf(".") + 1);
-	}
-	var appSpecInfoResult = aa.appSpecificInfo.editSingleAppSpecific(itemCap, itemName, itemValue, itemGroup);
-	if (appSpecInfoResult.getSuccess()) {
-		if (arguments.length < 3) //If no capId passed update the ASI Array
-			AInfo[itemName] = itemValue;
-	} else {
-		//logDebug("WARNING: " + itemName + " was not updated.");
-	}
-}
