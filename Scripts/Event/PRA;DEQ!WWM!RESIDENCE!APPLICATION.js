0a019d49267d5ecfd7dddc93c2710b39e7e8bd11
Ada <ada.chan@suffolkcountyny.gov>
2022-05-10 10:53:41 -0400

    test

--- Scripts/Event/PRA;DEQ!WWM!RESIDENCE!APPLICATION.js
+++ Scripts/Event/PRA;DEQ!WWM!RESIDENCE!APPLICATION.js
@@ -1,9 +1,4 @@
 //PRA:DEQ/WR/Residence/Application
-var showMessage = true;
-var showDebug = true;
-var emailText = "";
-var emailAddress = "ada.chan@suffolkcountyny.gov";//email to send report
-
 if (publicUser)
 { 
   
@@ -29,51 +24,6 @@ if (publicUser)
 
     var appStatus = getAppStatus(capId);
     
-     /*
-    // EHIMS-4832: Resubmission after user already submitted.    
-    if (isTaskActive("Plans Coordination") || 
-    appStatus == "Resubmitted" 
-    || appStatus == "Review in Process")
-    {
-        // 1. Set a flag
-        editAppSpecific("New Documents Uploaded", 'CHECKED', capId);
-            
-        // 2. Send email to Record Assignee                       
-        var cdScriptObjResult = aa.cap.getCapDetail(capId);
-        if (!cdScriptObjResult.getSuccess())
-            { logDebug("**ERROR: No cap detail script object : " + cdScriptObjResult.getErrorMessage()) ; }
-
-        var cdScriptObj = cdScriptObjResult.getOutput();
-
-        if (!cdScriptObj)
-            { logDebug("**ERROR: No cap detail script object") ; }
-
-        cd = cdScriptObj.getCapDetailModel();
-
-        // Record Assigned to
-        var assignedUserid = cd.getAsgnStaff();
-        if (assignedUserid !=  null)
-        {
-            iNameResult = aa.person.getUser(assignedUserid)
-
-            if(iNameResult.getSuccess())
-            {
-                assignedUser = iNameResult.getOutput();                   
-                var emailParams = aa.util.newHashtable();
-                var reportFile = new Array();
-                getRecordParams4Notification(emailParams);   
-                addParameter(emailParams, "$$assignedUser$$",assignedUser.getFirstName() + " " + assignedUser.getLastName());                 
-                addParameter(emailParams, "$$altID$$", capId.getCustomID());
-                if (assignedUser.getEmail() != null)
-                {
-                    sendNotification("", assignedUser.getEmail() , "", "DEQ_WWM_REVIEW_REQUIRED", emailParams, reportFile);
-                    logDebug("Send notification." + isTaskActive("Plans Coordination") + "," + getAppStatus());
-                }                    
-            }
-        }      
-
-    }*/
-    aa.sendMail("noreplyehimslower@suffolkcountyny.gov", emailAddress, "", "PRA - DEQ WWM", emailText);
     // Only if the application has been reviewed
     if(appStatus != "Received")
     {
@@ -97,38 +47,4 @@ function getAppStatus() {
 		logDebug("ERROR: Failed to get app status: " + capResult.getErrorMessage());
 	}
 	return appStatus;
-}
-
-function editAppSpecific(itemName, itemValue) // optional: itemCap
-{
-	var itemCap = capId;
-	var itemGroup = null;
-	if (arguments.length == 3)
-		itemCap = arguments[2]; // use cap ID specified in args
-	if (useAppSpecificGroupName) {
-		if (itemName.indexOf(".") < 0) {
-			//logDebug("**WARNING: editAppSpecific requires group name prefix when useAppSpecificGroupName is true");
-			return false
-		}
-		itemGroup = itemName.substr(0, itemName.indexOf("."));
-		itemName = itemName.substr(itemName.indexOf(".") + 1);
-	}
-	var appSpecInfoResult = aa.appSpecificInfo.editSingleAppSpecific(itemCap, itemName, itemValue, itemGroup);
-	if (appSpecInfoResult.getSuccess()) {
-		if (arguments.length < 3) //If no capId passed update the ASI Array
-			AInfo[itemName] = itemValue;
-	} else {
-		//logDebug("WARNING: " + itemName + " was not updated.");
-	}
-}
-
-function logDebug(dstr)
-{
-	//if (showDebug.substring(0,1).toUpperCase().equals("Y"))
-	if(showDebug)
-	{
-		aa.print(dstr)
-		emailText+= dstr + "<br>";
-		aa.debug(aa.getServiceProviderCode() + " : " + aa.env.getValue("CurrentUserID"),dstr)
-	}
 }
