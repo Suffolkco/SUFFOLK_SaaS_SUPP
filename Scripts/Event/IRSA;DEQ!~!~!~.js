424f9ac709bd9dc3d14dc61944f8de5df9072d2c
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2022-05-25 10:20:52 -0400

    updates

--- Scripts/Event/IRSA;DEQ!~!~!~.js
+++ Scripts/Event/IRSA;DEQ!~!~!~.js
@@ -603,7 +603,7 @@ if (appTypeArray[1] == "WWM")
                             //updating workdesc
                             if (!matches(iaLeachOtherType, null, "", " ", undefined) && !matches(iaLeachProduct, null, "", " ", undefined))
                             {
-                                updateWorkDesc(manu + " " + model + " " + iaLeachOtherType + " " + iaLeachProduct, wwmIA);
+                                updateWorkDesc(iaManufacturer + " " + iaModel + " " + iaLeachOtherType + " " + iaLeachProduct, wwmIA);
                             }
                             else if (!matches(iaLeachPoolType, null, "", " ", undefined))
                             {
