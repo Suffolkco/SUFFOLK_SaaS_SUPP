21f7fc9cb9263a314d04bdf6f31c67d82061015c
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2022-06-15 12:10:13 -0400

    updates

--- Scripts/Event/WTUA;DEQ!GENERAL!COMPLAINT!NA.js
+++ Scripts/Event/WTUA;DEQ!GENERAL!COMPLAINT!NA.js
@@ -1,30 +1,20 @@
 //WTUA:DEQ/GENERAL/COMPLAINT/NA
 var emailParams = aa.util.newHashtable();
+var templateName = "";
 
 if (matches(wfTask, "Complaint Received", "Investigation Review"))
 {
   if (wfStatus == "Reassign")
   {
-    var assignedUser = wfTaskObj.getAssignedStaff();
-    logDebug("assigned user is: " + assignedUser);
-    if (!matches(wfTaskObj.getAssignedStaff(), null, ""))
-    {
-      var userSplit = String(wfTaskObj.getAssignedStaff());
-      var assignUser = aa.people.getUsersByUserIdAndName(userSplit);
-      if (assignUser.getSuccess())
-      {
-        logDebug("assigned user is: " + assignedUser);
-        var userOut = assignUser.getOutput();
-        if (userOut != null)
-        {
-          logDebug("user email is: " + userOut.getEmail());
-          sendNotification("", userOut.getEmail(), "", templateName, emailParams, null);
-        }
-      }
-    }
+    var userId = getUserIDAssignedToTask(capId, wfTask)
+    var userToSend = aa.person.getUser(userId).getOutput();
+    logDebug("sending email to " + userToSend.getEmail());
+    sendNotification("", userToSend.getEmail(), "", templateName, emailParams, null);
   }
-
 }
+
+
+
 if (wfTask == "Investigation Review" && wfStatus == "Referred to Other Office")
 {
   var assignedUser = wfTaskObj.getAssignedStaff();
@@ -32,4 +22,31 @@ if (wfTask == "Investigation Review" && wfStatus == "Referred to Other Office")
   assignTask("Complaint Received", assignedUser);
   updateTask("Complaint Received", "In Review", "", "");
   //    sendNotification("", emailAddress, "", templateName, emailParams, null);
+}
+
+function getUserIDAssignedToTask(vCapId, taskName) {
+  currentUsrVar = null;
+  var taskResult1 = aa.workflow.getTask(vCapId, taskName);
+  if (taskResult1.getSuccess())
+  {
+    tTask = taskResult1.getOutput();
+  } else
+  {
+    logMessage("**ERROR: Failed to get workflow task object ");
+    return false;
+  }
+  taskItem = tTask.getTaskItem();
+  taskUserObj = tTask.getTaskItem().getAssignedUser();
+  taskUserObjLname = taskUserObj.getLastName();
+  taskUserObjFname = taskUserObj.getFirstName();
+  taskUserObjMname = taskUserObj.getMiddleName();
+  currentUsrVar = aa.person.getUser(taskUserObjFname, taskUserObjMname, taskUserObjLname).getOutput();
+  if (currentUsrVar != null)
+  {
+    currentUserIDVar = currentUsrVar.getGaUserID();
+    return currentUserIDVar;
+  } else
+  {
+    return false;
+  }
 }
