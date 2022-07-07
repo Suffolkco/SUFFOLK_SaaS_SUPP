026738fe9c10b479be7d8ac8f7f3ed59fbd2dc8f
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2022-07-07 14:42:33 -0400

    updates

--- Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
+++ Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
@@ -618,3 +618,40 @@
         }
         return null;
     }
+    function generateReportBatch(itemCap, reportName, module, parameters)
+    {
+        //returns the report file which can be attached to an email.
+        var user = currentUserID; // Setting the User Name
+        var report = aa.reportManager.getReportInfoModelByName(reportName);
+        if (!report.getSuccess() || report.getOutput() == null)
+        {
+            logDebug("**WARN report generation failed, missing report or incorrect name: " + reportName);
+            return false;
+        }
+        report = report.getOutput();
+        report.setModule(module);
+        report.setCapId(itemCap); //CSG Updated from itemCap.getCustomID() to just itemCap so the file would save to Record
+        report.setReportParameters(parameters);
+    
+        var permit = aa.reportManager.hasPermission(reportName, user);
+    
+        if (permit.getOutput().booleanValue())
+        {
+            var reportResult = aa.reportManager.getReportResult(report);
+            if (reportResult.getSuccess())
+            {
+                reportOutput = reportResult.getOutput();
+                var reportFile = aa.reportManager.storeReportToDisk(reportOutput);
+                reportFile = reportFile.getOutput();
+                return reportFile;
+            } else
+            {
+                logDebug("**WARN System failed get report: " + reportResult.getErrorType() + ":" + reportResult.getErrorMessage());
+                return false;
+            }
+        } else
+        {
+            logDebug("You have no permission.");
+            return false;
+        }
+    }
