1420a997feafb3e2a44a0409909cc1e01ac0f239
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2022-07-14 13:36:05 -0400

    updates

--- Scripts/Event/ALVA;DEQ!~!~!~.js
+++ Scripts/Event/ALVA;DEQ!~!~!~.js
@@ -1,3 +1,5 @@
+aa.sendMail("noreplyehims@suffolkcountyny.gov", "ryan.littlefield@scubeenterprise.com", "", "ALVA is running", "");
+
 var emailParams = aa.util.newHashtable();
 var currentUserID = aa.env.getValue("CurrentUserID");
 if (currentUserID.indexOf("PUBLICUSER") == 0)
@@ -9,4 +11,4 @@ if (publicUser)
 
     sendNotification("", "ryan.littlefield@scubeenterprise.com", "", "DEQ_OPC_ENF_INSP_REQ", emailParams, null);
 }
-aa.sendMail("noreplyehims@suffolkcountyny.gov", "ryan.littlefield@scubeenterprise.com", "", "ALVA is running", "publicuser is " + publicUser + " and currentuserid is " + currentUserID);
+//aa.sendMail("noreplyehims@suffolkcountyny.gov", "ryan.littlefield@scubeenterprise.com", "", "ALVA is running", "publicuser is " + publicUser + " and currentuserid is " + currentUserID);
