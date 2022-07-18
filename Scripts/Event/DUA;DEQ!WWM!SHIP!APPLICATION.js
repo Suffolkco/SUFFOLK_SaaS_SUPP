40d64447ba87b226a499e37e2b2b7f9a8504dfa4
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2022-07-18 15:48:18 -0400

    updates

--- Scripts/Event/DUA;DEQ!WWM!SHIP!APPLICATION.js
+++ Scripts/Event/DUA;DEQ!WWM!SHIP!APPLICATION.js
@@ -1,10 +1,25 @@
 //DUA:DEQ/WWM/SHIP/APPLICATION
 
+
+//need to get this so that it doesn't set this status on submittal, but does each time after that
 if (publicUser)
 {
-    logDebug("capId is: " + capId);
-    if (capId.isCompleteCap())
+    if (cap.isCompleteCap())
     {
-        updateAppStatus("Resubmitted");
+        var fileDateObj = cap.getFileDate();
+        var fileDate = "" + fileDateObj.getMonth() + "/" + fileDateObj.getDayOfMonth() + "/" + fileDateObj.getYear();
+        logDebug("Filedate is: " + fileDate);
+        var currentDate = new Date();
+        var currentDateParsed = (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + (currentDate.getYear() + 1900);
+        logDebug("currentdateparsed is: " + currentDateParsed);
+
+        if (fileDate != currentDateParsed)
+        {
+            updateAppStatus("Resubmitted");
+        }
+        else
+        {
+            logDebug("not updating today's date");
+        }
     }
 }
