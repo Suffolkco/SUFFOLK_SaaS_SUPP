ed3821486fb044c977c47a368cd026bedc3909ac
Ryan Littlefield <rlittlefield@scubeenterprise.com>
2022-07-18 12:16:03 -0400

    testing

--- Scripts/Event/ALVA;DEQ!~!~!~.js
+++ Scripts/Event/ALVA;DEQ!~!~!~.js
@@ -1,14 +1,23 @@
-aa.sendMail("noreplyehims@suffolkcountyny.gov", "ryan.littlefield@scubeenterprise.com", "", "ALVA is running", "");
-
-var emailParams = aa.util.newHashtable();
-var currentUserID = aa.env.getValue("CurrentUserID");
-if (currentUserID.indexOf("PUBLICUSER") == 0)
+try
 {
-    publicUser = true;
-}
-if (publicUser)
-{
+    aa.sendMail("noreplyehims@suffolkcountyny.gov", "ryan.littlefield@scubeenterprise.com", "", "ALVA is running", "");
 
-    sendNotification("", "ryan.littlefield@scubeenterprise.com", "", "DEQ_OPC_ENF_INSP_REQ", emailParams, null);
+    var emailParams = aa.util.newHashtable();
+    var currentUserID = aa.env.getValue("CurrentUserID");
+    if (currentUserID.indexOf("PUBLICUSER") == 0)
+    {
+        publicUser = true;
+    }
+    if (publicUser)
+    {
+
+        sendNotification("", "ryan.littlefield@scubeenterprise.com", "", "DEQ_OPC_ENF_INSP_REQ", emailParams, null);
+    }
+    //aa.sendMail("noreplyehims@suffolkcountyny.gov", "ryan.littlefield@scubeenterprise.com", "", "ALVA is running", "publicuser is " + publicUser + " and currentuserid is " + currentUserID);
 }
-//aa.sendMail("noreplyehims@suffolkcountyny.gov", "ryan.littlefield@scubeenterprise.com", "", "ALVA is running", "publicuser is " + publicUser + " and currentuserid is " + currentUserID);
+
+catch (err)
+{
+    aa.print("ERROR ENCOUNTERED");
+    aa.sendMail("noreplyehims@suffolkcountyny.gov", "ryan.littlefield@scubeenterprise.com", "", "Debug From ALVA - SUFFOLK", debug + " " + err.message);
+}
