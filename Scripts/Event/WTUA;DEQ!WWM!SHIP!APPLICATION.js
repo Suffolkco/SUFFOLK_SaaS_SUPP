6a22b55e01368e218a503ca92c395d6f2a9a1fc5
Ryan Littlefield <86311547+rlittlefieldsCube@users.noreply.github.com>
2022-08-03 14:33:27 -0400

    updates

--- Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
+++ Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
@@ -1061,3 +1061,69 @@ function arrayContainsValue(ary, value) {
     }
     return false;
 }
+function copyDocumentsToCapID(fromCapID, toCapID) {
+    var opDocArray = aa.document.getDocumentListByEntity(fromCapID.toString(), "CAP").getOutput();
+    var vDocArray = opDocArray.toArray();
+    for (var vCounter in vDocArray)
+    {
+        var vDoc = vDocArray[vCounter];
+        aa.document.createDocumentAssociation(vDoc, toCapID.toString(), "CAP");
+    }
+}
+function makePIN(length) {
+    var result = '';
+    var characters = 'ABCDEFGHJKMNPQRTWXY2346789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++)
+    {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+function editAppSpecificLOCAL(itemName, itemValue)  // optional: itemCap
+{
+    var itemCap = capId;
+    var itemGroup = null;
+    if (arguments.length == 3) itemCap = arguments[2]; // use cap ID specified in args
+
+    if (useAppSpecificGroupName)
+    {
+        if (itemName.indexOf(".") < 0) {logDebug("**WARNING: (editAppSpecific) requires group name prefix when useAppSpecificGroupName is true"); return false}
+
+
+        itemGroup = itemName.substr(0, itemName.indexOf("."));
+        itemName = itemName.substr(itemName.indexOf(".") + 1);
+    }
+    // change 2/2/2018 - update using: aa.appSpecificInfo.editAppSpecInfoValue(asiField)
+    // to avoid issue when updating a blank custom form via script. It was wiping out the field alias 
+    // and replacing with the field name
+
+    var asiFieldResult = aa.appSpecificInfo.getByList(itemCap, itemName);
+    if (asiFieldResult.getSuccess())
+    {
+        var asiFieldArray = asiFieldResult.getOutput();
+        if (asiFieldArray.length > 0)
+        {
+            var asiField = asiFieldArray[0];
+            if (asiField)
+            {
+                var origAsiValue = asiField.getChecklistComment();
+                asiField.setChecklistComment(itemValue);
+
+                var updateFieldResult = aa.appSpecificInfo.editAppSpecInfoValue(asiField);
+                if (updateFieldResult.getSuccess())
+                {
+                    logDebug("Successfully updated custom field on record: " + itemCap.getCustomID() + " on " + itemName + " with value: " + itemValue);
+                    if (arguments.length < 3) //If no capId passed update the ASI Array
+                        AInfo[itemName] = itemValue;
+                }
+                else {logDebug("WARNING: (editAppSpecific) " + itemName + " was not updated.");}
+            }
+            else {logDebug("WARNING: (editAppSpecific) " + itemName + " was not updated.");}
+        }
+    }
+    else
+    {
+        logDebug("ERROR: (editAppSpecific)" + asiFieldResult.getErrorMessage());
+    }
+}
