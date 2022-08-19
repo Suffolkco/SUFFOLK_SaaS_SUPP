53bb893ac3530eaf82dbd706ff079174debd066f
Ryan Littlefield <86311547+rlittlefieldsCube@users.noreply.github.com>
2022-08-19 10:07:08 -0400

    updates

--- Scripts/Master Scripts/AddLicenseToPublicUserAfter4ACA_CUSTOM.js
+++ Scripts/Master Scripts/AddLicenseToPublicUserAfter4ACA_CUSTOM.js
@@ -1,10 +1,8 @@
 /*------------------------------------------------------------------------------------------------------/
-| Program : UniversalMasterScriptV.js
-| Event   : UniversalMasterScript
+| Program : AddLicenseToPublicUserAfter4ACA_Custom.js
+| Event   : AddLicenseToPublicUserAfter4ACA 
 |
-| Usage   : Designed to work with most events and generate a generic framework to expose standard master scirpt functionality
-|			To utilize associate UniversalMasterScript to event and create a standard choice with same name as event
-|			universal master script will execute and attempt to call standard choice with same name as associate event. 
+| Usage   : Master Script by Accela.  See accompanying documentation and release notes.
 |
 | Client  : N/A
 | Action# : N/A
@@ -14,24 +12,15 @@
 |
 /------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------/
-| START Configurable Parameters
-|	The following script code will attempt to read the assocaite event and invoker the proper standard choices
-|    
+| START User Configurable Parameters
+|
+|     Only variables in the following section may be changed.  If any other section is modified, this
+|     will no longer be considered a "Master" script and will not be supported in future releases.  If
+|     changes are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
-var triggerEvent = aa.env.getValue("EventName");
-var controlString = "AddLicenseValidationToPublicUserAfter4ACA";
-var documentOnly = false;						// Document Only -- displays hierarchy of std choice steps
-
-
-var preExecute = "PreExecuteForAfterEvents";  		//Assume after event unless before decected
-var eventType = "After";				//Assume after event
-if (triggerEvent != ""){
-	controlString = triggerEvent;			// Standard choice for control
-	if(triggerEvent.indexOf("Before") > 0){
-		preExecute = "PreExecuteForBeforeEvents";
-		eventType = "Before";
-	}
-}
+var controlString = "AddLicenseToPublicUserAfter4ACA "; // Standard choice for control
+var preExecute = "PreExecuteForAfterEvents"; // Standard choice to execute first (for globals, etc)
+var documentOnly = false; // Document Only -- displays hierarchy of std choice steps
 
 /*------------------------------------------------------------------------------------------------------/
 | END User Configurable Parameters
@@ -42,11 +31,13 @@ var useSA = false;
 var SA = null;
 var SAScript = null;
 var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
-if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I")
+{
 	useSA = true;
 	SA = bzr.getOutput().getDescription();
 	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
-	if (bzr.getSuccess()) {
+	if (bzr.getSuccess())
+	{
 		SAScript = bzr.getOutput().getDescription();
 	}
 }
@@ -55,27 +46,31 @@ var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";
 var doStdChoices = true; // compatibility default
 var doScripts = false;
 var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice).getOutput().size() > 0;
-if (bzr) {
+if (bzr)
+{
 	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "STD_CHOICE");
 	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
 	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "SCRIPT");
 	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
 	var bvr3 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "USE_MASTER_INCLUDES");
-	if (bvr3.getSuccess()) {if(bvr3.getOutput().getDescription() == "No") useCustomScriptFile = false}; 
+	if (bvr3.getSuccess()) {if (bvr3.getOutput().getDescription() == "No") useCustomScriptFile = false};
 }
 
-if (SA) {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA,useCustomScriptFile));
-	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA,useCustomScriptFile));
+if (SA)
+{
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA, useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA, useCustomScriptFile));
 	eval(getScriptText(SAScript, SA));
-} else {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useCustomScriptFile));
-	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useCustomScriptFile));
+} else
+{
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", null, useCustomScriptFile));
 }
 
-eval(getScriptText("INCLUDES_CUSTOM",null,useCustomScriptFile));  
- 
-if (documentOnly) {
+eval(getScriptText("INCLUDES_CUSTOM", null, useCustomScriptFile));
+
+if (documentOnly)
+{
 	doStandardChoiceActions(controlString, false, 0);
 	aa.env.setValue("ScriptReturnCode", "0");
 	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");
@@ -85,17 +80,21 @@ if (documentOnly) {
 var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX", vEventName);
 
 function getScriptText(vScriptName, servProvCode, useProductScripts) {
-	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();
+	if (!servProvCode) servProvCode = aa.getServiceProviderCode();
 	vScriptName = vScriptName.toUpperCase();
 	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
-	try {
-		if (useProductScripts) {
+	try
+	{
+		if (useProductScripts)
+		{
 			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
-		} else {
+		} else
+		{
 			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
 		}
 		return emseScript.getScriptText() + "";
-	} catch (err) {
+	} catch (err)
+	{
 		return "";
 	}
 }
@@ -103,22 +102,14 @@ function getScriptText(vScriptName, servProvCode, useProductScripts) {
 /*------------------------------------------------------------------------------------------------------/
 | BEGIN Event Specific Variables
 /------------------------------------------------------------------------------------------------------*/
-//Log All Environmental Variables as  globals
-var params = aa.env.getParamValues();
-var keys =  params.keys();
-var key = null;
-while(keys.hasMoreElements())
-{
- key = keys.nextElement();
- eval("var " + key + " = aa.env.getValue(\"" + key + "\");");
- logDebug("Loaded Env Variable: " + key + " = " + aa.env.getValue(key));
-}
+
 
 /*------------------------------------------------------------------------------------------------------/
 | END Event Specific Variables
 /------------------------------------------------------------------------------------------------------*/
 
-if (preExecute.length) doStandardChoiceActions(preExecute,true,0); 	// run Pre-execution code
+if (preExecute.length)
+	doStandardChoiceActions(preExecute, true, 0); // run Pre-execution code
 
 logGlobals(AInfo);
 
@@ -126,19 +117,12 @@ logGlobals(AInfo);
 | <===========Main=Loop================>
 |
 /-----------------------------------------------------------------------------------------------------*/
-//
-//  Get the Standard choices entry we'll use for this App type
-//  Then, get the action/criteria pairs for this app
-//
 
-if (doStdChoices) doStandardChoiceActions(controlString,true,0);
+if (doStdChoices)
+	doStandardChoiceActions(controlString, true, 0);
 
-
-//
-//  Next, execute and scripts that are associated to the record type
-//
-
-if (doScripts) doScriptActions();
+if (doScripts)
+	doScriptActions();
 
 // this controller replaces lookups for STANDARD_SOLUTIONS and CONFIGURABLE_RULESETS
 doConfigurableScriptActions();
@@ -147,53 +131,28 @@ doConfigurableScriptActions();
 // Check for invoicing of fees
 //
 if (feeSeqList.length)
-	{
+{
 	invoiceResult = aa.finance.createInvoice(capId, feeSeqList, paymentPeriodList);
 	if (invoiceResult.getSuccess())
-		logMessage("Invoicing assessed fee items is successful.");
+		logDebug("Invoicing assessed fee items is successful.");
 	else
-		logMessage("**ERROR: Invoicing the fee items assessed to app # " + capIDString + " was not successful.  Reason: " +  invoiceResult.getErrorMessage());
-	}
+		logDebug("**ERROR: Invoicing the fee items assessed to app # " + capIDString + " was not successful.  Reason: " + invoiceResult.getErrorMessage());
+}
 
 /*------------------------------------------------------------------------------------------------------/
 | <===========END=Main=Loop================>
 /-----------------------------------------------------------------------------------------------------*/
-if(eventType == "After"){
-	if (debug.indexOf("**ERROR") > 0)
-		{
-		aa.env.setValue("ScriptReturnCode", "1");
-		aa.env.setValue("ScriptReturnMessage", debug);
-		}
-	else
-		{
-		aa.env.setValue("ScriptReturnCode", "0");
-		if (showMessage) aa.env.setValue("ScriptReturnMessage", message);
-		if (showDebug) 	aa.env.setValue("ScriptReturnMessage", debug);
-		}
-}
-else{ //Process Before Event with cancel check
-	if (debug.indexOf("**ERROR") > 0)
-		{
-		aa.env.setValue("ScriptReturnCode", "1");
-		aa.env.setValue("ScriptReturnMessage", debug);
-		}
-	else
-		{
-		if (cancel)
-			{
-			aa.env.setValue("ScriptReturnCode", "1");
-			if (showMessage) aa.env.setValue("ScriptReturnMessage", "<font color=red><b>Action Cancelled</b></font><br><br>" + message);
-			if (showDebug) 	aa.env.setValue("ScriptReturnMessage", "<font color=red><b>Action Cancelled</b></font><br><br>" + debug);
-			}
-		else
-			{
-			aa.env.setValue("ScriptReturnCode", "0");
-			if (showMessage) aa.env.setValue("ScriptReturnMessage", message);
-			if (showDebug) 	aa.env.setValue("ScriptReturnMessage", debug);
-			}
-		}
-}
 
-/*------------------------------------------------------------------------------------------------------/
-| <===========External Functions (used by Action entries)
-/------------------------------------------------------------------------------------------------------*/
+
+if (debug.indexOf("**ERROR") > 0)
+{
+	aa.env.setValue("ScriptReturnCode", "1");
+	aa.env.setValue("ScriptReturnMessage", debug);
+} else
+{
+	aa.env.setValue("ScriptReturnCode", "0");
+	if (showMessage)
+		aa.env.setValue("ScriptReturnMessage", message);
+	if (showDebug)
+		aa.env.setValue("ScriptReturnMessage", debug);
+}
