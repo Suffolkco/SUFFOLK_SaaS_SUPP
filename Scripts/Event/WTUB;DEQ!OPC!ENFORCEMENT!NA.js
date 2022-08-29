86e299de8f1921ccb7591cf47befe051fb2f9656
Ryan Littlefield <86311547+rlittlefieldsCube@users.noreply.github.com>
2022-08-29 10:37:56 -0400

    updates

--- Scripts/Event/WTUB;DEQ!OPC!ENFORCEMENT!NA.js
+++ Scripts/Event/WTUB;DEQ!OPC!ENFORCEMENT!NA.js
@@ -9,3 +9,14 @@ if (wfTask == "End Enforcement Action" && wfStatus == "Close")
         comment("You must pay the balance due before you can close this record.");
     }
 }
+
+if (wfTask == "Enforcement Request Review" && wfStatus == "NOPH Sent")
+{
+    var enforcementType = getAppSpecific("Enforcement Type");
+    if (matches(enforcementType, "", null, undefined, " "))
+    {
+        cancel = true;
+        showMessage = true;
+        comment("You must select an Enforcement Type on the Custom Fields tab before you can advance the workflow to this status.");
+    }
+}
