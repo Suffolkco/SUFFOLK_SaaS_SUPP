6d7cace6bfdd153c491e216ecf103103629edbb5
Ada <ada.chan@suffolkcountyny.gov>
2022-08-31 10:35:42 -0400

    EHIMS-4754

--- Scripts/Event/WTUB;DEQ!WWM!COMMERCIAL!APPLICATION.js
+++ Scripts/Event/WTUB;DEQ!WWM!COMMERCIAL!APPLICATION.js
@@ -265,7 +265,7 @@ function latestCompletedInspection()
 				logDebug("inspDate: " + inspDate);			
 	
 				var year = insps[i].getInspectionDate().getYear();
-				var month = insps[i].getInspectionDate().getMonth() - 1;
+				var month = insps[i].getInspectionDate().getMonth();
 				var day = insps[i].getInspectionDate().getDayOfMonth();
 				var hr = insps[i].getInspectionDate().getHourOfDay();
 				var min = insps[i].getInspectionDate().getMinute();
