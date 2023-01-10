f6373d0c2bd6845ac6691293eea4c5dae31f4d1d
Ada <ada.chan@suffolkcountyny.gov>
2023-01-10 14:22:07 -0500

    DOCKET-8

--- Scripts/Event/ASIUA;CONSUMERAFFAIRS!DOCKET!~!~.js
+++ Scripts/Event/ASIUA;CONSUMERAFFAIRS!DOCKET!~!~.js
@@ -3,11 +3,32 @@
 var showDebug = true;
 try
 {
-	
+
+	cheatSheet = loadASITable("VIOLATION CHEAT SHEET");
+
+	var i = 0;
+	var newCheatsheet = new Array();
+	for (var c in cheatSheet)
+	{
+		var newRow = new Array();
+		newRow["Violation Date"] = cheatSheet[c]["Violation Date"];
+		newRow["Occurrence Date"] = cheatSheet[c]["Occurrence Date"];
+		newRow["Case Number"] = cheatSheet[c]["Case Number"];		
+		newRow["Charge"] = cheatSheet[c]["Charge"];
+		newRow["Create Violation"] = cheatSheet[c]["Create Violation"];
+		newRow["Withdraw Violation"] = cheatSheet[c]["Withdraw Violation"];
+		newRow["Reference Violation Number"] = cheatSheet[c]["Reference Violation Number"];
+		newRow["Abbreviated Description"] = cheatSheet[c]["Abbreviated Description"];
+		newRow["Item"] = i++;
+		newCheatsheet.push(newRow);
+		break;
+	}
+
+	addASITable("VIOLATION CHEAT SHEET", newCheatsheet, capId);
+
 }
 catch (err) {
     var showDebug = false;
 	logDebug("A JavaScript Error occured: " + err.message + " In Line " + err.lineNumber);
 	}
-
 
