3f6685dbfd839e988ca18b061306f44206a98cf0
Ada <ada.chan@suffolkcountyny.gov>
2023-01-10 10:24:34 -0500

    DOCKET-8: Violation Cheatsheet.

---
+++ Scripts/Event/ASIUA;CONSUMERAFFAIRS!DOCKET!~!~.js
@@ -0,0 +1,13 @@
+// /ASIUA:
+// This is to add the unique ID to the custom list VIOLATION CHEAT SHEET
+var showDebug = true;
+try
+{
+	
+}
+catch (err) {
+    var showDebug = false;
+	logDebug("A JavaScript Error occured: " + err.message + " In Line " + err.lineNumber);
+	}
+
+
