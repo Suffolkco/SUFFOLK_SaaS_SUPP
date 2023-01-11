b2929848509aaf11689968a946f2dbb79a9917e5
Ada <ada.chan@suffolkcountyny.gov>
2023-01-11 08:20:55 -0500

    DOCKET-8

--- Scripts/Event/ASIUB;CONSUMERAFFAIRS!DOCKET!~!~.js
+++ Scripts/Event/ASIUB;CONSUMERAFFAIRS!DOCKET!~!~.js
@@ -5,10 +5,9 @@ try
 {
 	var AInfo = new Array();						// Create array for tokenized variables
 
-	//loadASITables4ACA();
+	loadASITables4ACA();
 
-	local_loadASITables4ACA();
-	
+	loadASITablesBefore();
 	//logGlobals(AInfo);
 
 	cheatSheet = loadASITable("VIOLATION CHEAT SHEET");
@@ -38,50 +37,124 @@ catch (err) {
 	logDebug("A JavaScript Error occured: " + err.message + " In Line " + err.lineNumber);
 	}
 
-	function local_loadASITables4ACA() {
+	function loadASITables4ACA() {
+
+		//
 		// Loads App Specific tables into their own array of arrays.  Creates global array objects
-		// Optional parameter, cap ID to load from.  If no CAP Id specified, use the capModel
-		var itemCap = capId;
-		if (1 == arguments.length) {
-			itemCap = arguments[0];
-			var gm = aa.appSpecificTableScript.getAppSpecificTableGroupModel(itemCap).getOutput()
-		} else {
-			var gm = cap.getAppSpecificTableGroupModel();   
-		}
-		for (var ta = gm.getTablesMap(), tai = ta.values().iterator(); tai.hasNext();) {
-			var tsm = tai.next();
-			if (!tsm.rowIndex.isEmpty()) {
-				var tempObject = new Array,
-				tempArray = new Array,
-				tn = tsm.getTableName();
-				tn = String(tn).replace(/[^a-zA-Z0-9]+/g, ""),
-				isNaN(tn.substring(0, 1)) || (tn = "TBL" + tn);
-				for (var tsmfldi = tsm.getTableField().iterator(), tsmcoli = tsm.getColumns().iterator(), numrows = 1; tsmfldi.hasNext();) {
-					if (!tsmcoli.hasNext()) {
-						var tsmcoli = tsm.getColumns().iterator();
-						tempArray.push(tempObject);
-						var tempObject = new Array;
-						numrows++
-					}
-					var tcol = tsmcoli.next();
-					var tobj = tsmfldi.next(); 
-					var tval = ""; 
-					try { 
-						if(!tobj || !tobj.getInputValue()){
-							tval = tobj;
-						} else {
-							tval = tobj.getInputValue(); 
-						}
-					}  catch (ex) { 
-						tval = tobj; 
-					}
-					logDebug("tcol: " + tcol + ", tval: " + tval);
-					tempObject[tcol.getColumnName()] = tval;
-				}
-				tempArray.push(tempObject);
-				var copyStr = "" + tn + " = tempArray";
-				logDebug("ASI Table Array : " + tn + " (" + numrows + " Rows)"),
-				eval(copyStr)
-			}
-		}
-	}
+	   //
+	   // Optional parameter, cap ID to load from.  If no CAP Id specified, use the capModel
+	   //
+   
+	   var itemCap = capId;
+	   if (1 == arguments.length) {
+		   itemCap = arguments[0];
+		   var gm = aa.appSpecificTableScript.getAppSpecificTableGroupModel(itemCap).getOutput()
+	   } else var gm = cap.getAppSpecificTableGroupModel();
+	   for (var ta = gm.getTablesMap(), tai = ta.values().iterator(); tai.hasNext();) {
+		   var tsm = tai.next();
+		   if (!tsm.rowIndex.isEmpty()) {
+			   var tempObject = new Array,
+			   tempArray = new Array,
+			   tn = tsm.getTableName();
+			   tn = String(tn).replace(/[^a-zA-Z0-9]+/g, ""),
+			   isNaN(tn.substring(0, 1)) || (tn = "TBL" + tn);
+			   for (var tsmfldi = tsm.getTableField().iterator(), tsmcoli = tsm.getColumns().iterator(), numrows = 1; tsmfldi.hasNext();) {
+				   if (!tsmcoli.hasNext()) {
+					   var tsmcoli = tsm.getColumns().iterator();
+					   tempArray.push(tempObject);
+					   var tempObject = new Array;
+					   numrows++
+				   }
+								  
+								  var tcol = tsmcoli.next();
+								  var tobj = tsmfldi.next(); 
+								  var tval = ""; 
+								  try 
+								  { 
+										 tval = tobj.getInputValue(); 
+										 logDebug("tval: " + tval);
+								  } 
+								  catch (ex) 
+								  { 
+										 tval = tobj; 
+								  }
+								  tempObject[tcol.getColumnName()] = tval;
+			   }
+			   tempArray.push(tempObject);
+			   var copyStr = "" + tn + " = tempArray";
+			   logDebug("ASI Table Array : " + tn + " (" + numrows + " Rows)"),
+			   eval(copyStr)
+		   }
+	   }
+   }
+	
+	
+   function loadASITablesBefore() {
+   
+	   //
+	   // Loads App Specific tables into their own array of arrays.  Creates global array objects
+	   //
+	   //Sometimes "AppSpecificTableGroupModel" is a list
+	   var gm = aa.env.getValue("AppSpecificTableGroupModel");
+   
+	   var gmItem = gm;
+   
+	   if (gm != null && typeof(gm).size != "undefined" && gm.size() > 0) {
+		   gmItem = gm.get(0);
+	   } else {
+		   gmItem = gm;
+	   }
+   
+	   if (null != gmItem && gmItem != "") {
+		   var ta = gmItem.getTablesMap().values();
+		   var tai = ta.iterator();
+		   while (tai.hasNext()) {
+			   var tsm = tai.next();
+   
+			   if (tsm.rowIndex.isEmpty())
+				   continue; // empty table
+   
+			   var tempObject = new Array();
+			   var tempArray = new Array();
+			   var tn = tsm.getTableName();
+   
+			   var numrows = 0;
+			   tn = String(tn).replace(/[^a-zA-Z0-9]+/g, '');
+   
+			   if (!isNaN(tn.substring(0, 1)))
+				   tn = "TBL" + tn // prepend with TBL if it starts with a number
+   
+					   if (!tsm.rowIndex.isEmpty()) {
+						   var tsmfldi = tsm.getTableField().iterator();
+						   var tsmcoli = tsm.getColumns().iterator();
+   
+						   var numrows = 1;
+						   while (tsmfldi.hasNext()) // cycle through fields
+						   {
+							   if (!tsmcoli.hasNext()) // cycle through columns
+							   {
+   
+								   var tsmcoli = tsm.getColumns().iterator();
+								   tempArray.push(tempObject); // end of record
+								   var tempObject = new Array(); // clear the temp obj
+								   numrows++;
+							   }
+							   var tcol = tsmcoli.next();
+							   var tval = tsmfldi.next();
+							   var readOnly = 'N';
+							   var fieldInfo = new asiTableValObj(tcol.getColumnName(), tval, readOnly);
+							   logDebug("fieldInfo: " + fieldInfo);
+							   tempObject[tcol.getColumnName()] = fieldInfo;
+   
+						   }
+   
+						   tempArray.push(tempObject); // end of record
+					   }
+   
+					   var copyStr = "" + tn + " = tempArray";
+			   aa.print("ASI Table Array : " + tn + " (" + numrows + " Rows)");
+			   eval(copyStr); // move to table name
+		   }
+	   }
+   } 
+	
