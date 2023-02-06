54a506196d12b047c4040b72ee2e04a5cac7838d
Ada <ada.chan@suffolkcountyny.gov>
2023-02-06 10:48:51 -0500

    EHIMS-4971: ACA created records do not assess fee until certain stage of completion

--- Scripts/Event/CTRCA;DEQ!WWM!RESIDENCE!APPLICATION.js
+++ Scripts/Event/CTRCA;DEQ!WWM!RESIDENCE!APPLICATION.js
@@ -1,5 +1,67 @@
 //CTRCA:DEQ/WWM/RESIDENCE/APPLICATION
 
+//ASA:DEQ/WWM/RESIDENCE/APPLICATION
+
+var methWat = AInfo["Method of Water Supply"];
+var methSew = AInfo["Method of Sewage Disposal"];
+var feeEx = AInfo["Fee Exempt"];
+var countyState = AInfo["Is this application part of the County/State I/A OWTS SIP grant/loan program?"]
+
+if (feeEx == "No" || feeEx == null )
+{
+	if (methSew.equals("Public Sewers")  ||
+	methSew.equals("Construct an STP") ||
+	methSew.equals("Existing STP"))
+	{
+		updateFee("RES-PW-PS", "DEQ_SFR", "FINAL", 1, "Y");
+	}
+	else
+	{
+		updateFee("RES-PW-PS", "DEQ_SFR", "FINAL", 0, "Y");
+	}
+	if (methWat.equals("Private Well"))
+	{
+		updateFee("RES-WELL-OWT", "DEQ_SFR", "FINAL", 1, "Y");
+		
+		if (feeExists("RES-PW-OWTS"))
+		{
+			updateFee("RES-PW-OWTS", "DEQ_SFR", "FINAL", 0, "Y");
+		}
+	}
+	else if (methWat.equals("Public Water"))
+	{
+		if (feeExists("RES-WELL-OWT"))
+		{
+			updateFee("RES-WELL-OWT", "DEQ_SFR", "FINAL", 0, "Y");
+		}
+		if (methSew.equals("Conventional Septic System") || methSew.equals("I/A System"))
+		{
+			updateFee("RES-PW-OWTS", "DEQ_SFR", "FINAL", 1, "Y");
+			
+		}
+		else
+		{
+			updateFee("RES-PW-OWTS", "DEQ_SFR", "FINAL", 0, "Y");
+		}
+	} 
+	else 
+	{
+		logDebug("No Fee.");
+	}
+}
+
+removeBORFees(capId);
+
+// Add BOR fee if the custom field is set to Yes by public user
+var borDocCheck = determineACADocumentAttached("Board of Review Application");    
+
+if(borDocCheck)
+{       
+	addFee("BOR", "DEQ_SFR", "FINAL", 1, "Y");
+}
+
+
+
 var b1ExpResult = aa.expiration.getLicensesByCapID(capId);
 if (b1ExpResult.getSuccess())
 {
