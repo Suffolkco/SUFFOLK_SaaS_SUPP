80ac29f47d7994bc9116b3d47bd02d1af8afd40f
Ada <ada.chan@suffolkcountyny.gov>
2023-02-06 10:56:29 -0500

    EHIMS-4971: ACA created records do not assess fee until certain stage of completion

--- Scripts/Event/CTRCA;DEQ!WWM!RESIDENCE!APPLICATION.js
+++ Scripts/Event/CTRCA;DEQ!WWM!RESIDENCE!APPLICATION.js
@@ -99,3 +99,35 @@ function jsDateToMMDDYYYY(pJavaScriptDate) {
 		return ("NULL PARAMETER VALUE");
 	}
 }
+function removeBORFees(itemCap) 
+{
+	getFeeResult = aa.fee.getFeeItems(itemCap, null, "NEW");
+	if (getFeeResult.getSuccess()) 
+	{
+		var feeList = getFeeResult.getOutput();
+		for (feeNum in feeList) 
+		{
+			if (feeList[feeNum].getFeeitemStatus().equals("NEW")) 
+			{
+				
+				if (feeList[feeNum].getFeeCod() == "BOR")
+				{
+					var feeSeq = feeList[feeNum].getFeeSeqNbr();
+					var editResult = aa.finance.removeFeeItem(itemCap, feeSeq);
+					if (editResult.getSuccess()) {
+						logDebug("Removed existing Fee Item: " + feeList[feeNum].getFeeCod());
+					} else {
+						logDebug("**ERROR: removing fee item (" + feeList[feeNum].getFeeCod() + "): " + editResult.getErrorMessage());
+						break
+					}
+				}
+				
+			}
+			
+		}
+	} 
+	else {
+		logDebug("**ERROR: getting fee items (" + feeList[feeNum].getFeeCod() + "): " + getFeeResult.getErrorMessage())
+	}
+
+} 
