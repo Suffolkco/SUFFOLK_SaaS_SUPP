628c0fcb1edc85f7d5da5c1656f9cf739b157dae
Ryan Littlefield <86311547+rlittlefieldsCube@users.noreply.github.com>
2023-02-15 15:29:52 -0500

    EHIMS2-180

---
+++ Scripts/Pageflow/PF_DEQ_REQ_IA_SERV_PROV.js
@@ -0,0 +1,108 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program : PF_DEQ_REQ_IA_DESIGNER
+| Event   : ACA_BEFORE Event
+|
+|
+| 
+| Author:RLittlefield
+/------------------------------------------------------------------------------------------------------*/
+
+var documentOnly = false;
+var disableTokens = false;
+var useAppSpecificGroupName = false;
+var useTaskSpecificGroupName = false;
+var enableVariableBranching = false;
+var maxEntries = 99;
+var cancel = false;
+var startDate = new Date();
+var startTime = startDate.getTime();
+var message = "";
+var debug = "";
+var br = "<br>";
+var showDebug = false;
+var showMessage = false;
+//Needed to load the includes files
+//This way we are not re-defining everything over and over.
+eval(getScriptText("INCLUDES_CUSTOM", null, true));
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", null, true));
+var cap = aa.env.getValue("CapModel");
+var capId = cap.getCapID();
+var AInfo = new Array();
+loadAppSpecific4ACA(AInfo);
+try
+{
+
+    // Require LP
+    var lpList = cap.getLicenseProfessionalList();
+    var correctType = false;
+
+    if (lpList != null)
+    {
+        for (i = 0; i < lpList.size(); i++)
+        {
+            logDebug("license type is: " + lpList.get(i).getLicenseType());
+            if (lpList.get(i).getLicenseType() == "IA Service Provider")
+            {
+                correctType = true;
+                break;
+            }
+        }
+    }
+
+    if (correctType == false)
+    {
+        cancel = true;
+        showMessage = true;
+        comment("You have chosen the incorrect License Type. Click ‘Create an Application’ to return to the previous page and select the ‘IA Service Provider’ option from the Licenses drop down. If this option is not available, please contact us as IAOWTSRME@suffolkcontyny.gov.");
+    }
+
+
+
+}
+catch (error)
+{
+    logDebug("an error was encoutered: " + error.message);
+    showDebug = true;
+    showMessage = true;
+}
+
+if (debug.indexOf("**ERROR") > 0 || debug.substr(0, 7) == "**ERROR")
+{
+    aa.env.setValue("ErrorCode", "1");
+    aa.env.setValue("ErrorMessage", debug);
+} else
+{
+    if (cancel)
+    {
+        aa.env.setValue("ErrorCode", "-2");
+        if (showMessage) aa.env.setValue("ErrorMessage", message);
+        if (showDebug) aa.env.setValue("ErrorMessage", debug);
+    } else
+    {
+        aa.env.setValue("ErrorCode", "0");
+        if (showMessage) aa.env.setValue("ErrorMessage", message);
+        if (showDebug) aa.env.setValue("ErrorMessage", debug);
+    }
+}
+
+
+//ACA Functions 
+function getScriptText(vScriptName, servProvCode, useProductScripts) {
+    if (!servProvCode) servProvCode = aa.getServiceProviderCode();
+    vScriptName = vScriptName.toUpperCase();
+    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+    try
+    {
+        if (useProductScripts)
+        {
+            var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+        } else
+        {
+            var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+        }
+        return emseScript.getScriptText() + "";
+    } catch (err)
+    {
+        return "";
+    }
+}
