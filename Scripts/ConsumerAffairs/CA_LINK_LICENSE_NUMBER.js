3c96ae56b535cc633d8f39d871ea7759f338d647
Ada <ada.chan@suffolkcountyny.gov>
2023-04-24 14:47:48 -0400

    Docket

--- Scripts/ConsumerAffairs/CA_LINK_LICENSE_NUMBER.js
+++ Scripts/ConsumerAffairs/CA_LINK_LICENSE_NUMBER.js
@@ -8,51 +8,44 @@ var licenseNumber = getAppSpecific("License Number", capId);
 		if (capLicResult.getSuccess()) {
 			licCapId = capLicResult.getOutput();
 
-			// License as Parent -> Complaint 
+			// License as Parent -> Complaint as Child
 			var capComplaintResult = aa.cap.getCapID(complaintNumber);
 			if (capComplaintResult.getSuccess()) {
 				recCapId = capComplaintResult.getOutput();
 
 				var linkResult = aa.cap.createAppHierarchy(licCapId, recCapId);
 				if (linkResult.getSuccess())
-					logDebug("Successfully linked to Complaint Record: " + recCapId);
+					logDebug("Successfully linked Complaint Record as child of license record: " + recCapId);
 				else
 					logDebug( "**ERROR: linking to parent application parent cap id (" + licCapId + "): " + linkResult.getErrorMessage());
 
 			}
 
-			// Complaint as Parent -> Docket 
+			// Complaint as Parent -> Docket as Child
 			var linkLResult = aa.cap.createAppHierarchy(recCapId, capId);
 			if (linkLResult.getSuccess())
-				logDebug("Successfully linked to Parent License Record : " + licCapId);
+				logDebug("Successfully linked to Docket Record as child of Complaint record : " + recCapId);
 			else
-				logDebug( "**ERROR: linking to parent application parent cap id (" + licCapId + "): " + linkLResult.getErrorMessage());
+				logDebug( "**ERROR: linking to parent application parent cap id (" + recCapId + "): " + linkLResult.getErrorMessage());
 
-				// Link Complaint Number		
+					
 		}
 	}
-	else // No license number
+	else // No license number. Just link docket to complaint record.
 	{
-		// Complaint as Parent -> Docket
+		// Complaint as Parent -> Docket as Child
 		var capComplaintResult = aa.cap.getCapID(complaintNumber);
 		if (capComplaintResult.getSuccess()) {
 			recCapId = capComplaintResult.getOutput();
 
-			var linkResult = aa.cap.createAppHierarchy(licCapId, recCapId);
+			var linkResult = aa.cap.createAppHierarchy(recCapId, capId);
 			if (linkResult.getSuccess())
-				logDebug("Successfully linked to Complaint Record: " + recCapId);
+				logDebug("Successfully linked Docket record as child to Complaint Record: " + recCapId);
 			else
-				logDebug( "**ERROR: linking to parent application parent cap id (" + licCapId + "): " + linkResult.getErrorMessage());
+				logDebug( "**ERROR: linking to parent application parent cap id (" + recCapId + "): " + linkResult.getErrorMessage());
 
 		}
-
-		// Complaint as Parent -> Docket 
-		var linkLResult = aa.cap.createAppHierarchy(recCapId, capId);
-		if (linkLResult.getSuccess())
-			logDebug("Successfully linked to Parent License Record : " + licCapId);
-		else
-			logDebug( "**ERROR: linking to parent application parent cap id (" + licCapId + "): " + linkLResult.getErrorMessage());
-
+		
 	}
 
 	
