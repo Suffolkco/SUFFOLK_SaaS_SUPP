fb524fd8cdc0da548008dcd947086396511fb0e4
Ada <ada.chan@suffolkcountyny.gov>
2023-05-01 14:20:16 -0400

    DAP-587: Update on Online Renewal Process- Email Status Update

--- Scripts/Event/ASA;CONSUMERAFFAIRS!~!RENEWAL!NA.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!~!RENEWAL!NA.js
@@ -51,6 +51,45 @@ if (conArray.length < 1)
     //copyAddresses(parentCapId, capId); 
     copyParcels(parentCapId, capId);
     copyParcelGisObjects();
+
+    // DAP-587: Send email to vendor that we have received the renewal by mail
+    if (!publicUser)
+    {
+    try{
+       
+        var emailText = ""
+            var emailParams = aa.util.newHashtable();
+            var reportFile = new Array();
+                
+            var contactType = "Vendor";
+            var contactInfo = getContactInfo(contactType, capId);
+            if(contactInfo == false){
+                logDebug("No vendor contact exists on this record");
+            }else{
+                
+               
+                // copy Vendor name, org name & phone to short notes
+                var fName = contactInfo[4];
+                var lName = contactInfo[5];					
+                var email = contactInfo[8];	
+        
+               
+                getRecordParams4Notification(emailParams);
+              
+
+                addParameter(emailParams, "$$altID$$", parentCapId.getCustomID());
+                addParameter(emailParams, "$$name$$", fName + " " + lName);
+                logDebug("fName:" + fName);
+                logDebug("parentCapId.getCustomID():" + parentCapId.getCustomID());		
+        
+                var success = sendNotification("", email, "", "DCA_RENEWAL_VENDOR_NOTIFICATION", emailParams, reportFile);	
+                logDebug("success:" + success + ", to: " + email);		
+            }
+            
+        }catch(err){
+            logDebug("**WARN: Error in ASA sending email to vendor -  " + err.message);
+        }
+    }
 } 
 
 function editAppSpecificLOCAL(itemName, itemValue)  // optional: itemCap
