50c69a21ab2726c5b34fbbd82b59654034384e19
Ada <ada.chan@suffolkcountyny.gov>
2023-05-01 15:00:25 -0400

    DAP-587

--- Scripts/Event/CTRCA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
+++ Scripts/Event/CTRCA;CONSUMERAFFAIRS!LICENSES!~!RENEWAL.js
@@ -79,6 +79,50 @@ if (!publicUser)
 
 }
 
+function getContactInfo(cType, capId) {
+    var returnArray = new Array();
+    var haveCType = false;
+    
+    var contModel = null; 
+    var consResult = aa.people.getCapContactByCapID(capId);	
+    if (consResult.getSuccess()) {
+        var cons = consResult.getOutput();
+        for (thisCon in cons) {
+            var capContactType = cons[thisCon].getCapContactModel().getPeople().getContactType();
+            if (capContactType == cType) {				
+                var contModel = cons[thisCon].getCapContactModel(); 
+                
+                var firstName = contModel.getFirstName();
+                var lastName = contModel.getLastName();
+                var business = contModel.getBusinessName();
+                var phone = contModel.getPhone1();
+                var addr1 = contModel.getAddressLine1();
+                var city = contModel.getCity();
+                var state = contModel.getState();
+                var zip = contModel.getZip();
+                var email = contModel.getPeople().getEmail();
+
+            
+                // build returnArray
+                returnArray.push(addr1);
+                returnArray.push(city);
+                returnArray.push(state);
+                returnArray.push(zip);
+                returnArray.push(firstName);
+                returnArray.push(lastName);
+                returnArray.push(business);
+                returnArray.push(phone);
+                returnArray.push(email);
+                return returnArray;
+                haveCType = true;
+            }
+        }
+    }
+    if (haveCType == false){
+        return false;
+    }
+}
+
 function checkForFee(pCapId, pFeeCode)
 
     {
