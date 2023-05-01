3901573ef36caf2d1b859dfbb56619bdaa554b3c
Ada <ada.chan@suffolkcountyny.gov>
2023-05-01 15:07:46 -0400

    DAP-587:

--- Scripts/Event/CTRCA;CONSUMERAFFAIRS!REGISTRATIONS!~!RENEWAL.js
+++ Scripts/Event/CTRCA;CONSUMERAFFAIRS!REGISTRATIONS!~!RENEWAL.js
@@ -1,83 +1,49 @@
 //CTRCA
 
-include("CA_REN_TO_LIC_SUBMITTAL");
-
-aa.cap.updateAccessByACA(capId, "Y");
-
+// DAP-587: Send email to vendor that we have received the renewal by mail
 if (!publicUser)
 {
-    if (!appMatch("ConsumerAffairs/Licenses/Dry Cleaning/Renewal") && !appMatch("ConsumerAffairs/Licenses/Restricted Electrical/Renewal") && !appMatch("ConsumerAffairs/Licenses/Restricted Plumbing/Renewal"))
-    {
-        logDebug("app is not Dry Cleaning");
-        addFee("LIC_REN_01", "CA_LIC_REN", "FINAL", 1, "Y");
-        logDebug("Added Renewal Fee");
-    }
-    if (appMatch("ConsumerAffairs/Licenses/Dry Cleaning/Renewal"))
-    {
-        logDebug("app is Dry Cleaning");
-        var dryCleanerExempt = checkForFee(parentCapId, "LIC_25")
-
-
-        if (!dryCleanerExempt) 
-        {
-            logDebug("should not be exempt from fee");
-            addFee("LIC_REN_01", "CA_LIC_REN", "FINAL", 1, "Y")
-            logDebug("Added Renewal Fee") 
-        }
-    }
-    if (appMatch("ConsumerAffairs/Licenses/Restricted Electrical/Renewal"))
-    {
-        addFee("LIC_09", "CA_LICENSE", "FINAL", 1, "Y") 
-    }
-    if (appMatch("ConsumerAffairs/Licenses/Restricted Plumbing/Renewal")) 
-    {
-        addFee("LIC_18", "CA_LICENSE", "FINAL", 1, "Y") 
-    }
+try{
     
-    // DAP-587: Send email to vendor that we have received the renewal by mail
-    if (!publicUser)
+    if ((appTypeArray[1] == "Licenses") ||(appTypeArray[1] == "ID Cards") || (appTypeArray[1] == "Registration"))
     {
-    try{
-       
-        if ((appTypeArray[1] == "Licenses") ||(appTypeArray[1] == "ID Cards") || (appTypeArray[1] == "Registration"))
-        {
-            var emailText = ""
+        var emailText = ""
+    
+        var parentCapId = getParentCapID4Renewal();
+            var emailParams = aa.util.newHashtable();
+            var reportFile = new Array();
+                
+            var contactType = "Vendor";
+            var contactInfo = getContactInfo(contactType, capId);
+            if(contactInfo == false){
+                logDebug("No vendor contact exists on this record");
+            }else{
+                
+            
+                // copy Vendor name, org name & phone to short notes
+                var fName = contactInfo[4];
+                var lName = contactInfo[5];					
+                var email = contactInfo[8];	
         
-            var parentCapId = getParentCapID4Renewal();
-                var emailParams = aa.util.newHashtable();
-                var reportFile = new Array();
-                    
-                var contactType = "Vendor";
-                var contactInfo = getContactInfo(contactType, capId);
-                if(contactInfo == false){
-                    logDebug("No vendor contact exists on this record");
-                }else{
-                    
-                
-                    // copy Vendor name, org name & phone to short notes
-                    var fName = contactInfo[4];
-                    var lName = contactInfo[5];					
-                    var email = contactInfo[8];	
             
-                
-                    getRecordParams4Notification(emailParams);
-                    logDebug("parentCapId:" + parentCapId);
-                    addParameter(emailParams, "$$altID$$", parentCapId.getCustomID());
-                    addParameter(emailParams, "$$name$$", fName + " " + lName);
-                    logDebug("fName:" + fName);
-                    logDebug("parentCapId.getCustomID():" + parentCapId.getCustomID());		
-            
-                    var success = sendNotification("", email, "", "DCA_RENEWAL_VENDOR_NOTIFICATION", emailParams, reportFile);	
-                    logDebug("success:" + success + ", to: " + email);		
-                }
+                getRecordParams4Notification(emailParams);
+                logDebug("parentCapId:" + parentCapId);
+                addParameter(emailParams, "$$altID$$", parentCapId.getCustomID());
+                addParameter(emailParams, "$$name$$", fName + " " + lName);
+                logDebug("fName:" + fName);
+                logDebug("parentCapId.getCustomID():" + parentCapId.getCustomID());		
+        
+                var success = sendNotification("", email, "", "DCA_RENEWAL_VENDOR_NOTIFICATION", emailParams, reportFile);	
+                logDebug("success:" + success + ", to: " + email);		
             }
-            
-        }catch(err){
-            logDebug("**WARN: Error in ASA sending email to vendor -  " + err.message);
         }
+        
+    }catch(err){
+        logDebug("**WARN: Error in ASA sending email to vendor -  " + err.message);
     }
-
 }
+
+
 
 function getContactInfo(cType, capId) {
     var returnArray = new Array();
