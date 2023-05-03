f2d828374a57704c3d9b45c7343fe4f8019c91d3
bhandhavya <bhandhavya.nadagoud@scubeenterprise.com>
2023-05-03 12:31:19 -0400

    Update ACA TAX MAP VALIDATION.js
    Fixed Syntax issue

--- Scripts/Pageflow/ACA TAX MAP VALIDATION.js
+++ Scripts/Pageflow/ACA TAX MAP VALIDATION.js
@@ -244,7 +244,7 @@ try
 				comment("Please enter at least one deeded property owner information below in order to continue");
 			}
 	}
-}
+
 }
 catch (ex)
 {
@@ -356,4 +356,163 @@ function getParcel(capId)
     capParcelArr = null;  
   }
   return capParcelArr;
+}
+
+
+
+//***********************************************************************
+//------------------------HELPER FUNCTIONS-------------------------------
+//***********************************************************************
+function exists(eVal, eArray) {
+	for (ii in eArray) {
+		if (eArray[ii] == eVal)
+			return true;
+	}
+
+	return false;
+}
+
+function matches(eVal, argList) {
+	for (var i = 1; i < arguments.length; i++) {
+		if (arguments[i] == eVal) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+function getAppSpecificInfo(capId) {
+	capAppSpecificInfo = null;
+	var s_result = aa.appSpecificInfo.getByCapID(capId);
+
+	if (s_result.getSuccess()) {
+		capAppSpecificInfo = s_result.getOutput();
+
+		if (capAppSpecificInfo == null || capAppSpecificInfo.length == 0) {
+			aa.print("WARNING: no appSpecificInfo on this CAP:" + capId);
+			capAppSpecificInfo = null;
+		}
+	} else {
+		aa.print("ERROR: Failed to appSpecificInfo: " + s_result.getErrorMessage());
+		capAppSpecificInfo = null;
+	}
+	// Return AppSpecificInfoModel[]
+	return capAppSpecificInfo;
+}
+
+function loadAppSpecific4ACA(thisArr) {
+	// Returns an associative array of App Specific Info
+	// Optional second parameter, cap ID to load from
+	//
+	// uses capModel in this event
+
+	var itemCap = capId;
+
+	if (arguments.length >= 2) {
+		itemCap = arguments[1]; // use cap ID specified in args
+
+		var fAppSpecInfoObj = aa.appSpecificInfo.getByCapID(itemCap).getOutput();
+
+		for (loopk in fAppSpecInfoObj) {
+			if (useAppSpecificGroupName) {
+				thisArr[fAppSpecInfoObj[loopk].getCheckboxType() + "." + fAppSpecInfoObj[loopk].checkboxDesc] = fAppSpecInfoObj[loopk].checklistComment;
+			} else {
+				thisArr[fAppSpecInfoObj[loopk].checkboxDesc] = fAppSpecInfoObj[loopk].checklistComment;
+			}
+		}
+	} else {
+		var capASI = capModel.getAppSpecificInfoGroups();
+
+		if (!capASI) {
+			logDebug("No ASI for the CapModel");
+		} else {
+			var i = capModel.getAppSpecificInfoGroups().iterator();
+			while (i.hasNext()) {
+				var group = i.next();
+				var fields = group.getFields();
+
+				if (fields != null) {
+					var iteFields = fields.iterator();
+					while (iteFields.hasNext()) {
+						var field = iteFields.next();
+
+						if (useAppSpecificGroupName) {
+							thisArr[field.getCheckboxType() + "." + field.getCheckboxDesc()] = field.getChecklistComment();
+						} else {
+							thisArr[field.getCheckboxDesc()] = field.getChecklistComment();
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+function checkForLPs() {
+	var capLPResult = capModel.getLicenseProfessionalList();
+
+	if (capLPResult != null && !capLPResult.isEmpty()) {
+		var capLPsArray = capLPResult.toArray();
+
+		/*	CHECK AND FLAG UPLOADED DOCUMENTS */
+		for (lp in capLPsArray) {
+			primaryContractor = capLPsArray[lp].getLicenseType();
+
+			if (exists(capLPsArray[lp].getLicenseType(), allowedContractorTypes)) {
+				//debug += lp + ".- LICENSE TYPE FOUND: " + capLPsArray[lp].getLicenseType() + " for Accessory Type: " + accessoryType + br;
+				allowApplication = true;
+				break;
+			}
+		}
+	} else {
+		cancel = true;
+		showMessage = true;
+		noLpsfound = true;
+	}
+}
+
+function getASITable4ACA(tableName) {
+	var gm = null;
+	if (String(cap.getClass()).indexOf("CapScriptModel") != -1) {
+		gm = cap.getCapModel().getAppSpecificTableGroupModel();
+	} else {
+		gm = cap.getAppSpecificTableGroupModel();
+	}
+	if (gm == null) {
+		return false;
+	}
+	var ta = gm.getTablesMap();
+	var tai = ta.values().iterator();
+	while (tai.hasNext()) {
+		var tsm = tai.next();
+		if (tsm.rowIndex.isEmpty())
+			continue;
+
+		var asitRow = new Array;
+		var asitTables = new Array;
+		var tn = tsm.getTableName();
+		if (tn != tableName) {
+			continue;
+		}
+
+		var tsmfldi = tsm.getTableField().iterator();
+		var tsmcoli = tsm.getColumns().iterator();
+		while (tsmfldi.hasNext()) {
+
+			var tcol = tsmcoli.next();
+			var tval = tsmfldi.next();
+
+			asitRow[tcol.getColumnName()] = tval;
+
+			if (!tsmcoli.hasNext()) {
+				tsmcoli = tsm.getColumns().iterator();
+				asitTables.push(asitRow);
+				asitRow = new Array;
+			}
+		}
+		return asitTables;
+	}
+	return false;
 }
