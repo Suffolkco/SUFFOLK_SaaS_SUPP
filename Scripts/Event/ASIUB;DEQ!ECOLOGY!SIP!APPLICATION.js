f5017b66c5e239b7ea6788f1ad708c099ed818e0
bhandhavya <bhandhavya.nadagoud@scubeenterprise.com>
2023-05-03 12:50:18 -0400

    Create ASIUB;DEQ!ECOLOGY!SIP!APPLICATION.js
    Doc changes for SIP record. To avoid adding rows manually

---
+++ Scripts/Event/ASIUB;DEQ!ECOLOGY!SIP!APPLICATION.js
@@ -0,0 +1,41 @@
+
+try 
+{
+
+                if((typeof (DEQSIPGRANTELIGIBILITY) == "object") &&  DEQSIPGRANTELIGIBILITY.length > 0)
+	{
+                     for (var vRowind in DEQSIPGRANTELIGIBILITY) 
+		{
+						var docRow = DEQSIPGRANTELIGIBILITY[vRowind];
+						var docType = String(docRow["Document Type"]);
+					  
+
+						if (docType == ""  || docType == null) 
+						{
+							cancel = true;
+							showMessage = true;
+							comment("A row cannot be manually added to this table");
+						}
+        }
+     }
+               
+                     if((typeof (DEQSIPGRANTPAYMENT) == "object") &&  DEQSIPGRANTPAYMENT.length > 0)
+	{
+                     for (var vRowind in DEQSIPGRANTPAYMENT) 
+		{
+						var docRow = DEQSIPGRANTPAYMENT[vRowind];
+						var docType = String(docRow["Document Type"]);
+					  
+
+						if (docType == ""  || docType == null) 
+						{
+							cancel = true;
+							showMessage = true;
+							comment("A row cannot be manually added to this table");
+						}
+        }
+     }   
+	
+} catch (err) {
+    logDebug("A JavaScript Error occurred: ASIUB:Building/*/*/* : " + err.message);
+}
