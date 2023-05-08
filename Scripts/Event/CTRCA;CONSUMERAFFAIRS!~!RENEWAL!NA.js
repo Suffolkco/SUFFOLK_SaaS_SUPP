2f2aeae079d37f12bad11b6a48348c48175bcdc7
Ada <ada.chan@suffolkcountyny.gov>
2023-05-08 08:27:04 -0400

    Fixed fee error.

--- Scripts/Event/CTRCA;CONSUMERAFFAIRS!~!RENEWAL!NA.js
+++ Scripts/Event/CTRCA;CONSUMERAFFAIRS!~!RENEWAL!NA.js
@@ -3,11 +3,12 @@ aa.cap.updateAccessByACA(capId, "Y");
 
 include("CA_REN_TO_LIC_SUBMITTAL");
 
+logDebug("appTypeArray[1]: " + appTypeArray[1]);
 
-/*if (!publicUser)
+(!publicUser)
 {
-    if ((appTypeArray[1] != "ID Cards") || (appTypeArray[1] != "Registration"))
+    if (matches(appTypeArray[1], "Licenses"))     
     {
     addFee("LIC_REN_01", "CA_LIC_REN", "FINAL", 1, "Y")
     }
-}*/ 
+} 
