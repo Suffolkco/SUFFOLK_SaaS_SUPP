d268ef969b533dbe102f2e00c917a9366c5341be
Ada <ada.chan@suffolkcountyny.gov>
2023-05-10 08:58:32 -0400

    DAP-559 Bounce Check-Expiration Date Issue

--- Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
+++ Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
@@ -9,6 +9,35 @@ if (matches(appTypeArray[1], "Registrations", "ID Cards", "Licenses")) {
 
 } 
 
+// DAP-559
+if (matches(appTypeArray[1], "Licenses") && appTypeArray[2] != "Renewal" && matches(appTypeArray[3], "NA"))
+{
+	if ( wfTask == "Issuance" && wfStatus == "Invalid Payment")
+	{
+		var expDateASI = getAppSpecific("Expiration Date", capId);
+
+		//Updating Expiration Date of License	
+		logDebug("Current ASI Expdate is: " + expDateASI);
+		
+		var expDate = new Date(expDateASI);
+		logDebug("expDate's date is " + expDate);	
+		var newExpDate = moveBackDate(expDate);
+		logDebug("New expiration date is " + newExpDate);
+
+		editAppSpecific("Expiration Date", newExpDate, capId);
+		var b1ExpResult = aa.expiration.getLicensesByCapID(capId);
+		if (b1ExpResult.getSuccess())
+		{
+			var b1Exp = b1ExpResult.getOutput();
+			b1Exp.setExpStatus("Expired");
+			b1Exp.setExpDate(aa.date.parseDate(newExpDate));
+			aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
+		} 
+		
+	}
+}
+
+
 logDebug("appTypeString  is " + appTypeString);
 // DAP-362
 if (matches(appTypeArray[1], "Registrations", "Licenses") && appTypeArray[2] != "Renewal" && matches(appTypeArray[3], "NA"))
@@ -41,11 +70,24 @@ if (matches(appTypeArray[1], "Registrations", "Licenses") && appTypeArray[2] != 
 	}
 
 } 
+function moveBackDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear()-2;
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [month, day, year].join('/');
+}
 function formatDate(date) {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
-        year = d.getFullYear();
+        year = d.getFullYear()-2;
 
     if (month.length < 2) 
         month = '0' + month;
