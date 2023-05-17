3cd62cde48a45507b75d9fb5c114282d46a53f40
Ada <ada.chan@suffolkcountyny.gov>
2023-05-17 08:20:04 -0400

    DAP-559: Bounce Check-Expiration Date Issue

--- Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
+++ Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
@@ -87,7 +87,7 @@ function formatDate(date) {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
-        year = d.getFullYear()-2;
+        year = d.getFullYear();
 
     if (month.length < 2) 
         month = '0' + month;
