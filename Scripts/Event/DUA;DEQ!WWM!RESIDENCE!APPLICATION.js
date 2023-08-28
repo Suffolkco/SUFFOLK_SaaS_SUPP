0ceaca993ac24303ff1f5632e68515b1bf681de5
Ada <ada.chan@suffolkcountyny.gov>
2023-08-28 10:53:52 -0400

    EHIMS-5115: Do Not Update Application Status to "Resubmitted" if Application Status is Already "Resubmitted"

--- Scripts/Event/DUA;DEQ!WWM!RESIDENCE!APPLICATION.js
+++ Scripts/Event/DUA;DEQ!WWM!RESIDENCE!APPLICATION.js
@@ -2,37 +2,8 @@
 if (publicUser)
 { 
     var appStatus = getAppStatus(capId);
-       
-    if (isTaskActive("Application Review") && appStatus != "Received")
-    {
-        updateTask("Application Review", "Resubmitted", "Additional information submitted by Applicant", "Additional information submitted by Applicant");
-        updateAppStatus("Resubmitted");
-    }
-
-    if (isTaskActive("Plans Distribution") && isTaskStatus("Plans Distribution","Awaiting Client Reply"))
-    {
-        updateTask("Plans Distribution", "Resubmitted", "Additional information submitted by Applicant", "Additional information submitted by Applicant");
-        updateAppStatus("Resubmitted");
-    }
-
-    if (isTaskActive("Inspections"))
-    {
-        updateTask("Inspections", "Resubmitted", "Additional information submitted by Applicant", "Additional information submitted by Applicant");
-        updateAppStatus("Resubmitted");
-    }
-
-    if (isTaskActive("Final Review"))
-    {
-        updateTask("Final Review", "Resubmitted", "Additional information submitted by Applicant", "Additional information submitted by Applicant");
-        updateAppStatus("Resubmitted");
-    }
-               
-    if (appStatus == "Awaiting Client Reply")
-    {
-        updateAppStatus("Resubmitted");        
-    }
-    // EHIMS-5036     
-    
+   
+    // EHIMS-5036  
     var body = "appStatus: " + appStatus + " capId: " + capId + " Complete cap? " + cap.isCompleteCap();
 
     aa.sendMail("noreplyehims@suffolkcountyny.gov","ada.chan@suffolkcountyny.gov", "", "DUA WWM Resid App", body);
