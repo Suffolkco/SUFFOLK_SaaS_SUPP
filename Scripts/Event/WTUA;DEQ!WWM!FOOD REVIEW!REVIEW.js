70ac98c4ee891e24c7821e1c9df3f98926b8a881
Ada <ada.chan@suffolkcountyny.gov>
2023-11-01 09:42:50 -0400

    EHIMS-831: Food review notice

--- Scripts/Event/WTUA;DEQ!WWM!FOOD REVIEW!REVIEW.js
+++ Scripts/Event/WTUA;DEQ!WWM!FOOD REVIEW!REVIEW.js
@@ -1,19 +1,57 @@
 var showDebug = false;
  
-if (wfTask == "Review" && wfStatus == "Complete")
-{
-    logDebug("WF Task and WF Status is correct.");
-    logDebug(wfComment + "is the value of wfComment.");
-    if (wfComment !== null)
+// EHIMS-831
+if (wfTask == "Review" && (wfStatus == "Complete" || wfStatus == "OWM Review Required"))
+{    
+    //logDebug(wfComment + "is the value of wfComment.");
+    //if (wfComment !== null)
     {
         logDebug("A report will be fired, with the Standard Comment inside.")
-        // EHIMS-831: Do not send any email out
-        //workflowFoodReviewCompleteWWM("Food Review Notice", "RecordID");
-        //need to single out Applicant only
-        // need to update notification template DEQ_WWM_FOOD_REVIEW_NOTICE with language from Suffolk County
+        workflowFoodReviewCompleteWWMLocal("Food Review Notice", "RecordID");        
     }
-    else if (wfComment == null)
-    {
-        logDebug("A report will not be fired, since there is no Comment entered here.")
-    }
+    
 }
+
+function workflowFoodReviewCompleteWWMLocal(reportName, reportParamRecID)
+{
+	var emailParams = aa.util.newHashtable();
+	var reportParams = aa.util.newHashtable();
+	var reportFile = new Array();
+	var conArray = getContactArray();
+	var conEmail = "";
+    var fromEmail = "";
+    var itemCap = aa.cap.getCap(capId).getOutput();
+	appTypeResult = itemCap.getCapType();
+	appTypeString = appTypeResult.toString(); 
+	appTypeArray = appTypeString.split("/");
+    var alternateID = capId.getCustomID();
+
+    reportParams.put(reportParamRecID, alternateID.toString());
+
+    rFile = generateReport(reportName,reportParams, appTypeArray[0])
+    logDebug("This is the rFile: " + rFile);           
+    
+        if (rFile) {
+        reportFile.push(rFile);
+        }
+
+	if(matches(fromEmail, null, "", undefined))
+	{
+		fromEmail = "";
+	}
+	for (con in conArray)
+	{
+        conEmail += conArray[con].email + "; ";
+	}
+	getRecordParams4Notification(emailParams);
+    getWorkflowParams4Notification(emailParams);
+    
+    //addParameter(emailParams, "$$applicationName$$", capId.getCapModel().getAppTypeAlias());
+    addParameter(emailParams, "$$altID$$", capId.getCustomID());
+	if (conEmail != null)
+	{
+        sendNotification("", conEmail, "", "DEQ_WWM_FOOD_REVIEW_REQUIRED", emailParams, reportFile);
+    }
+    logDebug(conEmail + "is the e-mail address.");
+}
+
