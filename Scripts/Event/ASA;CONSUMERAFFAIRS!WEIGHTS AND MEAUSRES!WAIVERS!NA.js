bcc4a84077be5da0bb518be7bb323b673949459c
Ada <ada.chan@suffolkcountyny.gov>
2024-03-11 10:26:31 -0400

    DAP-704: Adding fees on ACA application

---
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!WEIGHTS AND MEAUSRES!WAIVERS!NA.js
@@ -0,0 +1,61 @@
+//ASA;CONSUMERAFFAIRS!WEIGHTS AND MEAUSRES!WAIVERS!NA//
+
+var footage = AInfo["Gross Square Footage of Store"];
+
+if (footage != null)
+{
+	if (parseInt(footage) <= 3000)
+	{
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 1, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "PW_WM_W4", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 0, "Y");
+		
+	}
+	else if (parseInt(footage) > 3000 && parseInt(footage) <= 10000)
+	{
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 1, "Y");
+
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 0, "Y");			
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "PW_WM_W4", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 0, "Y");
+
+		
+	}
+	else if (parseInt(footage) > 10000 && parseInt(footage) <= 30000)
+	{
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 1, "Y");
+
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 0, "Y");			
+		updateFee("CAPOS_WT_MRS", "PW_WM_W4", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 0, "Y");
+		
+	}
+	else if (parseInt(footage) > 30000 && parseInt(footage) <= 90000)
+	{
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W4", "FINAL", 1, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 0, "Y");			
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 0, "Y");
+		
+	}
+	else if (parseInt(footage) > 90000)
+	{
+
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 1, "Y");
+
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 0, "Y");
+		updateFee("CAPOS_WT_MRS", "PW_WM_W4", "FINAL", 0, "Y");
+		
+	}
+}
+
+
+
+
