ea8c3c9e15a2be1af3c7f0596c3921a9c604184e
Ada <ada.chan@suffolkcountyny.gov>
2024-03-11 13:33:46 -0400

    DAP-704

--- Scripts/Event/ASA;CONSUMERAFFAIRS!WEIGHTS AND MEAUSRES!WAIVERS!NA.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!WEIGHTS AND MEAUSRES!WAIVERS!NA.js
@@ -1,7 +1,7 @@
 //ASA;CONSUMERAFFAIRS!WEIGHTS AND MEAUSRES!WAIVERS!NA//
 var showDebug = true;
 var emailAddress = "ada.chan@suffolkcountyny.gov";
-
+var emailText ="";
 
 var footage;
 if(!publicUser)
@@ -20,52 +20,52 @@ if (!matches(footage, null, undefined, ""))
 
 	if (parseInt(footage) <= 3000)
 	{
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 1, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "PW_WM_W4", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W1", "CAPOS_WT_MRS", "FINAL", 1, "Y");
+		updateFee("IPW_WM_W2","CAPOS_WT_MRS",  "FINAL", 0, "Y");
+		updateFee("IPW_WM_W3", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("PW_WM_W4", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W5", "CAPOS_WT_MRS", "FINAL", 0, "Y");
 		
 	}
 	else if (parseInt(footage) > 3000 && parseInt(footage) <= 10000)
 	{
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 1, "Y");
+		updateFee("IPW_WM_W2", "CAPOS_WT_MRS", "FINAL", 1, "Y");
 
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 0, "Y");			
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "PW_WM_W4", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W1", "CAPOS_WT_MRS",  "FINAL", 0, "Y");			
+		updateFee("IPW_WM_W3", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("PW_WM_W4", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W5", "CAPOS_WT_MRS", "FINAL", 0, "Y");
 
 		
 	}
 	else if (parseInt(footage) > 10000 && parseInt(footage) <= 30000)
 	{
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 1, "Y");
+		updateFee("IPW_WM_W3", "CAPOS_WT_MRS", "FINAL", 1, "Y");
 
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 0, "Y");			
-		updateFee("CAPOS_WT_MRS", "PW_WM_W4", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W1", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W2", "CAPOS_WT_MRS", "FINAL", 0, "Y");			
+		updateFee("PW_WM_W4", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W5", "CAPOS_WT_MRS", "FINAL", 0, "Y");
 		
 	}
 	else if (parseInt(footage) > 30000 && parseInt(footage) <= 90000)
 	{
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W4", "FINAL", 1, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 0, "Y");			
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W4","CAPOS_WT_MRS",  "FINAL", 1, "Y");
+		updateFee("IPW_WM_W1", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W2", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W3", "CAPOS_WT_MRS", "FINAL", 0, "Y");			
+		updateFee("IPW_WM_W5", "CAPOS_WT_MRS", "FINAL", 0, "Y");
 		
 	}
 	else if (parseInt(footage) > 90000)
 	{
 
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W5", "FINAL", 1, "Y");
+		updateFee("IPW_WM_W5", "CAPOS_WT_MRS", "FINAL", 1, "Y");
 
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W1", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W2", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "IPW_WM_W3", "FINAL", 0, "Y");
-		updateFee("CAPOS_WT_MRS", "PW_WM_W4", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W1", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W2", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("IPW_WM_W3", "CAPOS_WT_MRS", "FINAL", 0, "Y");
+		updateFee("PW_WM_W4", "CAPOS_WT_MRS", "FINAL", 0, "Y");
 		
 	}
 }
