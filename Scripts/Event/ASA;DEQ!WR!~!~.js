53a95161336d9196122f4e34993b1df473d3ae64
Ada <ada.chan@suffolkcountyny.gov>
2024-04-01 15:58:03 -0400

    EHIMS-5239: pdating Auto Email for Water Modification Records

---
+++ Scripts/Event/ASA;DEQ!WR!~!~.js
@@ -0,0 +1,26 @@
+//ASA;DEQ/WR/*/*
+// EHIMS-5239
+
+// Short Notes = Project Name in DEQ
+var shortNotes = getShortNotes(capId);
+logDebug("This is the value for project name: " + shortNotes);
+
+//Application Name
+var appName = cap.getSpecialText();
+logDebug("This is the value for Application Name: " + appName);
+
+var itemCapType = aa.cap.getCap(capId).getOutput().getCapType().toString();
+if(matches(itemCapType, "DEQ/WR/Water Modification/Application")) 
+{
+    if (publicUser)
+    {
+        // In ACA, the project name field is empty.
+        // We want to copy the application name to project name on ACA submittal.
+        updateShortNotes(appName); 
+    }
+    else
+    {
+        //In AA, we want the project name to be copied to the Application name field.
+        editAppName(shortNotes);
+    }
+}
