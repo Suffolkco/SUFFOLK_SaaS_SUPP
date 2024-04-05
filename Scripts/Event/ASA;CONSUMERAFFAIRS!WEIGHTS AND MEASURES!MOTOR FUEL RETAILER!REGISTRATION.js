0d1fee9139102231fddf78f8f659f9ce3f36d25b
Ada <ada.chan@suffolkcountyny.gov>
2024-04-05 15:26:22 -0400

    DAP-709

--- Scripts/Event/ASA;CONSUMERAFFAIRS!WEIGHTS AND MEASURES!MOTOR FUEL RETAILER!REGISTRATION.js
+++ Scripts/Event/ASA;CONSUMERAFFAIRS!WEIGHTS AND MEASURES!MOTOR FUEL RETAILER!REGISTRATION.js
@@ -8,3 +8,133 @@ voidRemoveFees("WM_RA");
 updateFee("WM_12", "CAPOS_WT_MRS", "FINAL", 1, "Y");	
 updateFee("WM_RA", "CAPOS_WT_MRS", "FINAL", 200, "Y");		
 
+// Whenenver a new GSR is applied, it should trigger a Call In/Device notification for weights and measures.
+var deviceNotification = createChildLocal("ConsumerAffairs", "Weights and Measures", "Call In", "NA", "Created from Retailer Record", capId);
+    
+if (deviceNotification != null)
+{	   
+    logDebug("Copy contacts from : " + capId.getCustomID() + " to " + deviceNotification.getCustomID());   
+	copyPeople(capId, deviceNotification);    
+   
+}
+
+
+function createChildLocal(grp, typ, stype, cat, desc) // optional parent capId
+{
+    //
+    // creates the new application and returns the capID object
+    //
+
+    var itemCap = capId
+    if (arguments.length > 5) itemCap = arguments[5]; // use cap ID specified in args
+
+    var appCreateResult = aa.cap.createApp(grp, typ, stype, cat, desc);
+
+    logDebug("creating cap " + grp + "/" + typ + "/" + stype + "/" + cat);
+    if (appCreateResult.getSuccess())
+    {
+        var newId = appCreateResult.getOutput();
+        logDebug("cap " + grp + "/" + typ + "/" + stype + "/" + cat + " created successfully ");
+
+        // create Detail Record
+        capModel = aa.cap.newCapScriptModel().getOutput();
+        capDetailModel = capModel.getCapModel().getCapDetailModel();
+        capDetailModel.setCapID(newId);
+        aa.cap.createCapDetail(capDetailModel);
+
+        var newObj = aa.cap.getCap(newId).getOutput();	//Cap object
+        var result = aa.cap.createAppHierarchy(itemCap, newId);
+        if (result.getSuccess())
+            logDebug("Child application successfully linked");
+        else
+            logDebug("Could not link applications");
+
+        // Copy Parcels
+        var capParcelResult = aa.parcel.getParcelandAttribute(itemCap, null);
+        if (capParcelResult.getSuccess())
+        {
+            var Parcels = capParcelResult.getOutput().toArray();
+            for (zz in Parcels)
+            {
+                logDebug("adding parcel #" + zz + " = " + Parcels[zz].getParcelNumber());
+                var newCapParcel = aa.parcel.getCapParcelModel().getOutput();
+                newCapParcel.setParcelModel(Parcels[zz]);
+                newCapParcel.setCapIDModel(newId);
+                newCapParcel.setL1ParcelNo(Parcels[zz].getParcelNumber());
+                newCapParcel.setParcelNo(Parcels[zz].getParcelNumber());
+                aa.parcel.createCapParcel(newCapParcel);
+            }
+        }
+
+        // Copy Addresses
+        capAddressResult = aa.address.getAddressByCapId(itemCap);
+        if (capAddressResult.getSuccess())
+        {
+            Address = capAddressResult.getOutput();
+            for (yy in Address)
+            {
+                newAddress = Address[yy];
+                newAddress.setCapID(newId);
+                aa.address.createAddress(newAddress);
+                logDebug("added address");
+            }
+        }
+
+        return newId;
+    }
+    else
+    {
+        logDebug("**ERROR: adding child App: " + appCreateResult.getErrorMessage());
+    }
+}
+function copyPeople(srcCapId, targetCapId)
+{
+  //1. Get people with source CAPID.
+  var capPeoples = getPeople(srcCapId);
+  logDebug("Source Cap ID:" + srcCapId);
+
+  if (capPeoples == null || capPeoples.length == 0)
+  {
+    logDebug("Didn't get the source peoples!");
+    return;
+  }
+  //2. Get people with target CAPID.
+  var targetPeople = getPeople(targetCapId);
+  //3. Check to see which people is matched in both source and target.
+  for (loopk in capPeoples)
+  {
+    sourcePeopleModel = capPeoples[loopk];
+    //3.1 Set target CAPID to source people.
+    sourcePeopleModel.getCapContactModel().setCapID(targetCapId);
+    
+    targetPeopleModel = null;
+    //3.2 Check to see if sourcePeople exist.
+    if (targetPeople != null && targetPeople.length > 0)
+    {
+      for (loop2 in targetPeople)
+      {
+        if (isMatchPeople(sourcePeopleModel, targetPeople[loop2]))
+        {
+          targetPeopleModel = targetPeople[loop2];
+          break;
+        }
+      }
+    }
+    //3.3 It is a matched people model.
+    if (targetPeopleModel != null)
+    {
+      //3.3.1 Copy information from source to target.
+      aa.people.copyCapContactModel(sourcePeopleModel.getCapContactModel(), 
+
+      targetPeopleModel.getCapContactModel());
+      //3.3.2 Edit People with source People information. 
+      aa.people.editCapContactWithAttribute(targetPeopleModel.getCapContactModel());
+    }
+    //3.4 It is new People model.
+    else
+    {
+      //3.4.1 Create new people.
+      aa.people.createCapContactWithAttribute(sourcePeopleModel.getCapContactModel());
+    }
+  }
+}
