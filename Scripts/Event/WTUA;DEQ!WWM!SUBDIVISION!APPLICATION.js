cc846da0c36104c6a52218e2ed02d79ea6c62e79
Ada <ada.chan@suffolkcountyny.gov>
2024-06-26 15:48:50 -0400

    EHIMS-5261

--- Scripts/Event/WTUA;DEQ!WWM!SUBDIVISION!APPLICATION.js
+++ Scripts/Event/WTUA;DEQ!WWM!SUBDIVISION!APPLICATION.js
@@ -393,3 +393,49 @@ function inspectionCompleted()
     }
     return inspectionCompleted;
 }
+function getDocumentList()
+{
+    // Returns an array of documentmodels if any
+    // returns an empty array if no documents
+
+    var docListArray = new Array();
+
+    docListResult = aa.document.getCapDocumentList(capId, currentUserID);
+
+    if (docListResult.getSuccess())
+    {
+        docListArray = docListResult.getOutput();
+    }
+    return docListArray;
+}
+
+function generateReportLocal(aaReportName,parameters,rModule) {
+	var reportName = aaReportName;
+      
+    report = aa.reportManager.getReportInfoModelByName(reportName);
+    report = report.getOutput();
+    
+    report.setModule(rModule);
+    report.setCapId(capId);	
+    report.setReportParameters(parameters);
+
+    var permit = aa.reportManager.hasPermission(reportName,currentUserID);
+
+    if(permit.getOutput().booleanValue()) {
+       var reportResult = aa.reportManager.getReportResult(report);
+     
+       if(reportResult) {
+	       reportResult = reportResult.getOutput();
+	       var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+			logMessage("Report Result: "+ reportResult);
+	       reportFile = reportFile.getOutput();
+	       return reportFile
+       } else {
+       		logMessage("Unable to run report: "+ reportName + " for Admin" + systemUserObj);
+       		return false;
+       }
+    } else {
+         logMessage("No permission to report: "+ reportName + " for Admin" + systemUserObj);
+         return false;
+    }
+}
