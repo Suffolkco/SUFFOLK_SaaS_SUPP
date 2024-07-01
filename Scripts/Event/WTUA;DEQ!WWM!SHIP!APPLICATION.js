5c37c91d964f0d054b15d9884edb7b302acc9c09
Ada <ada.chan@suffolkcountyny.gov>
2024-07-01 15:57:11 -0400

    EHIMS-5224

--- Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
+++ Scripts/Event/WTUA;DEQ!WWM!SHIP!APPLICATION.js
@@ -551,10 +551,10 @@ if (wfTask == "Final Review")
                 for(x in childArray){
                     var iaCapId = childArray[x];
                     logDebug("iaCapId: " + iaCapId);
+                    logDebug("capId: " + capId);
                     copyDocumentType(capId, iaCapId, "Final Sketch");                            
                 }
                  
-
             }
             rcRFiles.push(rcReportFile);
         }
@@ -1061,6 +1061,7 @@ if (wfStatus == "Withdrawn")
     updateAppStatus("Withdrawn");
 }
 
+
 /*
  * find a document on record and download it on disk, preparing it to be used on email attachment.
  * at least documentType or documentFileName should be passed, search priority for documentType.
@@ -1362,81 +1363,3 @@ function GetLastInspDate(capId) {
     return LastDate;
 }
 
-function copyDocumentType(pFromCapId, pToCapId, documentType)
-{
-    //Copies all attachments (documents) from pFromCapId to pToCapId
-    var categoryArray = new Array();
-
-    // third optional parameter is comma delimited list of categories to copy.
-    if (arguments.length > 2)
-    {
-        categoryList = arguments[2];
-        categoryArray = categoryList.split(",");
-    }
-
-    var capDocResult = aa.document.getDocumentListByEntity(pFromCapId, "CAP");
-    if (capDocResult.getSuccess())
-    {
-        if (capDocResult.getOutput().size() > 0)
-        {
-            for (docInx = 0; docInx < capDocResult.getOutput().size(); docInx++)
-            {
-                var documentObject = capDocResult.getOutput().get(docInx);
-                currDocCat = "" + documentObject.getDocCategory();
-
-                
-				logDebug("Document Category: " + currDocCat);
-				if (currDocCat == documentType) {
-
-                    if (categoryArray.length == 0 || exists(currDocCat, categoryArray))
-                    {
-                        // download the document content
-                        var useDefaultUserPassword = true;
-                        //If useDefaultUserPassword = true, there is no need to set user name & password, but if useDefaultUserPassword = false, we need define EDMS user name & password.
-                        var EMDSUsername = null;
-                        var EMDSPassword = null;
-                        var path = null;
-                        var downloadResult = aa.document.downloadFile2Disk(documentObject, documentObject.getModuleName(), EMDSUsername, EMDSPassword, useDefaultUserPassword);
-                        if (downloadResult.getSuccess())
-                        {
-                            path = downloadResult.getOutput();
-                        }
-                        var tmpEntId = pToCapId.getID1() + "-" + pToCapId.getID2() + "-" + pToCapId.getID3();
-                        documentObject.setDocumentNo(null);
-                        documentObject.setCapID(pToCapId)
-                        documentObject.setEntityID(tmpEntId);
-
-                        // Open and process file
-                        try
-                        {
-                            if (path != null && path != "")
-                            {
-                                // put together the document content - use java.io.FileInputStream
-                                var newContentModel = aa.document.newDocumentContentModel().getOutput();
-                                inputstream = aa.io.FileInputStream(path);
-                                newContentModel.setDocInputStream(inputstream);
-                                documentObject.setDocumentContent(newContentModel);
-                                var newDocResult = aa.document.createDocument(documentObject);
-                                if (newDocResult.getSuccess())
-                                {
-                                    newDocResult.getOutput();
-                                    logDebug("Successfully copied document: " + documentObject.getFileName() + " From: " + pFromCapId.getCustomID() + " To: " + pToCapId.getCustomID());
-                                }
-                                else
-                                {
-                                    logDebug("Failed to copy document: " + documentObject.getFileName());
-                                    logDebug(newDocResult.getErrorMessage());
-                                }
-                            }
-                        }
-                        catch (err)
-                        {
-                            logDebug("Error copying document: " + err.message);
-                            return false;
-                        }
-                    }
-                }
-            } // end for loop
-        }
-    }
-}
