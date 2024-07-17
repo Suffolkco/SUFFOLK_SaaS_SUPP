15221f353d69261d40e94f295c310e3260842cf0
Ada <ada.chan@suffolkcounty.gov>
2024-07-17 15:20:21 -0400

    EHIMS-5290: Do not copy contact

--- INCLUDES_CUSTOM/checkForRelatedSITERecord.js
+++ INCLUDES_CUSTOM/checkForRelatedSITERecord.js
@@ -45,7 +45,8 @@ function checkForRelatedSITERecord(parcelNumber) {
              var myParent = createParent("DEQ", "General", "Site", "NA", "Created from " + updateToName);
  
              copyParcels(capId, myParent);
-             copyOwner(capId, myParent);
+             // EHIMS-5290
+             //copyOwner(capId, myParent);
              copyParcelGisObjectsParent(capId, myParent);
              updateShortNotes(updateToName, myParent);        
              editAppName(updateToName, myParent);
