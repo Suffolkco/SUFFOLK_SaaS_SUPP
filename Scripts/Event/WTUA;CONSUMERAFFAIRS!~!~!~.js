f1795a441865709a80c813330b1cd9e56e567020
Ada <ada.chan@suffolkcountyny.gov>
2024-08-05 11:57:27 -0400

    Sysaid 36894: Shelved status

--- Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
+++ Scripts/Event/WTUA;CONSUMERAFFAIRS!~!~!~.js
@@ -8,6 +8,41 @@ if (matches(appTypeArray[1], "Registrations", "ID Cards", "Licenses")) {
 	}
 }
 
+if (matches(appTypeArray[1], "Licenses")) {
+	if (wfTask == "Issuance" && wfStatus == "Shelved")
+	{
+		//1.  Check to see if the renewal was initiated before.
+		result = aa.cap.getProjectByMasterID(capid, "Renewal", "Incomplete");
+		var tChild;
+		if (result.getSuccess()) {
+			childrenArr = result.getOutput();
+			logDebugLocal("Found renewal: " + childrenArr.length);
+			if (childrenArr != null && childrenArr.length > 0) {
+				
+				for (var cIdx in childrenArr) {
+					
+					logDebugLocal("Found renewal alt ID: " + childrenArr[cIdx].capID.toString());
+					var tChildId = childrenArr[cIdx].capID.toString().split("-");
+					tChild = aa.cap.getCapID(tChildId[0],tChildId[1],tChildId[2]).getOutput();						
+				}
+
+				feeCode = "LIC_REN_01";
+
+				if (feeExistsWithCapId(feeCode, "Invoiced", tChild) ||
+				feeExistsWithCapId(feeCode, "New", tChild))
+				{					
+					logDebugLocal("Void and remove fee: " + feeCode);
+					voidRemoveFeesLocal(feeCode, tChild);		
+					logDebug("Parent record has a status of Shelved. SLS_8 fee code instead.")
+					updateFee("SLS_38", "CA_SALES", "FINAL", 1, "Y", "N", null) 
+		
+				}
+			}
+	
+		}
+	}
+}
+
 // DOCKET-66
 if (matches(appTypeArray[1], "Licenses") && appTypeArray[2] != "Renewal" && matches(appTypeArray[3], "NA"))
 {
@@ -160,3 +195,87 @@ function getUserIDAssignedToTask(taskName,vCapId){
 		return false;
 		}
 	}
+
+function voidRemoveFeesLocal(vFeeCode, childCap)
+{
+var feeSeqArray = new Array();
+var invoiceNbrArray = new Array();
+var feeAllocationArray = new Array();
+var itemCap = capId;
+if (arguments.length > 1)
+    itemCap = arguments[1];
+
+// for each fee found
+//  	  if the fee is "NEW" remove it
+//  	  if the fee is "INVOICED" void it and invoice the void
+//
+
+var targetFees = loadFees(childCap);
+
+for (tFeeNum in targetFees)
+    {
+    targetFee = targetFees[tFeeNum];
+
+    if (targetFee.code.equals(vFeeCode))
+        {
+
+        // only remove invoiced or new fees, however at this stage all AE fees should be invoiced.
+
+        if (targetFee.status == "INVOICED")
+            {
+            var editResult = aa.finance.voidFeeItem(childCap, targetFee.sequence);
+
+            if (editResult.getSuccess())
+                logDebug("Voided existing Fee Item: " + targetFee.code);
+            else
+                { logDebug( "**ERROR: voiding fee item (" + targetFee.code + "): " + editResult.getErrorMessage()); return false; }
+
+            var feeSeqArray = new Array();
+            var paymentPeriodArray = new Array();
+
+            feeSeqArray.push(targetFee.sequence);
+            paymentPeriodArray.push(targetFee.period);
+            var invoiceResult_L = aa.finance.createInvoice(itemCap, feeSeqArray, paymentPeriodArray);
+
+            if (!invoiceResult_L.getSuccess())
+                {
+                logDebug("**ERROR: Invoicing the fee items voided " + thisFee.code + " was not successful.  Reason: " +  invoiceResult_L.getErrorMessage());
+                return false;
+                }
+
+            }
+
+
+
+        if (targetFee.status == "NEW")
+            {
+            // delete the fee
+            var editResult = aa.finance.removeFeeItem(itemCap, targetFee.sequence);
+
+            if (editResult.getSuccess())
+                logDebug("Removed existing Fee Item: " + targetFee.code);
+            else
+                { logDebug( "**ERROR: removing fee item (" + targetFee.code + "): " + editResult.getErrorMessage()); return false; }
+
+            }
+
+        } // each matching fee
+    }  // each  fee
+}  // function
+
+function feeExistsWithCapId(feestr,feeStatus,vCapId){
+	var feeResult = aa.fee.getFeeItems(vCapId, feestr, null);
+	
+	if (feeResult.getSuccess()) {
+		var feeObjArr = feeResult.getOutput();
+		logDebugLocal("Found fee code for : " + feestr + " and status: " + feeStatus);
+	} else {
+		logDebug("**ERROR: getting fee items: " + capContResult.getErrorMessage());
+		return false
+	}
+	for (ff in feeObjArr)
+		if (feestr.equals(feeObjArr[ff].getFeeCod()) && feeObjArr[ff].getFeeitemStatus() == feeStatus){
+            return true;
+        }
+	return false;
+} 
