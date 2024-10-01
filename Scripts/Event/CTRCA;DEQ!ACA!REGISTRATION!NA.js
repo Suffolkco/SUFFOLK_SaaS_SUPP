80103466dffd16735f6e9d94b4b1567f3565b28c
Ada <ada.chan@suffolkcountyny.gov>
2024-10-01 14:28:35 -0400

    EHIMS-5309

--- Scripts/Event/CTRCA;DEQ!ACA!REGISTRATION!NA.js
+++ Scripts/Event/CTRCA;DEQ!ACA!REGISTRATION!NA.js
@@ -2,43 +2,92 @@
 if (publicUser) {
 
 	aa.cap.updateAccessByACA(capId, "N");
-
-		// ASI Table
-	var pinTable = loadASITable("ACA REGISTRATION");
-
-	for (var p in pinTable)
+	var pinRequest = AInfo["Pin Request"];
+	var reportFile = new Array();	
+	if (pinRequest == 'Yes')
 	{
-		var pin = pinTable[p]["PIN"];
-		var recordId = pinTable[p]["Record ID"];
-		var id = pinTable[p]["Contact ID"];
-		var licCap = aa.cap.getCapID(parseInt(pin)).getOutput();
-		var contactID = parseInt(id);
-		var recordIdString = recordId.toString();  
+		if (AInfo["Pin Request"] == 'Yes')
+		{  
+			recordID = AInfo["Record ID"];
+			email = AInfo["Email Address"];
+			capId = getApplication(recordID);	
+			var capPeoples = getPeople(capId)
+			var reportParams1 = aa.util.newHashtable();
+			var emailParams = aa.util.newHashtable();	            
+			for (loopk in capPeoples)
+			{
+				cont = capPeoples[loopk];                 
+				peop = cont.getPeople();
+				conEmail = peop.getEmail();
+				logDebug("Scanning email: " + conEmail);
 
-		var capIDString = licCap.getCustomID();     
+				if (matches(conEmail, email))
+				{
+					logDebug("Found matching email: " + conEmail);
+					// Local contact ID
+					localCId = cont.getCapContactModel().getPeople().getContactSeqNumber();			
+					contactType = cont.getCapContactModel().getPeople().getContactType();
+					reportParams1.put("ContactID", localCId);
+					reportParams1.put("RecordID", capId.getCustomID());
+					reportParams1.put("ContactType", contactType);			
+					// ACA PIN - from reportParams1 above.      
+					rFile = generateReport("ACA Registration Pins-WWM",reportParams1, 'DEQ');
+							
+					if (rFile) {
+						reportFile.push(rFile);
+					}
 
-		logDebug(pin + "," + recordId + "," + id + "," + licCap);
+					getRecordParams4Notification(emailParams);	
+					addParameter(emailParams, "$$altID$$", capId.getCustomID());					
+    				var shortNotes = getShortNotes(capId);
+					addParameter(emailParams, "$$shortNotes$$", shortNotes);					
+					//sendNotification("", conEmail, "", "DEQ_WWM_AWAITING CLIENT REPLY_WITH_ACA_PIN", emailParams, reportFile);
+					sendNotification("", "ada.chan@suffolkcountyny.gov", "", "DEQ_WWM_AWAITING CLIENT REPLY_WITH_ACA_PIN", emailParams, reportFile);    
+					break;
+				}
 
-		logDebug("Switch to upper case: " + recordIdString.toUpperCase());
-		var recordIdUpperCase = recordIdString.toUpperCase();
+			}
+		}
+	}
+	else
+	{
+		// ASI Table
+		var pinTable = loadASITable("ACA REGISTRATION");
 
-		if (licCap)
+		for (var p in pinTable)
 		{
-			logDebug("CapIdString: " + capIDString + ", Record ID: " + recordId);
-			if (matches(capIDString, recordIdUpperCase))
-			{			
-				logDebug("Matching:" + capIDString + "," + recordIdUpperCase);  
-				editContactToSpecifcRecord(licCap, contactID, null);		
-								
-				// Set description
-				updateWorkDesc("See FAQ for details. No further steps required.");        
-				// Update Project Name
-				var projectName = "DO NOT SUBMIT UNDER THIS APPLICATION - SUBMIT UNDER " + capIDString;
-				// On ACA, this is short note
-				updateShortNotes(projectName);        
-				// On ACA, this is project name
-				editAppName(projectName);
-			}	
+			var pin = pinTable[p]["PIN"];
+			var recordId = pinTable[p]["Record ID"];
+			var id = pinTable[p]["Contact ID"];
+			var licCap = aa.cap.getCapID(parseInt(pin)).getOutput();
+			var contactID = parseInt(id);
+			var recordIdString = recordId.toString();  
+
+			var capIDString = licCap.getCustomID();     
+
+			logDebug(pin + "," + recordId + "," + id + "," + licCap);
+
+			logDebug("Switch to upper case: " + recordIdString.toUpperCase());
+			var recordIdUpperCase = recordIdString.toUpperCase();
+
+			if (licCap)
+			{
+				logDebug("CapIdString: " + capIDString + ", Record ID: " + recordId);
+				if (matches(capIDString, recordIdUpperCase))
+				{			
+					logDebug("Matching:" + capIDString + "," + recordIdUpperCase);  
+					editContactToSpecifcRecord(licCap, contactID, null);		
+									
+					// Set description
+					updateWorkDesc("See FAQ for details. No further steps required.");        
+					// Update Project Name
+					var projectName = "DO NOT SUBMIT UNDER THIS APPLICATION - SUBMIT UNDER " + capIDString;
+					// On ACA, this is short note
+					updateShortNotes(projectName);        
+					// On ACA, this is project name
+					editAppName(projectName);
+				}	
+			}
 		}
 	}
 	// Below is for single entry from ACA custom field
