8141ff4312e997e9d493d3e6d170bb776fbec9d7
Ada <ada.chan@suffolkcountyny.gov>
2024-10-07 10:05:25 -0400

    EHIMS-5278

--- INCLUDES_CUSTOM/applicationSubmittedWWM.js
+++ INCLUDES_CUSTOM/applicationSubmittedWWM.js
@@ -53,8 +53,8 @@ function applicationSubmittedWWM() {
 		reportParams1.put("ContactID", localCId);
 		reportParams1.put("RecordID", capId.getCustomID());
 		reportParams1.put("ContactType", contactType);			
-		// ACA PIN - from reportParams1 above.      
-		rFile = generateReport("ACA Registration Pins-WWM",reportParams1, 'DEQ');
+		// ACA PIN - from reportParams1 above.   
+		rFile = generateReportBatch(capId, "ACA Registration Pins-WWM", 'DEQ', reportParams1)			   		
 		logDebug("This is the ACA Pin File: " + rFile); 
 		if (rFile) {
 			reportFile.push(rFile);
@@ -70,7 +70,46 @@ function applicationSubmittedWWM() {
 			
 	}
 }
+function generateReportBatch(itemCap, reportName, module, parameters) {
+    //returns the report file which can be attached to an email.
+    var user = currentUserID; // Setting the User Name
+    var report = aa.reportManager.getReportInfoModelByName(reportName);
+    logDebug("This is the Report Parameter " + parameters);
+    if (!report.getSuccess() || report.getOutput() == null)
+    {
+        logDebug("**WARN report generation failed, missing report or incorrect name: " + reportName);
+        return false;
+    }
+    report = report.getOutput();
+    report.setModule(module);
+    report.setCapId(itemCap); //CSG Updated from itemCap.getCustomID() to just itemCap so the file would save to Record
+    report.setReportParameters(parameters);
 
+    var permit = aa.reportManager.hasPermission(reportName, user);
+    aa.print("This is the permission on the report" + permit);
+    logDebug("This is the permission on the report" + permit);
+    if (permit.getOutput().booleanValue())
+    {
+        var reportResult = aa.reportManager.getReportResult(report);
+        logDebug("Report Result" + reportResult.getSuccess());
+        if (reportResult.getSuccess())
+        {
+            reportOutput = reportResult.getOutput();
+            logDebug("This is the Report Output in the next part" + reportOutput);
+            var reportFile = aa.reportManager.storeReportToDisk(reportOutput);
+            reportFile = reportFile.getOutput();
+            return reportFile;
+        } else
+        {
+            logDebug("**WARN System failed get report: " + reportResult.getErrorType() + ":" + reportResult.getErrorMessage());
+            return false;
+        }
+    } else
+    {
+        logDebug("You have no permission.");
+        return false;
+    }
+}
 function getPeople(capId)
 {
   capPeopleArr = null;
