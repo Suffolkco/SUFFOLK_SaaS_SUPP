dc54b40643648fde5fa6d8161e3f90fd4051a7f4
Ada <ada.chan@suffolkcountyny.gov>
2024-10-16 08:56:35 -0400

    EHIMS-5309: Make Report Runnable in ACA; "ACA Registration PINs-WWM"

---
+++ Scripts/Pageflow/ACA PIN REQUEST AFTER.js
@@ -0,0 +1,319 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program : ACA PIN REQUEST AFTER.js
+| Event   : ACA PIN REQUEST AFTER
+|
+| Usage   : Master Script by Accela.  See accompanying documentation and release notes.
+|
+| Client  : N/A
+| Action# : N/A
+|
+| Notes   :
+|
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START User Configurable Parameters
+|
+|     Only variables in the following section may be changed.  If any other section is modified, this
+|     will no longer be considered a "Master" script and will not be supported in future releases.  If
+|     changes are made, please add notes above.
+/------------------------------------------------------------------------------------------------------*/
+var showMessage = false;						// Set to true to see results in popup window
+var showDebug = false;							// Set to true to see debug messages in popup window
+var preExecute = "PreExecuteForBeforeEvents"
+//var controlString = "";		// Standard choice for control
+var documentOnly = false;						// Document Only -- displays hierarchy of std choice steps
+var disableTokens = false;						// turn off tokenizing of std choices (enables use of "{} and []")
+var useAppSpecificGroupName = false;			// Use Group name when populating App Specific Info Values
+var useTaskSpecificGroupName = false;			// Use Group name when populating Task Specific Info Values
+var enableVariableBranching = false;			// Allows use of variable names in branching.  Branches are not followed in Doc Only
+var maxEntries = 99;							// Maximum number of std choice entries.  Entries must be Left Zero Padded
+/*------------------------------------------------------------------------------------------------------/
+| END User Configurable Parameters
+/------------------------------------------------------------------------------------------------------*/
+var cancel = false;
+var startDate = new Date();
+var startTime = startDate.getTime();
+var message =	"";							// Message String
+var debug = "";								// Debug String
+var br = "<BR>";							// Break Tag
+var feeSeqList = new Array();						// invoicing fee list
+var paymentPeriodList = new Array();					// invoicing pay periods
+
+if (documentOnly) {
+	doStandardChoiceActions(controlString,false,0);
+	aa.env.setValue("ScriptReturnCode", "0");
+	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");
+	aa.abortScript();
+}
+
+var useSA = false;
+var SA = null;
+var SAScript = null;
+var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_FOR_EMSE"); 
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") { 
+	useSA = true; 	
+	SA = bzr.getOutput().getDescription();
+	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_INCLUDE_SCRIPT"); 
+	if (bzr.getSuccess()) { SAScript = bzr.getOutput().getDescription(); }
+}
+
+if (SA) {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",SA));
+	eval(getScriptText(SAScript,SA));
+}
+else {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+}
+
+eval(getScriptText("INCLUDES_CUSTOM"));
+
+if (documentOnly) {
+	doStandardChoiceActions(controlString,false,0);
+	aa.env.setValue("ScriptReturnCode", "0");
+	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");
+	aa.abortScript();
+}
+
+function getScriptText(vScriptName){
+	var servProvCode = aa.getServiceProviderCode();
+	if (arguments.length > 1) servProvCode = arguments[1]; // use different serv prov code
+	vScriptName = vScriptName.toUpperCase();	
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		var emseScript = emseBiz.getScriptByPK(servProvCode,vScriptName,"ADMIN");
+		return emseScript.getScriptText() + "";	
+	} catch(err) {
+		return "";
+	}
+}
+
+var cap = aa.env.getValue("CapModel");
+var capId = cap.getCapID();
+var servProvCode = capId.getServiceProviderCode()       		// Service Provider Code
+var publicUser = false ;
+var currentUserID = aa.env.getValue("CurrentUserID");
+var publicUserID = aa.env.getValue("CurrentUserID");
+if (currentUserID.indexOf("PUBLICUSER") == 0) { currentUserID = "ADMIN" ; publicUser = true }  // ignore public users
+var capIDString = capId.getCustomID();					// alternate cap id string
+var systemUserObj = aa.person.getUser(currentUserID).getOutput();  	// Current User Object
+var appTypeResult = cap.getCapType();
+var appTypeString = appTypeResult.toString();				// Convert application type to string ("Building/A/B/C")
+var appTypeArray = appTypeString.split("/");				// Array of application type string
+var currentUserGroup;
+var currentUserGroupObj = aa.userright.getUserRight(appTypeArray[0],currentUserID).getOutput()
+if (currentUserGroupObj) currentUserGroup = currentUserGroupObj.getGroupName();
+var capName = cap.getSpecialText();
+var capStatus = cap.getCapStatus();
+var sysDate = aa.date.getCurrentDate();
+var sysDateMMDDYYYY = dateFormatted(sysDate.getMonth(),sysDate.getDayOfMonth(),sysDate.getYear(),"");
+var parcelArea = 0;
+
+var estValue = 0; var calcValue = 0; var feeFactor			// Init Valuations
+var valobj = aa.finance.getContractorSuppliedValuation(capId,null).getOutput();	// Calculated valuation
+if (valobj.length) {
+	estValue = valobj[0].getEstimatedValue();
+	calcValue = valobj[0].getCalculatedValue();
+	feeFactor = valobj[0].getbValuatn().getFeeFactorFlag();
+}
+
+var balanceDue = 0 ; var houseCount = 0; feesInvoicedTotal = 0;		// Init detail Data
+var capDetail = "";
+var capDetailObjResult = aa.cap.getCapDetail(capId);			// Detail
+if (capDetailObjResult.getSuccess())
+{
+	capDetail = capDetailObjResult.getOutput();
+	var houseCount = capDetail.getHouseCount();
+	var feesInvoicedTotal = capDetail.getTotalFee();
+	var balanceDue = capDetail.getBalance();
+}
+
+var AInfo = new Array();						// Create array for tokenized variables
+//loadAppSpecific4ACA(AInfo); 						// Add AppSpecific Info
+//loadTaskSpecific(AInfo);						// Add task specific info
+//loadParcelAttributes(AInfo);						// Add parcel attributes
+loadASITables4ACA();
+
+/*------------------------------------------------------------------------------------------------------/
+| BEGIN Event Specific Variables
+/------------------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------------------/
+| END Event Specific Variables
+/------------------------------------------------------------------------------------------------------*/
+
+if (preExecute.length) doStandardChoiceActions(preExecute,true,0); 	// run Pre-execution code
+
+logGlobals(AInfo); 
+var AInfo = new Array();
+loadAppSpecific4ACA(AInfo);
+/*------------------------------------------------------------------------------------------------------/
+| <===========Main=Loop================>
+|
+/-----------------------------------------------------------------------------------------------------*/
+try
+{       
+    logDebug(AInfo["Pin Request"]);
+
+    if (AInfo["Pin Request"] == 'Yes')
+    {  
+       
+        showMessage = true;           
+        comment("A letter containing a <strong>Record ID</strong>, <strong>PIN Number</strong> and a <strong>Contact ID</strong> will be sent to the email provided after your complete application has been submitted.");                   
+
+       
+    }    
+  
+}        
+catch (err) { logDebug(err)	}
+/*------------------------------------------------------------------------------------------------------/
+| <===========END=Main=Loop================>
+/-----------------------------------------------------------------------------------------------------*/
+
+function getApplicationLocal(appNum) {
+	var getCapResult = aa.cap.getCapID(appNum);
+	if (getCapResult.getSuccess())
+		return getCapResult.getOutput();
+	else
+		return null;
+	}
+
+if (debug.indexOf("**ERROR") > 0)
+	{
+	aa.env.setValue("ErrorCode", "1");
+	aa.env.setValue("ErrorMessage", debug);
+	}
+else
+	{
+	if (cancel)
+		{
+		aa.env.setValue("ErrorCode", "-2");
+		if (showMessage) aa.env.setValue("ErrorMessage", message);
+		if (showDebug) 	aa.env.setValue("ErrorMessage", debug);
+		}
+	else
+		{
+		aa.env.setValue("ErrorCode", "0");
+		if (showMessage) aa.env.setValue("ErrorMessage", message);
+		if (showDebug) 	aa.env.setValue("ErrorMessage", debug);
+		}
+	}
+
+/*------------------------------------------------------------------------------------------------------/
+| <===========External Functions (used by Action entries)
+/------------------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------------------/
+| Custom Functions (End)
+/------------------------------------------------------------------------------------------------------*/
+function editAppSpecific4ACA(itemName, itemValue) {
+
+
+
+    var i = cap.getAppSpecificInfoGroups().iterator();
+
+
+
+    while (i.hasNext()) {
+
+        var group = i.next();
+
+        var fields = group.getFields();
+
+        if (fields != null) {
+
+            var iteFields = fields.iterator();
+
+            while (iteFields.hasNext()) {
+
+                var field = iteFields.next();
+
+                if ((useAppSpecificGroupName && itemName.equals(field.getCheckboxType() + "." + 
+
+
+
+field.getCheckboxDesc())) || itemName.equals(field.getCheckboxDesc())) {
+
+                    field.setChecklistComment(itemValue);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+function getUserEmail() {
+    //optional parameter for userid
+    var userId = currentUserID;
+    if (arguments.length > 0)
+        userId = arguments[0];
+
+    var systemUserObjResult = aa.person.getUser(userId.toUpperCase());
+
+    if (systemUserObjResult.getSuccess()) {
+        var systemUserObj = systemUserObjResult.getOutput();
+
+        var userEmail = systemUserObj.getEmail();
+
+        if (userEmail)
+            return userEmail;
+        else
+            return false; 
+
+    } else {
+        aa.print(systemUserObjResult.getErrorMessage());
+        return false;
+    }
+}
+ 
+function getRefContactForPublicUser(userSeqNum) {
+	contractorPeopleBiz = aa.proxyInvoker.newInstance("com.accela.pa.people.ContractorPeopleBusiness").getOutput();
+	userList = aa.util.newArrayList();
+	userList.add(userSeqNum);
+	peopleList = contractorPeopleBiz.getContractorPeopleListByUserSeqNBR(aa.getServiceProviderCode(), userList); 
+	if (peopleList != null) {
+		peopleArray = peopleList.toArray();
+		if (peopleArray.length > 0)
+			return peopleArray[0];
+	}
+	return null;
+}
+function getPeople(capId)
+{
+  capPeopleArr = null;
+  var s_result = aa.people.getCapContactByCapID(capId);
+  if(s_result.getSuccess())
+  {
+    capPeopleArr = s_result.getOutput();
+    if (capPeopleArr == null || capPeopleArr.length == 0)
+    {
+      aa.print("WARNING: no People on this CAP:" + capId);
+      capPeopleArr = null;
+    }
+  }
+  else
+  {
+    aa.print("ERROR: Failed to People: " + s_result.getErrorMessage());
+    capPeopleArr = null;  
+  }
+  return capPeopleArr;
+}
+
+function matches(eVal, argList) {
+	for (var i = 1; i < arguments.length; i++) {
+		if (arguments[i] == eVal) {
+			return true;
+		}
+	}
+	return false;
+} 
+
+function debugObject(object) {
+	var output = ''; 
+	for (property in object) { 
+	  output += "<font color=red>" + property + "</font>" + ': ' + "<bold>" + object[property] + "</bold>" +'; ' + "<BR>"; 
+	} 
+	logDebug(output);
+} 
